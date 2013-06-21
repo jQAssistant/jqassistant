a6338da4cfe1c4a5dbf25beb8e572105b29fcc7b
Dirk Mahler <dirk.mahler@buschmais.com>
2013-06-21 23:41:47 +0200

    added reader for rule configurations

---
+++ core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/api/model/Constraint.java
@@ -0,0 +1,55 @@
+package com.buschmais.jqassistant.core.analysis.api.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dimahler
+ * Date: 6/21/13
+ * Time: 3:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constraint {
+
+    private String id;
+
+    private String description;
+
+    private Query query;
+
+    private Set<Concept> requiredConcepts = new HashSet<Concept>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Query getQuery() {
+        return query;
+    }
+
+    public void setQuery(Query query) {
+        this.query = query;
+    }
+
+    public Set<Concept> getRequiredConcepts() {
+        return requiredConcepts;
+    }
+
+    public void setRequiredConcepts(Set<Concept> requiredConcepts) {
+        this.requiredConcepts = requiredConcepts;
+    }
+
+}
--- core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/impl/ConstraintAnalysisImpl.java
+++ core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/impl/ConstraintAnalysisImpl.java
@@ -1,41 +1,22 @@
 package com.buschmais.jqassistant.core.analysis.impl;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.buschmais.jqassistant.core.analysis.api.ConstraintAnalysis;
-import com.buschmais.jqassistant.core.analysis.schema.v1.ConceptType;
-import com.buschmais.jqassistant.core.analysis.schema.v1.ConstraintGroupType;
-import com.buschmais.jqassistant.core.analysis.schema.v1.ConstraintType;
-import com.buschmais.jqassistant.core.analysis.schema.v1.QueryDefinitionType;
-import com.buschmais.jqassistant.core.analysis.schema.v1.ReferenceableType;
-import com.buschmais.jqassistant.core.analysis.schema.v1.Rules;
+import com.buschmais.jqassistant.core.analysis.api.*;
+import com.buschmais.jqassistant.core.analysis.api.model.Concept;
+import com.buschmais.jqassistant.core.analysis.api.model.Constraint;
+import com.buschmais.jqassistant.core.analysis.api.model.ConstraintGroup;
+import com.buschmais.jqassistant.core.analysis.schema.v1.*;
 import com.buschmais.jqassistant.store.api.Store;
 
-public class ConstraintAnalysisImpl implements ConstraintAnalysis {
+import java.util.*;
 
-	private final Map<String, QueryDefinitionType> queryDefinitions = new HashMap<String, QueryDefinitionType>();
-	private final Map<String, ConceptType> concepts = new HashMap<String, ConceptType>();
-	private final Map<String, ConstraintType> constraints = new HashMap<String, ConstraintType>();
-	private final Map<String, ConstraintGroupType> constraintGroups = new HashMap<String, ConstraintGroupType>();
+public class ConstraintAnalysisImpl implements ConstraintAnalyzer {
 
-	@Override
-	public void validateConstraints(Store store, List<Rules> rules) {
-		for (Rules rule : rules) {
-			cacheReferencables(rule.getQueryDefinition(), queryDefinitions);
-			cacheReferencables(rule.getConcept(), concepts);
-			cacheReferencables(rule.getConstraint(), constraints);
-			cacheReferencables(rule.getConstraintGroup(), constraintGroups);
-		}
-		for (ConceptType concept : concepts.values()) {
-		}
-	}
+    private final Map<String, Concept> concepts = new HashMap<String, Concept>();
+    private final Map<String, Constraint> constraints = new HashMap<String, Constraint>();
+    private final Map<String, ConstraintGroup> constraintGroups = new HashMap<String, ConstraintGroup>();
 
-	private <T extends ReferenceableType> void cacheReferencables(List<T> list,
-			Map<String, T> map) {
-		for (T t : list) {
-			map.put(t.getId(), t);
-		}
-	}
+    @Override
+    public void validateConstraints(Store store, List<ConstraintGroup> constraintGroups) {
+
+    }
 }
