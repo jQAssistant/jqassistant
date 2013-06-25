49ec0379c4af6b3e84b9067e4887482866eafa00
Dirk Mahler <dirk.mahler@buschmais.com>
2013-06-25 14:51:07 +0200

    - fixed compilation error - added concepts/constraints

---
+++ test/javaee6/src/test/java/com/buschmais/jqassistant/test/javaee6/logic/PersonDAOImplTest.java
@@ -0,0 +1,18 @@
+package com.buschmais.jqassistant.test.javaee6.logic;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dirk.mahler
+ * Date: 25.06.13
+ * Time: 11:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class PersonDAOImplTest {
+
+    @Test
+    public void testCreate() {
+
+    }
+}
