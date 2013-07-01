a5472d02f71287555bfe81c161118fe9cb47c010
Dirk Mahler <dirk.mahler@buschmais.com>
2013-07-01 08:02:49 +0200

    - fixed a NPE in EmbeddedGraphStore - moved default rules file directory to src/jqassistant (maven plugin)

--- core/store/src/main/java/com/buschmais/jqassistant/store/impl/EmbeddedGraphStore.java
+++ core/store/src/main/java/com/buschmais/jqassistant/store/impl/EmbeddedGraphStore.java
@@ -1,5 +1,6 @@
 package com.buschmais.jqassistant.store.impl;
 
+import com.buschmais.jqassistant.store.api.Store;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
@@ -7,72 +8,68 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.tooling.GlobalGraphOperations;
 
-import com.buschmais.jqassistant.store.api.Store;
-
 /**
  * {@link Store} implementation using an embedded Neo4j instance.
  */
 public class EmbeddedGraphStore extends AbstractGraphStore {
 
-	/**
-	 * The directory of the database.
-	 */
-	private final String databaseDirectory;
+    /**
+     * The directory of the database.
+     */
+    private final String databaseDirectory;
 
-	/**
-	 * The current {@link Transaction}.
-	 */
-	private Transaction transaction = null;
+    /**
+     * The current {@link Transaction}.
+     */
+    private Transaction transaction = null;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param databaseDirectory
-	 *            The directory of the database.
-	 */
-	public EmbeddedGraphStore(String databaseDirectory) {
-		this.databaseDirectory = databaseDirectory;
-	}
+    /**
+     * Constructor.
+     *
+     * @param databaseDirectory The directory of the database.
+     */
+    public EmbeddedGraphStore(String databaseDirectory) {
+        this.databaseDirectory = databaseDirectory;
+    }
 
-	@Override
-	protected GraphDatabaseService startDatabase() {
-		return new GraphDatabaseFactory()
-				.newEmbeddedDatabase(databaseDirectory);
-	}
+    @Override
+    protected GraphDatabaseService startDatabase() {
+        return new GraphDatabaseFactory().newEmbeddedDatabase(databaseDirectory);
+    }
 
-	@Override
-	protected void stopDatabase(GraphDatabaseService database) {
-		database.shutdown();
-	}
+    @Override
+    protected void stopDatabase(GraphDatabaseService database) {
+        if (database != null) {
+            database.shutdown();
+        }
+    }
 
-	@Override
-	public void beginTransaction() {
-		if (transaction != null) {
-			throw new IllegalStateException(
-					"There is already an existing transaction.");
-		}
-		transaction = database.beginTx();
-	}
+    @Override
+    public void beginTransaction() {
+        if (transaction != null) {
+            throw new IllegalStateException("There is already an existing transaction.");
+        }
+        transaction = database.beginTx();
+    }
 
-	@Override
-	public void endTransaction() {
-		super.endTransaction();
-		if (transaction == null) {
-			throw new IllegalStateException("There is no existing transaction.");
-		}
-		transaction.success();
-		transaction.finish();
-		transaction = null;
-	}
+    @Override
+    public void endTransaction() {
+        super.endTransaction();
+        if (transaction == null) {
+            throw new IllegalStateException("There is no existing transaction.");
+        }
+        transaction.success();
+        transaction.finish();
+        transaction = null;
+    }
 
-	@Override
-	public void reset() {
-		for (Relationship relationShip : GlobalGraphOperations.at(database)
-				.getAllRelationships()) {
-			relationShip.delete();
-		}
-		for (Node node : GlobalGraphOperations.at(database).getAllNodes()) {
-			node.delete();
-		}
-	}
+    @Override
+    public void reset() {
+        for (Relationship relationShip : GlobalGraphOperations.at(database).getAllRelationships()) {
+            relationShip.delete();
+        }
+        for (Node node : GlobalGraphOperations.at(database).getAllNodes()) {
+            node.delete();
+        }
+    }
 }
