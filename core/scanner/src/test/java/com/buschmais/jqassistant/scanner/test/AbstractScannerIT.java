6e01c366caa420c0ef3dd37938f38854886cc1e7
Dirk Mahler <dirk.mahler@buschmais.com>
2013-07-23 08:13:13 +0200

    fix broken continuous build

--- core/scanner/src/test/java/com/buschmais/jqassistant/scanner/test/AbstractScannerIT.java
+++ core/scanner/src/test/java/com/buschmais/jqassistant/scanner/test/AbstractScannerIT.java
@@ -6,6 +6,8 @@ import com.buschmais.jqassistant.store.impl.EmbeddedGraphStore;
 import org.junit.After;
 import org.junit.Before;
 
+import java.io.IOException;
+
 public abstract class AbstractScannerIT {
 
     protected Store store;
@@ -27,8 +29,27 @@ public abstract class AbstractScannerIT {
         store.stop();
     }
 
+    /**
+     * Return the {@link com.buschmais.jqassistant.scanner.ClassScanner.ScanListener} to be used for scanning.
+     * <p>The default implementation returns a listener without any functionality, a class may override this method to return a listener implementing specific behavior.</p>
+     *
+     * @return The {@link com.buschmais.jqassistant.scanner.ClassScanner.ScanListener}.
+     */
     protected ClassScanner.ScanListener getScanListener() {
-        return new ClassScanner.ScanListener() {};
+        return new ClassScanner.ScanListener() {
+        };
+    }
+
+    /**
+     * Scans the given classes.
+     *
+     * @param classes The classes.
+     * @throws IOException If scanning fails.
+     */
+    protected void scanClasses(Class<?>... classes) throws IOException {
+        store.beginTransaction();
+        scanner.scanClasses(classes);
+        store.endTransaction();
     }
 
 }
---
+++ core/scanner/src/test/java/com/buschmais/jqassistant/scanner/test/AnonymousInnerClassIT.java
@@ -0,0 +1,36 @@
+package com.buschmais.jqassistant.scanner.test;
+
+import com.buschmais.jqassistant.scanner.test.matcher.ClassDescriptorMatcher;
+import com.buschmais.jqassistant.scanner.test.sets.innerclass.AnonymousInnerClass;
+import com.buschmais.jqassistant.store.api.model.ClassDescriptor;
+import com.buschmais.jqassistant.store.api.model.QueryResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.buschmais.jqassistant.scanner.test.matcher.ClassDescriptorMatcher.classDescriptor;
+import static org.junit.Assert.assertThat;
+
+public class AnonymousInnerClassIT extends AbstractScannerIT {
+
+    @Test
+    public void outerClass() throws IOException {
+        scanClasses(AnonymousInnerClass.class);
+        QueryResult result = store.executeQuery("MATCH (outerClass:CLASS) RETURN c");
+        Iterable<QueryResult.Row> rows = result.getRows();
+        QueryResult.Row row = rows.iterator().next();
+        ClassDescriptor outerClass = row.get("outerClass");
+        assertThat(outerClass, classDescriptor(AnonymousInnerClass.class));
+    }
+
+    @Test
+    public void innerClass() throws IOException {
+        String resourceName = "/" + AnonymousInnerClass.class.getName().replace(".", "/") + "$1.class";
+        InputStream is = AnonymousInnerClassIT.class.getResourceAsStream(resourceName);
+        store.beginTransaction();
+        scanner.scanInputStream(is, resourceName);
+        store.endTransaction();
+    }
+}
