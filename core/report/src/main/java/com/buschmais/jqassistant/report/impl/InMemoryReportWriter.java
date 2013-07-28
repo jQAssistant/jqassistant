8ee9411748add1109483677fc5a53729811412a4
Dirk Mahler <dirk.mahler@buschmais.com>
2013-07-28 16:18:30 +0200

    #10 implemented XML report writer

--- core/report/src/main/java/com/buschmais/jqassistant/report/impl/InMemoryReportWriter.java
+++ core/report/src/main/java/com/buschmais/jqassistant/report/impl/InMemoryReportWriter.java
@@ -1,7 +1,8 @@
 package com.buschmais.jqassistant.report.impl;
 
-import com.buschmais.jqassistant.report.api.ReportWriter;
 import com.buschmais.jqassistant.core.model.api.*;
+import com.buschmais.jqassistant.report.api.ReportWriter;
+import com.buschmais.jqassistant.report.api.ReportWriterException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,41 +20,41 @@ public class InMemoryReportWriter implements ReportWriter {
     private Result currentResult;
 
     @Override
-    public void begin() {
+    public void begin() throws ReportWriterException {
     }
 
     @Override
-    public void end() {
+    public void end() throws ReportWriterException {
     }
 
     @Override
-    public void beginConcept(Concept concept) {
+    public void beginConcept(Concept concept) throws ReportWriterException {
     }
 
     @Override
-    public void endConcept() {
+    public void endConcept() throws ReportWriterException {
         addResult(this.conceptResults);
     }
 
     @Override
-    public void beginConstraintGroup(ConstraintGroup constraintGroup) {
+    public void beginConstraintGroup(ConstraintGroup constraintGroup) throws ReportWriterException {
     }
 
     @Override
-    public void endConstraintGroup() {
+    public void endConstraintGroup() throws ReportWriterException {
     }
 
     @Override
-    public void beginConstraint(Constraint constraint) {
+    public void beginConstraint(Constraint constraint) throws ReportWriterException {
     }
 
     @Override
-    public void endConstraint() {
+    public void endConstraint() throws ReportWriterException {
         addResult(this.constraintViolations);
     }
 
     @Override
-    public void setResult(Result result) {
+    public void setResult(Result result) throws ReportWriterException {
         this.currentResult = result;
     }
 
