282e71e42e3d987eff913aadd6c700772411e209
Dirk Mahler <dirk.mahler@buschmais.com>
2013-08-29 17:44:03 +0200

    - implemented decoding of node collections returned by Cypher queries into descriptor collections

--- core/scanner/src/test/java/com/buschmais/jqassistant/core/scanner/test/ArrayIT.java
+++ core/scanner/src/test/java/com/buschmais/jqassistant/core/scanner/test/ArrayIT.java
@@ -20,9 +20,9 @@ public class ArrayIT extends AbstractScannerIT {
         scanClasses(Array.class);
         Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("className", Array.class.getName());
-        TestResult testResult = executeQuery("MATCH (t:TYPE)-[:CONTAINS]->(f:FIELD) WHERE t.FQN={className} RETURN f", parameters);
+        TestResult testResult = query("MATCH (t:TYPE)-[:CONTAINS]->(f:FIELD) WHERE t.FQN={className} RETURN f", parameters);
         assertThat(testResult.getColumn("f"), hasItem(fieldDescriptor(Array.class, "stringArray")));
-        testResult = executeQuery("MATCH (t:TYPE)-[:CONTAINS]->(m:METHOD) WHERE t.FQN={className} RETURN m", parameters);
+        testResult = query("MATCH (t:TYPE)-[:CONTAINS]->(m:METHOD) WHERE t.FQN={className} RETURN m", parameters);
         assertThat(testResult.getColumn("m"), allOf(hasItem(methodDescriptor(Array.class, "getStringArray")), hasItem(methodDescriptor(Array.class, "getStringArray"))));
     }
 }
