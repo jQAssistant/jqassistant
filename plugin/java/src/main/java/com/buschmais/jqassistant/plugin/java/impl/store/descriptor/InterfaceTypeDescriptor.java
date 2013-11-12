df1a93c9dad4eaa62814c9904822b0db6bf9f49c
Dirk Mahler <dirk.mahler@buschmais.com>
2013-11-12 16:41:19 +0100

    #28 renamed wrong label  for InterfaceTypeDescriptor.java

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/InterfaceTypeDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/InterfaceTypeDescriptor.java
@@ -5,6 +5,6 @@ import com.buschmais.cdo.neo4j.api.annotation.Label;
 /**
  * Denotes an interface type.
  */
-@Label("ITNERFACE")
+@Label("INTERFACE")
 public interface InterfaceTypeDescriptor extends TypeDescriptor{
 }
