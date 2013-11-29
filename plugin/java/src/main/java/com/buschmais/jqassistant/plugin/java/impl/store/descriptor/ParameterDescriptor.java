add3dda59fa5e226e072072b53e49cf30de54df7
Dirk Mahler <dirk.mahler@buschmais.com>
2013-11-29 16:38:04 +0100

    - removed FQN from field and method descriptors

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/ParameterDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/ParameterDescriptor.java
@@ -1,6 +1,7 @@
 package com.buschmais.jqassistant.plugin.java.impl.store.descriptor;
 
 import com.buschmais.cdo.neo4j.api.annotation.Label;
+import com.buschmais.cdo.neo4j.api.annotation.Property;
 import com.buschmais.jqassistant.core.store.api.descriptor.FullQualifiedNameDescriptor;
 
 /**
@@ -8,4 +9,10 @@ import com.buschmais.jqassistant.core.store.api.descriptor.FullQualifiedNameDesc
  */
 @Label(value = "PARAMETER")
 public interface ParameterDescriptor extends FullQualifiedNameDescriptor, DependentDescriptor, AnnotatedDescriptor {
+
+	@Property("INDEX")
+	public int getIndex();
+
+	public void setIndex(int index);
+
 }
