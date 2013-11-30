03c841bbc13a14a275e671f9c4da1b49b35b5295
Dirk Mahler <dirk.mahler@buschmais.com>
2013-11-30 20:08:31 +0100

    - applied changes of CDO API

--- core/store/src/main/java/com/buschmais/jqassistant/core/store/impl/EmbeddedGraphStore.java
+++ core/store/src/main/java/com/buschmais/jqassistant/core/store/impl/EmbeddedGraphStore.java
@@ -3,7 +3,10 @@ package com.buschmais.jqassistant.core.store.impl;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Properties;
 
+import com.buschmais.cdo.api.bootstrap.CdoUnit;
 import org.neo4j.kernel.GraphDatabaseAPI;
 
 import com.buschmais.cdo.api.CdoManager;
@@ -17,39 +20,38 @@ import com.buschmais.jqassistant.core.store.api.Store;
  */
 public class EmbeddedGraphStore extends AbstractGraphStore {
 
-	/**
-	 * The directory of the database.
-	 */
-	private final String databaseDirectory;
+    /**
+     * The directory of the database.
+     */
+    private final String databaseDirectory;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param databaseDirectory
-	 *            The directory of the database.
-	 */
-	public EmbeddedGraphStore(String databaseDirectory) {
-		this.databaseDirectory = databaseDirectory;
-	}
+    /**
+     * Constructor.
+     *
+     * @param databaseDirectory The directory of the database.
+     */
+    public EmbeddedGraphStore(String databaseDirectory) {
+        this.databaseDirectory = databaseDirectory;
+    }
 
-	@Override
-	protected GraphDatabaseAPI getDatabaseAPI(CdoManager cdoManager) {
-		return (GraphDatabaseAPI) ((Neo4jCdoManager) cdoManager).getGraphDatabaseService();
-	}
+    @Override
+    protected GraphDatabaseAPI getDatabaseAPI(CdoManager cdoManager) {
+        return (GraphDatabaseAPI) ((Neo4jCdoManager) cdoManager).getGraphDatabaseService();
+    }
 
-	@Override
-	protected CdoManagerFactory createCdoManagerFactory(Collection<Class<?>> types) {
-		File database = new File(databaseDirectory);
-		try {
-			return new EmbeddedNeo4jCdoManagerFactoryImpl(database.toURI().toURL(), types.toArray(new Class<?>[0]));
-		} catch (MalformedURLException e) {
-			throw new IllegalArgumentException("Cannot create CdoManagerFactory.", e);
-		}
-	}
+    @Override
+    protected CdoManagerFactory createCdoManagerFactory(Collection<Class<?>> types) {
+        File database = new File(databaseDirectory);
+        try {
+            return new EmbeddedNeo4jCdoManagerFactoryImpl(new CdoUnit(null, null, database.toURI().toURL(), null, new HashSet<>(types), CdoUnit.ValidationMode.NONE, new Properties()));
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("Cannot create CdoManagerFactory.", e);
+        }
+    }
 
-	@Override
-	protected void closeCdoManagerFactory(CdoManagerFactory cdoManagerFactory) {
-		cdoManagerFactory.close();
-	}
+    @Override
+    protected void closeCdoManagerFactory(CdoManagerFactory cdoManagerFactory) {
+        cdoManagerFactory.close();
+    }
 
 }
