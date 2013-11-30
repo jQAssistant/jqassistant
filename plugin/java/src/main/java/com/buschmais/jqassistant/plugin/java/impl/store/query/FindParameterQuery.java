12109ecc3321894f60f660bfadd39fb63d3cd3b2
Michael Hunger <github@jexp.de>
2013-11-30 18:23:34 +0100

    changed queries to syntax that actually does node-by-id lookups and not full scans with filter on equality.

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/query/FindParameterQuery.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/query/FindParameterQuery.java
@@ -3,7 +3,7 @@ package com.buschmais.jqassistant.plugin.java.impl.store.query;
 import com.buschmais.cdo.neo4j.api.annotation.Cypher;
 import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.ParameterDescriptor;
 
-@Cypher("match (m:METHOD)-[:HAS]->(p:PARAMETER) where m={method} and p.INDEX={index} return p as parameter")
+@Cypher("match (m:METHOD)-[:HAS]->(p:PARAMETER) where id(m)=id({method}) and p.INDEX={index} return p as parameter")
 public interface FindParameterQuery {
 
 	ParameterDescriptor getParameter();
