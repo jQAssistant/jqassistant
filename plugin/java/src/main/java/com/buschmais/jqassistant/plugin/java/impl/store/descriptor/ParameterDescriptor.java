1f1a983b7abdea8c4027d3a8cf7261548cc58862
Dirk Mahler <dirk.mahler@buschmais.com>
2013-12-02 13:01:48 +0100

    -applied latest CDO changes

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/ParameterDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/ParameterDescriptor.java
@@ -2,13 +2,12 @@ package com.buschmais.jqassistant.plugin.java.impl.store.descriptor;
 
 import com.buschmais.cdo.neo4j.api.annotation.Label;
 import com.buschmais.cdo.neo4j.api.annotation.Property;
-import com.buschmais.jqassistant.core.store.api.descriptor.FullQualifiedNameDescriptor;
 
 /**
  * Describes a parameter of a method.
  */
 @Label(value = "PARAMETER")
-public interface ParameterDescriptor extends FullQualifiedNameDescriptor, DependentDescriptor, AnnotatedDescriptor {
+public interface ParameterDescriptor extends DependentDescriptor, AnnotatedDescriptor {
 
 	@Property("INDEX")
 	public int getIndex();
