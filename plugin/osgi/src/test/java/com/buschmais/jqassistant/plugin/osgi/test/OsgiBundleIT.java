7d5d98326ef93551076832ab88b35e4b31a54b72
nachtgold <kai.zimmermann@robotron.de>
2014-01-11 00:39:23 +0100

    #35 add final concept of unused public classes within bundles

--- plugin/osgi/src/test/java/com/buschmais/jqassistant/plugin/osgi/test/OsgiBundleIT.java
+++ plugin/osgi/src/test/java/com/buschmais/jqassistant/plugin/osgi/test/OsgiBundleIT.java
@@ -3,17 +3,23 @@ package com.buschmais.jqassistant.plugin.osgi.test;
 import com.buschmais.jqassistant.core.analysis.api.AnalyzerException;
 import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
 import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.PackageDescriptor;
+import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.osgi.test.api.data.Request;
 import com.buschmais.jqassistant.plugin.osgi.test.api.service.Service;
+import com.buschmais.jqassistant.plugin.osgi.test.impl.Activator;
+import com.buschmais.jqassistant.plugin.osgi.test.impl.ServiceImpl;
+
 import org.junit.Test;
 
 import javax.validation.constraints.NotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
 import static com.buschmais.jqassistant.plugin.java.test.matcher.PackageDescriptorMatcher.packageDescriptor;
+import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertThat;
@@ -71,6 +77,24 @@ public class OsgiBundleIT extends AbstractPluginIT {
         List<PackageDescriptor> packages = query("MATCH (b:OSGI:BUNDLE)-[:IMPORTS]->(p:PACKAGE) RETURN p").getColumn("p");
         assertThat(packages.size(), equalTo(1));
         assertThat(packages, hasItems(packageDescriptor(NotNull.class.getPackage())));
+        store.commitTransaction();
+    }
+
+    /**
+     * Verifies the concept "osgi-bundle:PublicClassesWithUninvokedMethodsInUnexportedPackages".
+     *
+     * @throws IOException       If the test fails.
+     * @throws AnalyzerException If the test fails.
+     */
+    @Test
+    public void publicClassesWithUninvokedMethodsInUnexportedPackages() throws IOException, AnalyzerException {
+        scanURLs(getManifestUrls());
+        scanClassesDirectory(Service.class);
+        applyConcept("osgi-bundle:PublicClassesWithUninvokedMethodsInUnexportedPackages");
+        store.beginTransaction();
+        List<TypeDescriptor> classes = query("MATCH (c:TYPE:CLASS:UNUSED) RETURN c").getColumn("c");
+        assertThat(classes.size(), equalTo(2));
+        assertThat(classes, hasItems(typeDescriptor(Activator.class), typeDescriptor(ServiceImpl.class)));
         store.commitTransaction();
     }
 
