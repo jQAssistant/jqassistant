a6e833178502c125e6c0856f663850a99da4e4f3
Dirk Mahler <dirk.mahler@buschmais.com>
2014-01-22 16:41:45 +0100

    #35 added concept for internal types and constraint for public internal types

--- core/analysis/src/test/java/com/buschmais/jqassistant/core/analysis/test/matcher/ResultMatcher.java
+++ core/analysis/src/test/java/com/buschmais/jqassistant/core/analysis/test/matcher/ResultMatcher.java
@@ -1,56 +1,86 @@
 package com.buschmais.jqassistant.core.analysis.test.matcher;
 
+import com.buschmais.jqassistant.core.analysis.api.Result;
+import com.buschmais.jqassistant.core.analysis.api.rule.AbstractExecutable;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
-import com.buschmais.jqassistant.core.analysis.api.Result;
-import com.buschmais.jqassistant.core.analysis.api.rule.AbstractExecutable;
+import java.util.Map;
 
 /**
  * A matcher for {@link Result}s.
  */
 public class ResultMatcher<E extends AbstractExecutable> extends TypeSafeMatcher<Result<E>> {
 
-	private Matcher<? extends AbstractExecutable> executableMatcher;
+    private Matcher<? extends AbstractExecutable> executableMatcher;
+    private Matcher<? super Iterable<? super Map<?, ?>>> rowsMatcher;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param executableMatcher
-	 *            The expected executable type.
-	 */
-	protected ResultMatcher(Matcher<? extends AbstractExecutable> executableMatcher) {
-		this.executableMatcher = executableMatcher;
-	}
+    /**
+     * Constructor.
+     *
+     * @param executableMatcher The expected executable type.
+     */
+    protected ResultMatcher(Matcher<? extends AbstractExecutable> executableMatcher, Matcher<? super Iterable<? super Map<?, ?>>> rowsMatcher) {
+        this.executableMatcher = executableMatcher;
+        this.rowsMatcher = rowsMatcher;
+    }
 
-	@Override
-	public boolean matchesSafely(Result item) {
-		return this.executableMatcher.matches(item.getExecutable());
-	}
+    /**
+     * Constructor.
+     *
+     * @param executableMatcher The expected executable type.
+     */
+    protected ResultMatcher(Matcher<? extends AbstractExecutable> executableMatcher) {
+        this(executableMatcher, null);
+    }
 
-	@Override
-	public void describeTo(Description description) {
-		description.appendText("Result(");
-		executableMatcher.describeTo(description);
-		description.appendText(")");
-	}
+    @Override
+    public boolean matchesSafely(Result item) {
+        return (this.executableMatcher.matches(item.getExecutable()) && (rowsMatcher == null) || rowsMatcher.matches(item.getRows()));
+    }
 
-	/**
-	 * Return a {@link ResultMatcher}.
-	 * 
-	 * @param constraintMatcher
-	 *            The matcher for the expected constraint.
-	 * @return The {@link ResultMatcher}.
-	 */
-	public static <E extends AbstractExecutable> Matcher<? super Result<E>> result(Matcher<E> constraintMatcher) {
-		return new ResultMatcher(constraintMatcher);
-	}
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("Result(");
+        executableMatcher.describeTo(description);
+        if (rowsMatcher != null) {
+            description.appendText("(rows=");
+            rowsMatcher.describeTo(description);
+            description.appendText(")");
+        }
+        description.appendText(")");
+    }
 
-	@Override
-	protected void describeMismatchSafely(Result item, Description mismatchDescription) {
-		mismatchDescription.appendText("Result(");
-		executableMatcher.describeMismatch(item.getExecutable(), mismatchDescription);
-		mismatchDescription.appendText(")");
-	}
+    @Override
+    protected void describeMismatchSafely(Result item, Description mismatchDescription) {
+        mismatchDescription.appendText("Result(");
+        if (!executableMatcher.matches(item.getExecutable())) {
+            executableMatcher.describeMismatch(item.getExecutable(), mismatchDescription);
+        }
+        if (rowsMatcher != null && !rowsMatcher.matches(item.getRows())) {
+            rowsMatcher.describeMismatch(item.getRows(), mismatchDescription);
+        }
+        mismatchDescription.appendText(")");
+    }
+
+    /**
+     * Return a {@link ResultMatcher}.
+     *
+     * @param constraintMatcher The matcher for the expected constraint.
+     * @return The {@link ResultMatcher}.
+     */
+    public static <E extends AbstractExecutable> Matcher<? super Result<E>> result(Matcher<E> constraintMatcher) {
+        return new ResultMatcher<E>(constraintMatcher);
+    }
+
+    /**
+     * Return a {@link ResultMatcher}.
+     *
+     * @param constraintMatcher The matcher for the expected constraint.
+     * @return The {@link ResultMatcher}.
+     */
+    public static <E extends AbstractExecutable> Matcher<? super Result<E>> result(Matcher<E> constraintMatcher, Matcher<? super Iterable<? super Map<?, ?>>> rowsMatcher) {
+        return new ResultMatcher<E>(constraintMatcher, rowsMatcher);
+    }
 }
