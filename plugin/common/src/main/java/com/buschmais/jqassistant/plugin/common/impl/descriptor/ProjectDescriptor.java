d440530bbeb58ecf5e5791b24a1c470f3ed2cb43
kai.zimmermann <kai.zimmermann@robotron.de>
2014-01-23 17:19:14 +0100

    Feierabendcheckin

---
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/impl/descriptor/ProjectDescriptor.java
@@ -0,0 +1,7 @@
+package com.buschmais.jqassistant.plugin.common.impl.descriptor;
+
+import com.buschmais.jqassistant.core.store.api.descriptor.Descriptor;
+
+public interface ProjectDescriptor extends Descriptor {
+
+}
