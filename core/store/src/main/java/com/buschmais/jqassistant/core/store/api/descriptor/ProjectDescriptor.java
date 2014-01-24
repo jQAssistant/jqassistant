72c3f1a94ed68c6acd71c9e0cea3cfa77a08d789
kai.zimmermann <kai.zimmermann@robotron.de>
2014-01-24 15:23:40 +0100

    #1 documentation added

--- core/store/src/main/java/com/buschmais/jqassistant/core/store/api/descriptor/ProjectDescriptor.java
+++ core/store/src/main/java/com/buschmais/jqassistant/core/store/api/descriptor/ProjectDescriptor.java
@@ -3,6 +3,9 @@ package com.buschmais.jqassistant.core.store.api.descriptor;
 import java.io.File;
 import java.util.Collection;
 
+/**
+ * Base interface for all project descriptors.
+ */
 public interface ProjectDescriptor extends Descriptor {
 
 	public Collection<? extends File> getAdditionalFiles();
