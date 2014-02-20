930659f061b00c51c9eb704fc783a43d5ac6a579
Dirk Mahler <dirk.mahler@buschmais.com>
2014-02-20 16:34:12 +0100

    #44  fix export of required concepts

--- maven/jqassistant-maven-plugin/src/it/singlemodule/export-rules/verify.groovy
+++ maven/jqassistant-maven-plugin/src/it/singlemodule/export-rules/verify.groovy
@@ -1,1 +1,6 @@
-assert new File(basedir, 'target/jqassistant/jqassistant-rules.xml').exists()
+def rulesFile = new File(basedir, 'target/jqassistant/jqassistant-rules.xml')
+assert rulesFile.exists()
+def rulesNode = new XmlSlurper().parse(rulesFile)
+assert rulesNode.concept.size() == 8
+assert rulesNode.constraint.size() == 2
+assert rulesNode.group.size() == 1
