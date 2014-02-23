70d8dd36311dd7a5facbecb59a0a3e934cfbb45a
Dirk Mahler <dirk.mahler@buschmais.com>
2014-02-23 18:08:27 +0100

    #45 moved caching of type descriptors from store to class visitor helper

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/ClassScannerPlugin.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/ClassScannerPlugin.java
@@ -1,6 +1,5 @@
 package com.buschmais.jqassistant.plugin.java.impl.scanner;
 
-import com.buschmais.jqassistant.core.store.api.Store;
 import com.buschmais.jqassistant.plugin.common.impl.scanner.AbstractFileScannerPlugin;
 import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.java.impl.store.resolver.DescriptorResolverFactory;
@@ -16,8 +15,12 @@ import java.io.IOException;
  */
 public class ClassScannerPlugin extends AbstractFileScannerPlugin<TypeDescriptor> {
 
+    private VisitorHelper visitorHelper;
+
     @Override
     protected void initialize() {
+        DescriptorResolverFactory resolverFactory = new DescriptorResolverFactory(getStore());
+        this.visitorHelper = new VisitorHelper(getStore(), resolverFactory);
     }
 
     @Override
@@ -27,9 +30,7 @@ public class ClassScannerPlugin extends AbstractFileScannerPlugin<TypeDescriptor
 
     @Override
     public TypeDescriptor scanFile(StreamSource streamSource) throws IOException {
-        Store store = getStore();
-        DescriptorResolverFactory resolverFactory = new DescriptorResolverFactory(store);
-        ClassVisitor visitor = new ClassVisitor(new VisitorHelper(store, resolverFactory));
+        ClassVisitor visitor = new ClassVisitor(visitorHelper);
         new ClassReader(streamSource.getInputStream()).accept(visitor, 0);
         return visitor.getTypeDescriptor();
     }
