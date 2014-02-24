6cd44b1ce29ca90c772afa6d8df2d72e3ff51047
Dirk Mahler <dirk.mahler@buschmais.com>
2014-02-24 09:04:15 +0100

    #45 prevent recursive scanning of junit report directories

--- core/scanner/src/main/java/com/buschmais/jqassistant/core/scanner/api/FileScanner.java
+++ core/scanner/src/main/java/com/buschmais/jqassistant/core/scanner/api/FileScanner.java
@@ -11,43 +11,44 @@ import com.buschmais.jqassistant.core.store.api.descriptor.Descriptor;
  */
 public interface FileScanner {
 
-	/**
-	 * Scans an archive,e .g. JAR file.
-	 * 
-	 * @param archive
-	 *            The archive.
-	 * @throws IOException
-	 *             If scanning fails.
-	 */
-	Iterable<Descriptor> scanArchive(File archive) throws IOException;
+    /**
+     * Scans an archive,e .g. JAR file.
+     *
+     * @param archive The archive.
+     * @throws IOException If scanning fails.
+     */
+    Iterable<Descriptor> scanArchive(File archive) throws IOException;
 
-	/**
-	 * Scans a directory.
-	 * 
-	 * @param directory
-	 *            The directory.
-	 * @throws IOException
-	 *             If Scanning fails.
-	 */
-	Iterable<Descriptor> scanDirectory(File directory) throws IOException;
+    /**
+     * Scans a directory recursively.
+     *
+     * @param directory The directory.
+     * @throws IOException If Scanning fails.
+     */
+    Iterable<Descriptor> scanDirectory(File directory) throws IOException;
 
-	/**
-	 * Scans the given classes.
-	 * 
-	 * @param classes
-	 *            The classes.
-	 * @throws IOException
-	 *             If Scanning fails.
-	 */
-	Iterable<Descriptor> scanClasses(Class<?>... classes) throws IOException;
+    /**
+     * Scans a directory.
+     *
+     * @param directory The directory.
+     * @param recursive if <code>true</code> sub directories will be scanned recursively.
+     * @throws IOException If Scanning fails.
+     */
+    Iterable<Descriptor> scanDirectory(File directory, boolean recursive) throws IOException;
 
-	/**
-	 * Scans the given URLs.
-	 * 
-	 * @param urls
-	 *            The URLs.
-	 * @throws IOException
-	 *             If Scanning fails.
-	 */
-	Iterable<Descriptor> scanURLs(URL... urls) throws IOException;
+    /**
+     * Scans the given classes.
+     *
+     * @param classes The classes.
+     * @throws IOException If Scanning fails.
+     */
+    Iterable<Descriptor> scanClasses(Class<?>... classes) throws IOException;
+
+    /**
+     * Scans the given URLs.
+     *
+     * @param urls The URLs.
+     * @throws IOException If Scanning fails.
+     */
+    Iterable<Descriptor> scanURLs(URL... urls) throws IOException;
 }
