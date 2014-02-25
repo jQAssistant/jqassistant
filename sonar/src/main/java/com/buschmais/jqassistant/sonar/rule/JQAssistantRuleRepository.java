729f61e7e52e85830c1be9f8d3576b22b7030f32
Dirk Mahler <dirk.mahler@buschmais.com>
2014-02-25 09:25:49 +0100

    #44 add rules from plugins to rule repository

--- sonar/src/main/java/com/buschmais/jqassistant/sonar/rule/JQAssistantRuleRepository.java
+++ sonar/src/main/java/com/buschmais/jqassistant/sonar/rule/JQAssistantRuleRepository.java
@@ -1,13 +1,23 @@
 package com.buschmais.jqassistant.sonar.rule;
 
+import com.buschmais.jqassistant.core.analysis.api.RuleSetReader;
+import com.buschmais.jqassistant.core.analysis.api.rule.Concept;
+import com.buschmais.jqassistant.core.analysis.api.rule.RuleSet;
+import com.buschmais.jqassistant.core.analysis.impl.RuleSetReaderImpl;
+import com.buschmais.jqassistant.core.pluginmanager.api.PluginManager;
+import com.buschmais.jqassistant.core.pluginmanager.impl.PluginManagerImpl;
 import com.buschmais.jqassistant.sonar.JQAssistant;
 import org.sonar.api.resources.Java;
 import org.sonar.api.rules.AnnotationRuleParser;
 import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.RuleParam;
 import org.sonar.api.rules.RuleRepository;
 
+import javax.xml.transform.Source;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public final class JQAssistantRuleRepository extends RuleRepository {
 
@@ -23,6 +33,20 @@ public final class JQAssistantRuleRepository extends RuleRepository {
 
     @Override
     public List<Rule> createRules() {
-        return annotationRuleParser.parse(JQAssistant.KEY, RULE_CLASSES);
+        List<Rule> rules = new ArrayList<Rule>();
+        PluginManager pluginManager = new PluginManagerImpl();
+        List<Source> ruleSources = pluginManager.getRuleSources();
+        RuleSetReader ruleSetReader = new RuleSetReaderImpl();
+        RuleSet ruleSet = ruleSetReader.read(ruleSources);
+        Map<String, Concept> concepts = ruleSet.getConcepts();
+        for (Concept concept : concepts.values()) {
+            Rule conceptRule = Rule.create(JQAssistant.KEY, concept.getId(), concept.getId());
+            conceptRule.setDescription(concept.getDescription());
+            RuleParam cypher = conceptRule.createParameter("cypher");
+            cypher.setDefaultValue(concept.getQuery().getCypher());
+            rules.add(conceptRule);
+        }
+        rules.addAll(annotationRuleParser.parse(JQAssistant.KEY, RULE_CLASSES));
+        return rules;
     }
 }
