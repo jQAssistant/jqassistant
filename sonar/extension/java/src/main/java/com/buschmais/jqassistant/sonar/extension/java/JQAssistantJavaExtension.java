4129687e33596ba8c828c2e31bb0f1891019808a
Dirk Mahler <dirk.mahler@buschmais.com>
2014-02-27 09:06:10 +0100

    #44 refactored module structure to enable extensions

---
+++ sonar/extension/java/src/main/java/com/buschmais/jqassistant/sonar/extension/java/JQAssistantJavaExtension.java
@@ -0,0 +1,18 @@
+package com.buschmais.jqassistant.sonar.extension.java;
+
+import org.sonar.api.SonarPlugin;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Dirk Mahler on 26.02.14.
+ */
+public class JQAssistantJavaExtension extends SonarPlugin {
+
+    @Override
+    public List getExtensions() {
+        return Collections.emptyList();
+    }
+
+}
