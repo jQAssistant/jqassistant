3fb6fc6e023a4331322e1e1c7e6f781dc29067df
Dirk Mahler <dirk.mahler@buschmais.com>
2014-03-11 15:41:49 +0100

    #49 added a concept for JPA named queries

--- plugin/jpa2/src/test/java/com/buschmais/jqassistant/plugin/jpa2/test/Jpa2IT.java
+++ plugin/jpa2/src/test/java/com/buschmais/jqassistant/plugin/jpa2/test/Jpa2IT.java
@@ -1,18 +1,5 @@
 package com.buschmais.jqassistant.plugin.jpa2.test;
 
-import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
-import static com.buschmais.jqassistant.plugin.java.test.matcher.ValueDescriptorMatcher.valueDescriptor;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.junit.Assert.assertThat;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-
-import org.hamcrest.Matcher;
-import org.junit.Test;
-
 import com.buschmais.jqassistant.core.analysis.api.AnalyzerException;
 import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
 import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.PropertyDescriptor;
@@ -20,78 +7,104 @@ import com.buschmais.jqassistant.plugin.jpa2.impl.store.descriptor.PersistenceDe
 import com.buschmais.jqassistant.plugin.jpa2.impl.store.descriptor.PersistenceUnitDescriptor;
 import com.buschmais.jqassistant.plugin.jpa2.test.matcher.PersistenceUnitMatcher;
 import com.buschmais.jqassistant.plugin.jpa2.test.set.entity.JpaEntity;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
+import static com.buschmais.jqassistant.plugin.java.test.matcher.ValueDescriptorMatcher.valueDescriptor;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tests for the JPA concepts.
  */
 public class Jpa2IT extends AbstractPluginIT {
 
-	/**
-	 * Verifies the concept "entity:Entity".
-	 *
-	 * @throws java.io.IOException
-	 *             If the test fails.
-	 * @throws AnalyzerException
-	 *             If the test fails.
-	 */
-	@Test
-	public void jpaEntity() throws IOException, AnalyzerException {
-		scanClasses(JpaEntity.class);
-		applyConcept("jpa2:Entity");
+    /**
+     * Verifies the concept "jpa2:Entity".
+     *
+     * @throws java.io.IOException If the test fails.
+     * @throws AnalyzerException   If the test fails.
+     */
+    @Test
+    public void entity() throws IOException, AnalyzerException {
+        scanClasses(JpaEntity.class);
+        applyConcept("jpa2:Entity");
         store.beginTransaction();
         assertThat(query("MATCH (e:TYPE:ENTITY) RETURN e").getColumn("e"), hasItem(typeDescriptor(JpaEntity.class)));
         store.commitTransaction();
-	}
+    }
 
-	/**
-	 * Verifies scanning of model descriptors.
-	 *
-	 * @throws java.io.IOException
-	 *             If the test fails.
-	 * @throws AnalyzerException
-	 *             If the test fails.
-	 */
-	@Test
-	public void persistenceDescriptor() throws IOException, AnalyzerException {
-		scanClassesDirectory(JpaEntity.class);
+    /**
+     * Verifies the concept "jpa2:NamedQuery".
+     *
+     * @throws java.io.IOException If the test fails.
+     * @throws AnalyzerException   If the test fails.
+     */
+    @Test
+    public void namedQuery() throws IOException, AnalyzerException {
+        scanClasses(JpaEntity.class);
+        applyConcept("jpa2:NamedQuery");
         store.beginTransaction();
-		TestResult testResult = query("MATCH (p:JPA:PERSISTENCE) RETURN p");
-		assertThat(testResult.getRows().size(), equalTo(1));
-		List<? super PersistenceDescriptor> persistenceDescriptors = testResult.getColumn("p");
-		PersistenceDescriptor persistenceDescriptor = (PersistenceDescriptor) persistenceDescriptors.get(0);
-		assertThat(persistenceDescriptor.getVersion(), equalTo("2.0"));
-		Set<PersistenceUnitDescriptor> persistenceUnits = persistenceDescriptor.getContains();
-		assertThat(persistenceUnits, hasItem(PersistenceUnitMatcher.persistenceUnitDescriptor("persistence-unit")));
+        TestResult query = query("MATCH (e:ENTITY)-[:DEFINES]->(n:JPA2:NAMEDQUERY) RETURN n.NAME as name, n.QUERY as query");
+        List<Map<String, Object>> rows = query.getRows();
+        assertThat(rows.size(), equalTo(1));
+        Map<String, Object> row = rows.get(0);
+        assertThat((String) row.get("name"), equalTo(JpaEntity.TESTQUERY_NAME));
+        assertThat((String) row.get("query"), equalTo(JpaEntity.TESTQUERY_QUERY));
         store.commitTransaction();
-	}
+    }
 
-	/**
-	 * Verifies scanning of model unit descriptors.
-	 *
-	 * @throws java.io.IOException
-	 *             If the test fails.
-	 * @throws AnalyzerException
-	 *             If the test fails.
-	 */
-	@Test
-	public void persistenceUnitDescriptor() throws IOException, AnalyzerException {
-		scanClassesDirectory(JpaEntity.class);
+    /**
+     * Verifies scanning of model descriptors.
+     *
+     * @throws java.io.IOException If the test fails.
+     * @throws AnalyzerException   If the test fails.
+     */
+    @Test
+    public void persistenceDescriptor() throws IOException, AnalyzerException {
+        scanClassesDirectory(JpaEntity.class);
         store.beginTransaction();
-		TestResult testResult = query("MATCH (pu:JPA:PERSISTENCEUNIT) RETURN pu");
-		assertThat(testResult.getRows().size(), equalTo(1));
-		List<? super PersistenceUnitDescriptor> persistenceUnitDescriptors = testResult.getColumn("pu");
-		PersistenceUnitDescriptor persistenceUnitDescriptor = (PersistenceUnitDescriptor) persistenceUnitDescriptors.get(0);
-		assertThat(persistenceUnitDescriptor.getName(), equalTo("persistence-unit"));
-		assertThat(persistenceUnitDescriptor.getDescription(), equalTo("description"));
-		assertThat(persistenceUnitDescriptor.getJtaDataSource(), equalTo("jtaDataSource"));
-		assertThat(persistenceUnitDescriptor.getNonJtaDataSource(), equalTo("nonJtaDataSource"));
-		assertThat(persistenceUnitDescriptor.getProvider(), equalTo("provider"));
-		assertThat(persistenceUnitDescriptor.getValidationMode(), equalTo("AUTO"));
-		assertThat(persistenceUnitDescriptor.getSharedCacheMode(), equalTo("ENABLE_SELECTIVE"));
-		assertThat(persistenceUnitDescriptor.getContains(), hasItem(typeDescriptor(JpaEntity.class)));
-		Matcher<? super PropertyDescriptor> valueMatcher = (Matcher<? super PropertyDescriptor>) valueDescriptor("stringProperty",
-				equalTo("stringValue"));
-		assertThat(persistenceUnitDescriptor.getProperties(), hasItem(valueMatcher));
+        TestResult testResult = query("MATCH (p:JPA:PERSISTENCE) RETURN p");
+        assertThat(testResult.getRows().size(), equalTo(1));
+        List<? super PersistenceDescriptor> persistenceDescriptors = testResult.getColumn("p");
+        PersistenceDescriptor persistenceDescriptor = (PersistenceDescriptor) persistenceDescriptors.get(0);
+        assertThat(persistenceDescriptor.getVersion(), equalTo("2.0"));
+        Set<PersistenceUnitDescriptor> persistenceUnits = persistenceDescriptor.getContains();
+        assertThat(persistenceUnits, hasItem(PersistenceUnitMatcher.persistenceUnitDescriptor("persistence-unit")));
         store.commitTransaction();
-	}
+    }
+
+    /**
+     * Verifies scanning of model unit descriptors.
+     *
+     * @throws java.io.IOException If the test fails.
+     * @throws AnalyzerException   If the test fails.
+     */
+    @Test
+    public void persistenceUnitDescriptor() throws IOException, AnalyzerException {
+        scanClassesDirectory(JpaEntity.class);
+        store.beginTransaction();
+        TestResult testResult = query("MATCH (pu:JPA:PERSISTENCEUNIT) RETURN pu");
+        assertThat(testResult.getRows().size(), equalTo(1));
+        List<? super PersistenceUnitDescriptor> persistenceUnitDescriptors = testResult.getColumn("pu");
+        PersistenceUnitDescriptor persistenceUnitDescriptor = (PersistenceUnitDescriptor) persistenceUnitDescriptors.get(0);
+        assertThat(persistenceUnitDescriptor.getName(), equalTo("persistence-unit"));
+        assertThat(persistenceUnitDescriptor.getDescription(), equalTo("description"));
+        assertThat(persistenceUnitDescriptor.getJtaDataSource(), equalTo("jtaDataSource"));
+        assertThat(persistenceUnitDescriptor.getNonJtaDataSource(), equalTo("nonJtaDataSource"));
+        assertThat(persistenceUnitDescriptor.getProvider(), equalTo("provider"));
+        assertThat(persistenceUnitDescriptor.getValidationMode(), equalTo("AUTO"));
+        assertThat(persistenceUnitDescriptor.getSharedCacheMode(), equalTo("ENABLE_SELECTIVE"));
+        assertThat(persistenceUnitDescriptor.getContains(), hasItem(typeDescriptor(JpaEntity.class)));
+        Matcher<? super PropertyDescriptor> valueMatcher = valueDescriptor("stringProperty", equalTo("stringValue"));
+        assertThat(persistenceUnitDescriptor.getProperties(), hasItem(valueMatcher));
+        store.commitTransaction();
+    }
 }
--- plugin/jpa2/src/test/java/com/buschmais/jqassistant/plugin/jpa2/test/set/entity/JpaEntity.java
+++ plugin/jpa2/src/test/java/com/buschmais/jqassistant/plugin/jpa2/test/set/entity/JpaEntity.java
@@ -1,10 +1,17 @@
 package com.buschmais.jqassistant.plugin.jpa2.test.set.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * A JPA entity
  */
 @Entity
+@NamedQueries(@NamedQuery(name = JpaEntity.TESTQUERY_NAME, query = JpaEntity.TESTQUERY_QUERY))
 public class JpaEntity {
+
+    public static final String TESTQUERY_NAME = "testQuery";
+    public static final String TESTQUERY_QUERY = "SELECT e FROM JpaEntity e";
+
 }
