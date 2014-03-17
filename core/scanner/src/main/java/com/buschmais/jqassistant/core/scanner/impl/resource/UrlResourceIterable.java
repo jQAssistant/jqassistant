22f95f3e385b6b2d29354c43dbac4b453addb635
Dirk Mahler <dirk.mahler@buschmais.com>
2014-03-17 14:52:33 +0100

    #42 clean up resource scanning

---
+++ core/scanner/src/main/java/com/buschmais/jqassistant/core/scanner/impl/resource/UrlResourceIterable.java
@@ -0,0 +1,51 @@
+package com.buschmais.jqassistant.core.scanner.impl.resource;
+
+import com.buschmais.jqassistant.core.scanner.api.FileScannerPlugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collection;
+
+/**
+ * A resource iterable for processing an array of URLs.
+ */
+public class UrlResourceIterable extends AbstractResourceIterable<URL> {
+    private final URL[] urls;
+    int index;
+
+    public UrlResourceIterable(Collection<FileScannerPlugin> plugins, URL... urls) {
+        super(plugins);
+        this.urls = urls;
+        index = 0;
+    }
+
+    @Override
+    protected boolean hasNextResource() {
+        return index < urls.length;
+    }
+
+    @Override
+    protected URL nextResource() {
+        return urls[index++];
+    }
+
+    @Override
+    protected boolean isDirectory(URL resource) {
+        return false;
+    }
+
+    @Override
+    protected String getName(URL resource) {
+        return resource.getPath();
+    }
+
+    @Override
+    protected InputStream openInputStream(String fileName, URL resource) throws IOException {
+        return resource.openStream();
+    }
+
+    @Override
+    protected void close() {
+    }
+}
