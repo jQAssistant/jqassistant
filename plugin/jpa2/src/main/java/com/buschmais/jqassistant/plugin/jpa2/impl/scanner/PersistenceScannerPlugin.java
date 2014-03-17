53b24fa7e0390d15b309d4094109cf4a10475d20
Dirk Mahler <dirk.mahler@buschmais.com>
2014-03-17 16:20:55 +0100

    #52 fixed NPE for optional elements in persistence.xml

--- plugin/jpa2/src/main/java/com/buschmais/jqassistant/plugin/jpa2/impl/scanner/PersistenceScannerPlugin.java
+++ plugin/jpa2/src/main/java/com/buschmais/jqassistant/plugin/jpa2/impl/scanner/PersistenceScannerPlugin.java
@@ -9,6 +9,8 @@ import com.buschmais.jqassistant.plugin.jpa2.impl.store.descriptor.PersistenceDe
 import com.buschmais.jqassistant.plugin.jpa2.impl.store.descriptor.PersistenceUnitDescriptor;
 import com.sun.java.xml.ns.persistence.ObjectFactory;
 import com.sun.java.xml.ns.persistence.Persistence;
+import com.sun.java.xml.ns.persistence.PersistenceUnitCachingType;
+import com.sun.java.xml.ns.persistence.PersistenceUnitValidationModeType;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -24,67 +26,73 @@ import static com.sun.java.xml.ns.persistence.Persistence.PersistenceUnit.Proper
  */
 public class PersistenceScannerPlugin extends AbstractFileScannerPlugin {
 
-	private JAXBContext jaxbContext;
+    private JAXBContext jaxbContext;
 
     private DescriptorResolverFactory descriptorResolverFactory;
 
     @Override
     protected void initialize() {
-		try {
-			jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
-		} catch (JAXBException e) {
-			throw new IllegalStateException("Cannot create JAXB context.", e);
-		}
+        try {
+            jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+        } catch (JAXBException e) {
+            throw new IllegalStateException("Cannot create JAXB context.", e);
+        }
         descriptorResolverFactory = new DescriptorResolverFactory(getStore());
-	}
+    }
 
-	@Override
-	public boolean matches(String file, boolean isDirectory) {
-		return "META-INF/persistence.xml".equals(file) || "WEB-INF/persistence.xml".equals(file);
-	}
+    @Override
+    public boolean matches(String file, boolean isDirectory) {
+        return "META-INF/persistence.xml".equals(file) || "WEB-INF/persistence.xml".equals(file);
+    }
 
-	@Override
-	public PersistenceDescriptor scanFile(StreamSource streamSource) throws IOException {
-		Persistence persistence;
-		try {
-			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-			persistence = unmarshaller.unmarshal(streamSource, Persistence.class).getValue();
-		} catch (JAXBException e) {
-			throw new IOException("Cannot read model descriptor.", e);
-		}
+    @Override
+    public PersistenceDescriptor scanFile(StreamSource streamSource) throws IOException {
+        Persistence persistence;
+        try {
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            persistence = unmarshaller.unmarshal(streamSource, Persistence.class).getValue();
+        } catch (JAXBException e) {
+            throw new IOException("Cannot read model descriptor.", e);
+        }
         Store store = getStore();
         PersistenceDescriptor persistenceDescriptor = store.create(PersistenceDescriptor.class);
-		persistenceDescriptor.setName(streamSource.getSystemId());
-		persistenceDescriptor.setVersion(persistence.getVersion());
-		// Create model units
-		for (PersistenceUnit persistenceUnit : persistence.getPersistenceUnit()) {
-			PersistenceUnitDescriptor persistenceUnitDescriptor = store.create(PersistenceUnitDescriptor.class);
-			persistenceUnitDescriptor.setName(persistenceUnit.getName());
-			persistenceUnitDescriptor.setDescription(persistenceUnit.getDescription());
-			persistenceUnitDescriptor.setJtaDataSource(persistenceUnit.getJtaDataSource());
-			persistenceUnitDescriptor.setNonJtaDataSource(persistenceUnit.getNonJtaDataSource());
-			persistenceUnitDescriptor.setProvider(persistenceUnit.getProvider());
-			persistenceUnitDescriptor.setValidationMode(persistenceUnit.getValidationMode().name());
-			persistenceUnitDescriptor.setSharedCacheMode(persistenceUnit.getSharedCacheMode().name());
-			for (String clazz : persistenceUnit.getClazz()) {
-				TypeDescriptor typeDescriptor = descriptorResolverFactory.getTypeDescriptorResolver().resolve(clazz);
-				persistenceUnitDescriptor.getContains().add(typeDescriptor);
-			}
-			// Create model unit properties
-			for (Property property : persistenceUnit.getProperties().getProperty()) {
-				PropertyDescriptor propertyDescriptor = store.create(PropertyDescriptor.class);
-				propertyDescriptor.setName(property.getName());
-				propertyDescriptor.setValue(property.getValue());
-				persistenceUnitDescriptor.getProperties().add(propertyDescriptor);
-			}
-			// Add model unit to model descriptor
-			persistenceDescriptor.getContains().add(persistenceUnitDescriptor);
-		}
-		return persistenceDescriptor;
-	}
+        persistenceDescriptor.setName(streamSource.getSystemId());
+        persistenceDescriptor.setVersion(persistence.getVersion());
+        // Create model units
+        for (PersistenceUnit persistenceUnit : persistence.getPersistenceUnit()) {
+            PersistenceUnitDescriptor persistenceUnitDescriptor = store.create(PersistenceUnitDescriptor.class);
+            persistenceUnitDescriptor.setName(persistenceUnit.getName());
+            persistenceUnitDescriptor.setDescription(persistenceUnit.getDescription());
+            persistenceUnitDescriptor.setJtaDataSource(persistenceUnit.getJtaDataSource());
+            persistenceUnitDescriptor.setNonJtaDataSource(persistenceUnit.getNonJtaDataSource());
+            persistenceUnitDescriptor.setProvider(persistenceUnit.getProvider());
+            PersistenceUnitValidationModeType validationMode = persistenceUnit.getValidationMode();
+            if (validationMode != null) {
+                persistenceUnitDescriptor.setValidationMode(validationMode.name());
+            }
+            PersistenceUnitCachingType sharedCacheMode = persistenceUnit.getSharedCacheMode();
+            if (sharedCacheMode != null) {
+                persistenceUnitDescriptor.setSharedCacheMode(sharedCacheMode.name());
+            }
+            for (String clazz : persistenceUnit.getClazz()) {
+                TypeDescriptor typeDescriptor = descriptorResolverFactory.getTypeDescriptorResolver().resolve(clazz);
+                persistenceUnitDescriptor.getContains().add(typeDescriptor);
+            }
+            // Create model unit properties
+            for (Property property : persistenceUnit.getProperties().getProperty()) {
+                PropertyDescriptor propertyDescriptor = store.create(PropertyDescriptor.class);
+                propertyDescriptor.setName(property.getName());
+                propertyDescriptor.setValue(property.getValue());
+                persistenceUnitDescriptor.getProperties().add(propertyDescriptor);
+            }
+            // Add model unit to model descriptor
+            persistenceDescriptor.getContains().add(persistenceUnitDescriptor);
+        }
+        return persistenceDescriptor;
+    }
 
-	@Override
-	public PersistenceDescriptor scanDirectory(String name) throws IOException {
-		return null;
-	}
+    @Override
+    public PersistenceDescriptor scanDirectory(String name) throws IOException {
+        return null;
+    }
 }
