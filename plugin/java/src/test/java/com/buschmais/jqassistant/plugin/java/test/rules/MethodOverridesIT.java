776ec5b700e2da80b2a7e42aedd23a36766d22d8
Dirk Mahler <dirk.mahler@buschmais.com>
2014-03-21 18:28:25 +0100

    #43 do not create overrides for private methods

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/MethodOverridesIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/MethodOverridesIT.java
@@ -5,12 +5,14 @@ import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
 import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.MethodDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.set.rules.java.ClassType;
 import com.buschmais.jqassistant.plugin.java.test.set.rules.java.InterfaceType;
+import com.buschmais.jqassistant.plugin.java.test.set.rules.java.SubClassType;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.constructorDescriptor;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -21,7 +23,7 @@ import static org.junit.Assert.assertThat;
 public class MethodOverridesIT extends AbstractPluginIT {
 
     /**
-     * Verifies the concept "java:MethodOverrides".
+     * Verifies the concept "java:MethodOverrides" for a class implementing an interface.
      *
      * @throws IOException       If the test fails.
      * @throws AnalyzerException If the test fails.
@@ -40,6 +42,32 @@ public class MethodOverridesIT extends AbstractPluginIT {
         Map<String, Object> row1 = rows.get(1);
         assertThat((MethodDescriptor) row1.get("method"), methodDescriptor(ClassType.class, "doSomething", String.class));
         assertThat((MethodDescriptor) row1.get("otherMethod"), methodDescriptor(InterfaceType.class, "doSomething", String.class));
+        store.commitTransaction();
+    }
+
+    /**
+     * Verifies the concept "java:MethodOverrides" for a class implementing an interface.
+     *
+     * @throws IOException       If the test fails.
+     * @throws AnalyzerException If the test fails.
+     */
+    @Test
+    public void methodOverridesSubClass() throws IOException, AnalyzerException, NoSuchMethodException {
+        scanClasses(ClassType.class, SubClassType.class);
+        applyConcept("java:MethodOverrides");
+        store.beginTransaction();
+        TestResult result = query("MATCH (method:METHOD)-[:OVERRIDES]->(otherMethod:METHOD) RETURN method, otherMethod ORDER BY method.SIGNATURE");
+        List<Map<String, Object>> rows = result.getRows();
+        assertThat(rows.size(), equalTo(3));
+        Map<String, Object> row0 = rows.get(0);
+        assertThat((MethodDescriptor) row0.get("method"), constructorDescriptor(SubClassType.class));
+        assertThat((MethodDescriptor) row0.get("otherMethod"), constructorDescriptor(ClassType.class));
+        Map<String, Object> row1 = rows.get(1);
+        assertThat((MethodDescriptor) row1.get("method"), methodDescriptor(ClassType.class, "doSomething", int.class));
+        assertThat((MethodDescriptor) row1.get("otherMethod"), methodDescriptor(InterfaceType.class, "doSomething", int.class));
+        Map<String, Object> row2 = rows.get(2);
+        assertThat((MethodDescriptor) row2.get("method"), methodDescriptor(ClassType.class, "doSomething", String.class));
+        assertThat((MethodDescriptor) row2.get("otherMethod"), methodDescriptor(InterfaceType.class, "doSomething", String.class));
         store.commitTransaction();
     }
 }
