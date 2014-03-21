409095d9217bce9ae6e56ed1d34c2cc91464bbb7
Dirk Mahler <dirk.mahler@buschmais.com>
2014-03-21 16:12:27 +0100

    - upgrade to latest cdo snapshot

--- plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/matcher/ArtifactDescriptorMatcher.java
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/matcher/ArtifactDescriptorMatcher.java
@@ -1,33 +1,30 @@
 package com.buschmais.jqassistant.plugin.common.test.matcher;
 
-import org.hamcrest.Matcher;
-
+import com.buschmais.jqassistant.core.scanner.api.descriptor.ArtifactDescriptor;
 import com.buschmais.jqassistant.core.store.test.matcher.AbstractDescriptorMatcher;
-import com.buschmais.jqassistant.plugin.common.impl.descriptor.ArtifactDescriptor;
+import org.hamcrest.Matcher;
 
 /**
  * A matcher for {@link ArtifactDescriptor}s.
  */
 public class ArtifactDescriptorMatcher extends AbstractDescriptorMatcher<ArtifactDescriptor> {
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param id
-	 *            The expected artifact id.
-	 */
-	protected ArtifactDescriptorMatcher(String id) {
-		super(ArtifactDescriptor.class, id);
-	}
+    /**
+     * Constructor.
+     *
+     * @param id The expected artifact id.
+     */
+    protected ArtifactDescriptorMatcher(String id) {
+        super(ArtifactDescriptor.class, id);
+    }
 
-	/**
-	 * Return a {@link ArtifactDescriptorMatcher}.
-	 * 
-	 * @param id
-	 *            The artifact id.
-	 * @return The {@link ArtifactDescriptorMatcher}.
-	 */
-	public static Matcher<? super ArtifactDescriptor> artifactDescriptor(String id) {
-		return new ArtifactDescriptorMatcher(id);
-	}
+    /**
+     * Return a {@link ArtifactDescriptorMatcher}.
+     *
+     * @param id The artifact id.
+     * @return The {@link ArtifactDescriptorMatcher}.
+     */
+    public static Matcher<? super ArtifactDescriptor> artifactDescriptor(String id) {
+        return new ArtifactDescriptorMatcher(id);
+    }
 }
