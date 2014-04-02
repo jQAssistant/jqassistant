125ec68dc1cb7a42f65f6ca1a3c05e0d5157b18b
Dirk Mahler <dirk.mahler@buschmais.com>
2014-04-02 18:39:28 +0200

    - applied latest xo changes (instances as query parameters)

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/query/GetOrCreateMethodQuery.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/query/GetOrCreateMethodQuery.java
@@ -3,7 +3,7 @@ package com.buschmais.jqassistant.plugin.java.impl.store.query;
 import com.buschmais.jqassistant.plugin.java.impl.store.descriptor.MethodDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Cypher;
 
-@Cypher("match (t:TYPE) where id(t)=id({type}) create unique (t)-[:DECLARES]->(m:METHOD {SIGNATURE:{signature}}) return m as method")
+@Cypher("match (t:TYPE) where id(t)={type} create unique (t)-[:DECLARES]->(m:METHOD {SIGNATURE:{signature}}) return m as method")
 public interface GetOrCreateMethodQuery {
 
     MethodDescriptor getMethod();
