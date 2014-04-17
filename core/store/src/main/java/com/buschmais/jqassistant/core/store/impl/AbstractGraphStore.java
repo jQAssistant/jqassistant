63ff736440783a7420a949250880bdc7ed48e97b
Dirk Mahler <dirk.mahler@buschmais.com>
2014-04-17 15:33:44 +0200

    #51 prepare analyze shell command

--- core/store/src/main/java/com/buschmais/jqassistant/core/store/impl/AbstractGraphStore.java
+++ core/store/src/main/java/com/buschmais/jqassistant/core/store/impl/AbstractGraphStore.java
@@ -28,61 +28,61 @@ import static com.buschmais.xo.api.Query.Result.CompositeRowObject;
 public abstract class AbstractGraphStore implements Store {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractGraphStore.class);
-    private XOManagerFactory cdoManagerFactory;
-    private XOManager cdoManager;
+    private XOManagerFactory xoManagerFactory;
+    private XOManager xoManager;
 
     @Override
     public void start(Collection<Class<?>> types) {
-        cdoManagerFactory = createCdoManagerFactory(types);
-        cdoManager = cdoManagerFactory.createXOManager();
+        xoManagerFactory = createXOManagerFactory(types);
+        xoManager = xoManagerFactory.createXOManager();
     }
 
     @Override
     public void stop() {
-        if (cdoManager != null) {
-            if (cdoManager.currentTransaction().isActive()) {
+        if (xoManager != null) {
+            if (xoManager.currentTransaction().isActive()) {
                 LOGGER.warn("Rolling back an active transaction.");
-                cdoManager.currentTransaction().rollback();
+                xoManager.currentTransaction().rollback();
             }
-            cdoManager.close();
+            xoManager.close();
         }
-        if (cdoManagerFactory != null) {
-            closeCdoManagerFactory(cdoManagerFactory);
+        if (xoManagerFactory != null) {
+            closeXOManagerFactory(xoManagerFactory);
         }
     }
 
     @Override
     public <T extends Descriptor> T create(Class<T> type) {
-        T descriptor = cdoManager.create(type);
+        T descriptor = xoManager.create(type);
         return descriptor;
     }
 
     @Override
     public <T extends FullQualifiedNameDescriptor> T create(Class<T> type, String fullQualifiedName) {
-        T descriptor = cdoManager.create(type);
+        T descriptor = xoManager.create(type);
         descriptor.setFullQualifiedName(fullQualifiedName);
         return descriptor;
     }
 
     @Override
     public <T extends Descriptor, C extends T> C migrate(T descriptor, Class<C> concreteType) {
-        return cdoManager.migrate(descriptor, concreteType);
+        return xoManager.migrate(descriptor, concreteType);
     }
 
     @Override
     public <T extends Descriptor> T find(Class<T> type, String fullQualifiedName) {
-        ResultIterable<T> result = cdoManager.find(type, fullQualifiedName);
+        ResultIterable<T> result = xoManager.find(type, fullQualifiedName);
         return result.hasResult() ? result.getSingleResult() : null;
     }
 
     @Override
     public Result<CompositeRowObject> executeQuery(String query, Map<String, Object> parameters) {
-        return cdoManager.createQuery(query).withParameters(parameters).execute();
+        return xoManager.createQuery(query).withParameters(parameters).execute();
     }
 
     @Override
     public <Q> Result<Q> executeQuery(Class<Q> query, Map<String, Object> parameters) {
-        return cdoManager.createQuery(query).withParameters(parameters).execute();
+        return xoManager.createQuery(query).withParameters(parameters).execute();
     }
 
 
@@ -98,7 +98,7 @@ public abstract class AbstractGraphStore implements Store {
         boolean hasResult;
         do {
             beginTransaction();
-            Query<Long> deleteNodesAndRelQuery = cdoManager.createQuery(deleteNodesAndRels, Long.class);
+            Query<Long> deleteNodesAndRelQuery = xoManager.createQuery(deleteNodesAndRels, Long.class);
             hasResult = deleteNodesAndRelQuery.execute().getSingleResult() > 0;
             commitTransaction();
         } while (hasResult);
@@ -106,23 +106,23 @@ public abstract class AbstractGraphStore implements Store {
 
     @Override
     public void beginTransaction() {
-        cdoManager.currentTransaction().begin();
+        xoManager.currentTransaction().begin();
     }
 
     @Override
     public void commitTransaction() {
-        cdoManager.currentTransaction().commit();
+        xoManager.currentTransaction().commit();
     }
 
     @Override
     public void rollbackTransaction() {
-        cdoManager.currentTransaction().rollback();
+        xoManager.currentTransaction().rollback();
     }
 
     public GraphDatabaseAPI getDatabaseService() {
         beginTransaction();
         try {
-            return getDatabaseAPI(cdoManager);
+            return getDatabaseAPI(xoManager);
         } finally {
             commitTransaction();
         }
@@ -135,13 +135,13 @@ public abstract class AbstractGraphStore implements Store {
      *
      * @return The {@link GraphDatabaseService} instance to use.
      */
-    protected abstract XOManagerFactory createCdoManagerFactory(Collection<Class<?>> types);
+    protected abstract XOManagerFactory createXOManagerFactory(Collection<Class<?>> types);
 
     /**
      * Delegates to the sub class to stop the database.
      *
      * @param database The used {@link GraphDatabaseService} instance.
      */
-    protected abstract void closeCdoManagerFactory(XOManagerFactory database);
+    protected abstract void closeXOManagerFactory(XOManagerFactory database);
 
 }
