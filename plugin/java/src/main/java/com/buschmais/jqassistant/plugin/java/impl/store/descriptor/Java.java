d4234ca4f45dcffd86c76c52350ce3b5d1f55afe
Dirk Mahler <dirk.mahler@buschmais.com>
2014-04-28 18:46:22 +0200

    #56 prepare rendering of source code information in reports

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/Java.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/Java.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.buschmais.jqassistant.core.store.api.descriptor.Language;
+import com.buschmais.jqassistant.core.report.api.Language;
+import com.buschmais.jqassistant.core.report.api.LanguageElement;
+import com.buschmais.jqassistant.core.report.api.SourceProvider;
+import com.buschmais.jqassistant.core.store.api.descriptor.Descriptor;
 
 /**
  * Defines the language elements for "Java".
@@ -17,7 +20,151 @@ public @interface Java {
 
     JavaLanguageElement value();
 
-    enum JavaLanguageElement {
-        Package, Type, Field, Method;
+    public enum JavaLanguageElement implements LanguageElement {
+        Package {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new SourceProvider<PackageDescriptor>() {
+                    @Override
+                    public String getName(PackageDescriptor descriptor) {
+                        return descriptor.getFullQualifiedName();
+                    }
+
+                    @Override
+                    public String getSource(PackageDescriptor descriptor) {
+                        return descriptor.getFileName();
+                    }
+
+                    @Override
+                    public int[] getLineNumbers(PackageDescriptor descriptor) {
+                        return null;
+                    }
+                };
+            }
+        },
+        Type {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new SourceProvider<TypeDescriptor>() {
+                    @Override
+                    public String getName(TypeDescriptor descriptor) {
+                        return descriptor.getFullQualifiedName();
+                    }
+
+                    @Override
+                    public String getSource(TypeDescriptor descriptor) {
+                        return null;
+                    }
+
+                    @Override
+                    public int[] getLineNumbers(TypeDescriptor descriptor) {
+                        return new int[] { 1 };
+                    }
+                };
+            }
+        },
+        Field {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new MemberSourceDescriptor();
+            }
+        },
+        ReadField {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new SourceProvider<ReadsDescriptor>() {
+                    @Override
+                    public String getName(ReadsDescriptor descriptor) {
+                        return new MemberSourceDescriptor().getName(descriptor.getMethod());
+                    }
+
+                    @Override
+                    public String getSource(ReadsDescriptor descriptor) {
+                        return descriptor.getMethod().getDeclaringType().getFileName();
+                    }
+
+                    @Override
+                    public int[] getLineNumbers(ReadsDescriptor descriptor) {
+                        return descriptor.getLineNumbers();
+                    }
+                };
+            }
+        },
+        WriteField {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new SourceProvider<WritesDescriptor>() {
+                    @Override
+                    public String getName(WritesDescriptor descriptor) {
+                        return new MemberSourceDescriptor().getName(descriptor.getMethod());
+                    }
+
+                    @Override
+                    public String getSource(WritesDescriptor descriptor) {
+                        return descriptor.getMethod().getDeclaringType().getFileName();
+                    }
+
+                    @Override
+                    public int[] getLineNumbers(WritesDescriptor descriptor) {
+                        return descriptor.getLineNumbers();
+                    }
+                };
+            }
+        },
+        Method {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new MemberSourceDescriptor();
+            }
+        },
+        Constructor {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new MemberSourceDescriptor();
+            }
+        },
+        MethodInvocation {
+            @Override
+            public SourceProvider<? extends Descriptor> getSourceProvider() {
+                return new SourceProvider<InvokesDescriptor>() {
+                    @Override
+                    public String getName(InvokesDescriptor descriptor) {
+                        return new MemberSourceDescriptor().getName(descriptor.getInvokedMethod());
+                    }
+
+                    @Override
+                    public String getSource(InvokesDescriptor descriptor) {
+                        return descriptor.getInvokingMethod().getDeclaringType().getFileName();
+                    }
+
+                    @Override
+                    public int[] getLineNumbers(InvokesDescriptor descriptor) {
+                        return descriptor.getLineNumbers();
+                    }
+                };
+            }
+        };
+
+        /**
+         * {@link com.buschmais.jqassistant.core.report.api.SourceProvider}
+         * implementation for type members.
+         */
+        private static class MemberSourceDescriptor implements SourceProvider<MemberDescriptor> {
+            @Override
+            public String getName(MemberDescriptor descriptor) {
+                return descriptor.getDeclaringType().getFullQualifiedName() + "#" + descriptor.getSignature();
+            }
+
+            @Override
+            public String getSource(MemberDescriptor descriptor) {
+                return descriptor.getDeclaringType().getFileName();
+            }
+
+            @Override
+            public int[] getLineNumbers(MemberDescriptor descriptor) {
+                return null;
+            }
+        }
     }
+
 }
--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/ReadsDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/ReadsDescriptor.java
@@ -3,13 +3,14 @@ package com.buschmais.jqassistant.plugin.java.impl.store.descriptor;
 import static com.buschmais.xo.neo4j.api.annotation.Relation.Incoming;
 import static com.buschmais.xo.neo4j.api.annotation.Relation.Outgoing;
 
+import com.buschmais.jqassistant.core.store.api.descriptor.Descriptor;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
 /**
  * Defines a READs relation between a method and a field.
  */
 @Relation("READS")
-public interface ReadsDescriptor extends LineNumberDescriptor {
+public interface ReadsDescriptor extends Descriptor, LineNumberDescriptor {
 
     @Outgoing
     MethodDescriptor getMethod();
