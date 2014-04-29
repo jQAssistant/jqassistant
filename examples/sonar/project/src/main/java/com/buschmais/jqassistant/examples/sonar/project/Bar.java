572f733388dc3ca1e6468d86fb3922cf040d786f
Dirk Mahler <dirk.mahler@buschmais.com>
2014-04-29 17:23:45 +0200

    #56 minor cleanup

--- examples/sonar/project/src/main/java/com/buschmais/jqassistant/examples/sonar/project/Bar.java
+++ examples/sonar/project/src/main/java/com/buschmais/jqassistant/examples/sonar/project/Bar.java
@@ -1,8 +1,8 @@
 package com.buschmais.jqassistant.examples.sonar.project;
 
 /**
- * A class violating the example constraint defined in the jQAssistant Sonar
- * plugin extension.
+ * A class violating example constraints defined in the jQAssistant Sonar plugin
+ * extension.
  */
 public class Bar {
 
--- examples/sonar/project/src/main/java/com/buschmais/jqassistant/examples/sonar/project/Foo.java
+++ examples/sonar/project/src/main/java/com/buschmais/jqassistant/examples/sonar/project/Foo.java
@@ -1,8 +1,8 @@
 package com.buschmais.jqassistant.examples.sonar.project;
 
 /**
- * A class matching the example constraint defined in the jQAssistant Sonar
- * plugin extension.
+ * A class violating example constraints defined in the jQAssistant Sonar plugin
+ * extension.
  */
 public class Foo {
 
