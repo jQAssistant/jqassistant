02f9a2cd6a3d8e5ff017198147fb87784771b57a
Dirk Mahler <dirk.mahler@buschmais.com>
2014-06-12 20:27:37 +0200

    #68 fix build error

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/PackageDirectoryDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/store/descriptor/PackageDirectoryDescriptor.java
@@ -1,6 +1,9 @@
 package com.buschmais.jqassistant.plugin.java.impl.store.descriptor;
 
+import static com.buschmais.jqassistant.plugin.java.impl.store.descriptor.Java.JavaLanguageElement.Package;
+
 import com.buschmais.jqassistant.core.store.api.descriptor.FileDescriptor;
 
+@Java(Package)
 public interface PackageDirectoryDescriptor extends PackageDescriptor, FileDescriptor {
 }
