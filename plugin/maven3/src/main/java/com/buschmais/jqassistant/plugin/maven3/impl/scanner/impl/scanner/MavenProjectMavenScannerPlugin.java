c93cd2ed757b8ff235675427120d5a7367ee9ca6
Dirk Mahler <dirk.mahler@buschmais.com>
2014-06-16 16:06:23 +0200

    #68 cleanup of maven project scanner

--- plugin/maven3/src/main/java/com/buschmais/jqassistant/plugin/maven3/impl/scanner/impl/scanner/MavenProjectMavenScannerPlugin.java
+++ plugin/maven3/src/main/java/com/buschmais/jqassistant/plugin/maven3/impl/scanner/impl/scanner/MavenProjectMavenScannerPlugin.java
@@ -8,6 +8,7 @@ import static java.util.Collections.emptyList;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -59,42 +60,102 @@ public class MavenProjectMavenScannerPlugin extends AbstractMavenProjectScannerP
         return emptyList();
     }
 
+    /**
+     * Add project specific information.
+     * 
+     * @param project
+     *            The project.
+     * @param projectDescriptor
+     *            The project descriptor.
+     * @param mainArtifactDescriptor
+     *            The artifact descriptor representing the main artifact.
+     * @param testArtifactDescriptor
+     *            The artifact descriptor representing the test artifact.
+     */
     private void addProjectDetails(MavenProject project, MavenProjectDirectoryDescriptor projectDescriptor, ArtifactDescriptor mainArtifactDescriptor,
             ArtifactDescriptor testArtifactDescriptor) {
         Store store = getStore();
         store.beginTransaction();
         try {
-            MavenProject parent = project.getParent();
-            if (parent != null) {
-                MavenProjectDescriptor parentDescriptor = resolveProject(parent, MavenProjectDescriptor.class);
-                projectDescriptor.setParent(parentDescriptor);
-            }
-            if (mainArtifactDescriptor != null && testArtifactDescriptor != null) {
-                DependsOnDescriptor dependsOnDescriptor = store.create(testArtifactDescriptor, DependsOnDescriptor.class, mainArtifactDescriptor);
-                dependsOnDescriptor.setScope(Artifact.SCOPE_TEST);
-            }
-            for (MavenProject module : (List<MavenProject>) project.getCollectedProjects()) {
+            addParent(project, projectDescriptor);
+            addModules(project, projectDescriptor);
+            addDependencies(project, mainArtifactDescriptor, testArtifactDescriptor, store);
+        } finally {
+            store.commitTransaction();
+        }
+    }
+
+    /**
+     * Add the relation to the parent project.
+     * 
+     * @param project
+     *            The project.
+     * @param projectDescriptor
+     *            The project descriptor.
+     */
+    private void addParent(MavenProject project, MavenProjectDirectoryDescriptor projectDescriptor) {
+        MavenProject parent = project.getParent();
+        if (parent != null) {
+            MavenProjectDescriptor parentDescriptor = resolveProject(parent, MavenProjectDescriptor.class);
+            projectDescriptor.setParent(parentDescriptor);
+        }
+    }
+
+    /**
+     * Add relations to the modules.
+     * 
+     * @param project
+     *            The project.
+     * @param projectDescriptor
+     *            The project descriptor.
+     */
+    private void addModules(MavenProject project, MavenProjectDirectoryDescriptor projectDescriptor) {
+        File projectDirectory = project.getBasedir();
+        Set<File> modules = new HashSet<>();
+        for (String moduleName : (List<String>) project.getModules()) {
+            File module = new File(projectDirectory, moduleName);
+            modules.add(module);
+        }
+        for (MavenProject module : (List<MavenProject>) project.getCollectedProjects()) {
+            if (modules.contains(module.getBasedir())) {
                 MavenProjectDescriptor moduleDescriptor = resolveProject(module, MavenProjectDescriptor.class);
                 projectDescriptor.getModules().add(moduleDescriptor);
             }
-            for (Artifact dependency : (Set<Artifact>) project.getDependencyArtifacts()) {
-                ArtifactDescriptor dependencyDescriptor = resolveArtifact(dependency);
-                DependsOnDescriptor dependsOnDescriptor;
-                ArtifactDescriptor dependentDescriptor;
-                String scope = dependency.getScope();
-                if (Artifact.SCOPE_TEST.equals(scope)) {
-                    dependentDescriptor = testArtifactDescriptor;
-                } else {
-                    dependentDescriptor = mainArtifactDescriptor;
-                }
-                if (dependentDescriptor != null) {
-                    dependsOnDescriptor = store.create(dependentDescriptor, DependsOnDescriptor.class, dependencyDescriptor);
-                    dependsOnDescriptor.setScope(scope);
-                    dependsOnDescriptor.setOptional(dependency.isOptional());
-                }
+        }
+    }
+
+    /**
+     * Add dependency relations to the artifacts.
+     * 
+     * @param project
+     *            The project.
+     * @param mainArtifactDescriptor
+     *            The artifact descriptor representing the main artifact.
+     * @param testArtifactDescriptor
+     *            The artifact descriptor representing the test artifact.
+     * @param store
+     *            The store.
+     */
+    private void addDependencies(MavenProject project, ArtifactDescriptor mainArtifactDescriptor, ArtifactDescriptor testArtifactDescriptor, Store store) {
+        if (mainArtifactDescriptor != null && testArtifactDescriptor != null) {
+            DependsOnDescriptor dependsOnDescriptor = store.create(testArtifactDescriptor, DependsOnDescriptor.class, mainArtifactDescriptor);
+            dependsOnDescriptor.setScope(Artifact.SCOPE_TEST);
+        }
+        for (Artifact dependency : (Set<Artifact>) project.getDependencyArtifacts()) {
+            ArtifactDescriptor dependencyDescriptor = resolveArtifact(dependency);
+            DependsOnDescriptor dependsOnDescriptor;
+            ArtifactDescriptor dependentDescriptor;
+            String scope = dependency.getScope();
+            if (Artifact.SCOPE_TEST.equals(scope)) {
+                dependentDescriptor = testArtifactDescriptor;
+            } else {
+                dependentDescriptor = mainArtifactDescriptor;
             }
-        } finally {
-            store.commitTransaction();
+            if (dependentDescriptor != null) {
+                dependsOnDescriptor = store.create(dependentDescriptor, DependsOnDescriptor.class, dependencyDescriptor);
+                dependsOnDescriptor.setScope(scope);
+                dependsOnDescriptor.setOptional(dependency.isOptional());
+            }
         }
     }
 
