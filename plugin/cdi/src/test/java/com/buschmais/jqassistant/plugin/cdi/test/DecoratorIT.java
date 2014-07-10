ec059d65f9fb9323bbb7c2a5cbc5d707d16b806f
Dirk Mahler <dirk.mahler@buschmais.com>
2014-07-10 08:55:42 +0200

    #78 extracted decorator rules to separate file

---
+++ plugin/cdi/src/test/java/com/buschmais/jqassistant/plugin/cdi/test/DecoratorIT.java
@@ -0,0 +1,38 @@
+package com.buschmais.jqassistant.plugin.cdi.test;
+
+import static com.buschmais.jqassistant.plugin.java.test.matcher.FieldDescriptorMatcher.fieldDescriptor;
+import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.buschmais.jqassistant.core.analysis.api.AnalysisException;
+import com.buschmais.jqassistant.plugin.cdi.test.set.beans.decorator.DecoratorBean;
+import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
+
+/**
+ * Tests for the CDI concepts.
+ */
+public class DecoratorIT extends AbstractJavaPluginIT {
+
+    /**
+     * Verifies the concept "cdi:Decorator".
+     * 
+     * @throws java.io.IOException
+     *             If the test fails.
+     * @throws com.buschmais.jqassistant.core.analysis.api.AnalysisException
+     *             If the test fails.
+     */
+    @Test
+    public void decorator() throws IOException, AnalysisException, NoSuchMethodException, NoSuchFieldException {
+        scanClasses(DecoratorBean.class);
+        applyConcept("decorator:Decorator");
+        store.beginTransaction();
+        assertThat(query("MATCH (e:Decorator) RETURN e").getColumn("e"), hasItem(typeDescriptor(DecoratorBean.class)));
+        assertThat(query("MATCH (e:Field:Decorator:Delegate) RETURN e").getColumn("e"), hasItem(fieldDescriptor(DecoratorBean.class, "delegate")));
+        store.commitTransaction();
+    }
+}
