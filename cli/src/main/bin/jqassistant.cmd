f823f22582380311647e6943afa4f88345092dcf
Dirk Mahler <dirk.mahler@buschmais.com>
2014-07-19 08:05:42 +0200

    #102 prepare ci distribution

--- cli/src/main/bin/jqassistant.cmd
+++ cli/src/main/bin/jqassistant.cmd
@@ -1,2 +1,5 @@
 @echo off
-echo Not yet implemented.
+set BIN_DIR=%~dp0%
+set LIB_DIR=%BIN_DIR%\..\lib
+echo %LIB_DIR%
+java -jar %LIB_DIR%\${project.artifactId}-${project.version}.${project.packaging} %*
