813b3c843dc4d05b48646419845d440559ccf816
Jens Nerche <j.nerche@kontext-e.de>
2014-08-07 22:02:44 +0200

    Merge branch 'master' of https://github.com/buschmais/jqassistant
    Conflicts:
    	core/pluginmanager/src/main/java/com/buschmais/jqassistant/core/pluginmanager/impl/ScannerPluginRepositoryImpl.java
    	scm/cli/src/main/java/com/buschmais/jqassistant/scm/cli/AnalyzeTask.java

---
+++ core/plugin/src/main/java/com/buschmais/jqassistant/core/plugin/impl/ScannerPluginRepositoryImpl.java
@@ -0,0 +1,56 @@
+package com.buschmais.jqassistant.core.plugin.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.buschmais.jqassistant.core.plugin.api.PluginConfigurationReader;
+import com.buschmais.jqassistant.core.plugin.api.PluginRepositoryException;
+import com.buschmais.jqassistant.core.plugin.api.ScannerPluginRepository;
+import com.buschmais.jqassistant.core.plugin.schema.v1.JqassistantPlugin;
+import com.buschmais.jqassistant.core.plugin.schema.v1.ScannerType;
+import com.buschmais.jqassistant.core.scanner.api.ScannerPlugin;
+import com.buschmais.jqassistant.core.store.api.Store;
+
+/**
+ * Scanner plugin repository implementation.
+ */
+public class ScannerPluginRepositoryImpl extends AbstractPluginRepository implements ScannerPluginRepository {
+
+    private final List<ScannerPlugin<?>> scannerPlugins;
+
+    /**
+     * Constructor.
+     */
+    public ScannerPluginRepositoryImpl(PluginConfigurationReader pluginConfigurationReader, Store store, Map<String, Object> properties)
+            throws PluginRepositoryException {
+        List<JqassistantPlugin> plugins = pluginConfigurationReader.getPlugins();
+        this.scannerPlugins = getScannerPlugins(plugins, store, properties);
+    }
+
+    @Override
+    public List<ScannerPlugin<?>> getScannerPlugins() throws PluginRepositoryException {
+        return scannerPlugins;
+    }
+
+    private <T extends ScannerPlugin> List<T> getScannerPlugins(List<JqassistantPlugin> plugins, Store store, Map<String, Object> properties)
+            throws PluginRepositoryException {
+        List<T> scannerPlugins = new ArrayList<>();
+        for (JqassistantPlugin plugin : plugins) {
+            ScannerType scannerType = plugin.getScanner();
+            if (scannerType != null) {
+                for (String scannerPluginName : scannerType.getClazz()) {
+                    T scannerPlugin = createInstance(scannerPluginName);
+                    if (scannerPlugin != null) {
+                        // properties is mutable, so every plugin should get its
+                        // own copy
+                        scannerPlugin.initialize(store, new HashMap<>(properties));
+                        scannerPlugins.add(scannerPlugin);
+                    }
+                }
+            }
+        }
+        return scannerPlugins;
+    }
+}
