7dfb89fa08be42956c6e1899a0560561b20569bc
Dirk Mahler <dirk.mahler@buschmais.com>
2014-09-15 17:20:04 +0200

    #113 fix property for custom rule directory

--- maven/src/main/java/com/buschmais/jqassistant/scm/maven/ProjectResolver.java
+++ maven/src/main/java/com/buschmais/jqassistant/scm/maven/ProjectResolver.java
@@ -6,7 +6,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 
 /**
- * Resolver for base projects in a multi-module hierarchy.
+ * Resolver for root modules in a multi-module hierarchy.
  */
 public final class ProjectResolver {
 
@@ -37,6 +37,8 @@ public final class ProjectResolver {
      * 
      * @param module
      *            The current module.
+     * @param rulesDirectory
+     *            The name of the directory used for identifying the root module
      * @return The {@link MavenProject} containing a rules directory.
      * @throws MojoExecutionException
      *             If the directory cannot be resolved.
@@ -59,19 +61,29 @@ public final class ProjectResolver {
         throw new MojoExecutionException("Cannot resolve base directory.");
     }
 
-    static File getRulesDirectory(MavenProject currentProject, String rulesDirectory) {
-        return new File(currentProject.getBasedir().getAbsolutePath() + File.separator + rulesDirectory);
+    /**
+     * Returns the directory containing rules.
+     * 
+     * @param rootModule
+     *            The root module of the project.
+     * @param rulesDirectory
+     *            The name of the directory used for identifying the root
+     *            module.
+     * @return The file representing the directory.
+     */
+    static File getRulesDirectory(MavenProject rootModule, String rulesDirectory) {
+        return new File(rootModule.getBasedir().getAbsolutePath() + File.separator + rulesDirectory);
     }
 
     /**
      * Determines the directory for writing output files.
      * 
-     * @param baseProject
-     *            The base project.
+     * @param rootModule
+     *            The root module of the project.
      * @return The report directory.
      */
-    static File getOutputDirectory(MavenProject baseProject) {
-        String directoryName = baseProject.getBuild().getDirectory() + "/" + OUTPUT_DIRECTORY;
+    static File getOutputDirectory(MavenProject rootModule) {
+        String directoryName = rootModule.getBuild().getDirectory() + "/" + OUTPUT_DIRECTORY;
         File directory = new File(directoryName);
         directory.mkdirs();
         return directory;
@@ -80,21 +92,21 @@ public final class ProjectResolver {
     /**
      * Determines a report file name.
      * 
-     * @param baseProject
+     * @param rootModule
      *            The base project.
      * @param reportFile
      *            The report file as specified in the pom.xml file or on the
      *            command line.
      * @return The resolved {@link java.io.File}.
-     * @throws org.apache.maven.plugin.MojoExecutionException
+     * @throws MojoExecutionException
      *             If the file cannot be determined.
      */
-    static File getOutputFile(MavenProject baseProject, File reportFile, String defaultFile) throws MojoExecutionException {
+    static File getOutputFile(MavenProject rootModule, File reportFile, String defaultFile) throws MojoExecutionException {
         File selectedXmlReportFile;
         if (reportFile != null) {
             selectedXmlReportFile = reportFile;
-        } else if (baseProject != null) {
-            selectedXmlReportFile = new File(getOutputDirectory(baseProject) + "/" + defaultFile);
+        } else if (rootModule != null) {
+            selectedXmlReportFile = new File(getOutputDirectory(rootModule) + "/" + defaultFile);
         } else {
             throw new MojoExecutionException("Cannot determine report file.");
         }
