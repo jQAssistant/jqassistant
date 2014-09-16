a44a38c55e495cfac00180a66d4c36e6cb86df22
Dirk Mahler <dirk.mahler@buschmais.com>
2014-09-16 20:50:44 +0200

    #115 cache scanner plugins per item type

--- core/scanner/src/main/java/com/buschmais/jqassistant/core/scanner/impl/ScannerImpl.java
+++ core/scanner/src/main/java/com/buschmais/jqassistant/core/scanner/impl/ScannerImpl.java
@@ -1,10 +1,10 @@
 package com.buschmais.jqassistant.core.scanner.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Map;
 
 import com.buschmais.jqassistant.core.scanner.api.Scanner;
 import com.buschmais.jqassistant.core.scanner.api.ScannerListener;
@@ -17,11 +17,11 @@ import com.buschmais.jqassistant.core.store.api.type.FileDescriptor;
  */
 public class ScannerImpl implements Scanner {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ScannerImpl.class);
-
     private final List<ScannerPlugin<?>> scannerPlugins;
 
     private final ScannerListener scannerListener;
+
+    private final Map<Class<?>, List<ScannerPlugin<?>>> scannerPluginsPerType = new HashMap<>();
 
     /**
      * Constructor.
@@ -53,21 +53,41 @@ public class ScannerImpl implements Scanner {
     @Override
     public <I> FileDescriptor scan(final I item, final String path, final Scope scope) throws IOException {
         FileDescriptor fileDescriptor = null;
-        for (ScannerPlugin<?> scannerPlugin : scannerPlugins) {
-            Class<?> scannerPluginType = scannerPlugin.getType();
-            if (scannerPluginType.isAssignableFrom(item.getClass())) {
-                ScannerPlugin<I> selectedPlugin = (ScannerPlugin<I>) scannerPlugin;
-                if (selectedPlugin.accepts(item, path, scope)) {
-                    if (scannerListener != null) {
-                        scannerListener.before(item, path, scope);
-                    }
-                    fileDescriptor = selectedPlugin.scan(item, path, scope, this);
-                    if (scannerListener != null) {
-                        scannerListener.after(item, path, scope, fileDescriptor);
-                    }
+        Class<?> itemClass = item.getClass();
+        for (ScannerPlugin<?> scannerPlugin : getScannerPluginsForType(itemClass)) {
+            ScannerPlugin<I> selectedPlugin = (ScannerPlugin<I>) scannerPlugin;
+            if (selectedPlugin.accepts(item, path, scope)) {
+                if (scannerListener != null) {
+                    scannerListener.before(item, path, scope);
+                }
+                fileDescriptor = selectedPlugin.scan(item, path, scope, this);
+                if (scannerListener != null) {
+                    scannerListener.after(item, path, scope, fileDescriptor);
                 }
             }
         }
         return fileDescriptor;
+    }
+
+    /**
+     * Determine the list of scanner plugins that handle the given type.
+     * 
+     * @param type
+     *            The type.
+     * @return The list of plugins.
+     */
+    private List<ScannerPlugin<?>> getScannerPluginsForType(Class<?> type) {
+        List<ScannerPlugin<?>> plugins = scannerPluginsPerType.get(type);
+        if (plugins == null) {
+            plugins = new ArrayList<>();
+            for (ScannerPlugin<?> scannerPlugin : scannerPlugins) {
+                Class<?> scannerPluginType = scannerPlugin.getType();
+                if (scannerPluginType.isAssignableFrom(type)) {
+                    plugins.add(scannerPlugin);
+                }
+                scannerPluginsPerType.put(type, plugins);
+            }
+        }
+        return plugins;
     }
 }
