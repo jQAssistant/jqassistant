ab76e70a60e6911865fd6c0b65866b4cea1ffedd
Dirk Mahler <dirk.mahler@buschmais.com>
2014-09-24 17:46:04 +0200

    removed deprecated jQA rules

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/JarScannerPlugin.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/JarScannerPlugin.java
@@ -1,7 +1,6 @@
 package com.buschmais.jqassistant.plugin.java.impl.scanner;
 
 import com.buschmais.jqassistant.core.scanner.api.Scope;
-import com.buschmais.jqassistant.core.store.api.type.FileDescriptor;
 import com.buschmais.jqassistant.plugin.common.impl.scanner.AbstractArchiveScannerPlugin;
 import com.buschmais.jqassistant.plugin.java.api.scanner.JavaScope;
 
@@ -10,11 +9,6 @@ public class JarScannerPlugin extends AbstractArchiveScannerPlugin {
     @Override
     protected String getExtension() {
         return ".jar";
-    }
-
-    @Override
-    protected FileDescriptor afterArchive(FileDescriptor fileDescriptor) {
-        return fileDescriptor;
     }
 
     @Override
