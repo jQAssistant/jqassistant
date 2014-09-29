e67d2d1f6cc1214d4f41d230257403a5ea667644
Dirk Mahler <dirk.mahler@buschmais.com>
2014-09-29 18:10:07 +0200

    #76 push current artifact to scanner context

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/JarScannerPlugin.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/JarScannerPlugin.java
@@ -1,7 +1,12 @@
 package com.buschmais.jqassistant.plugin.java.impl.scanner;
 
+import java.io.File;
+
+import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
 import com.buschmais.jqassistant.core.scanner.api.Scope;
+import com.buschmais.jqassistant.core.store.api.type.ArchiveDescriptor;
 import com.buschmais.jqassistant.plugin.common.impl.scanner.AbstractArchiveScannerPlugin;
+import com.buschmais.jqassistant.plugin.java.api.model.JarArchiveDescriptor;
 import com.buschmais.jqassistant.plugin.java.api.scanner.JavaScope;
 
 public class JarScannerPlugin extends AbstractArchiveScannerPlugin {
@@ -14,5 +19,10 @@ public class JarScannerPlugin extends AbstractArchiveScannerPlugin {
     @Override
     protected Scope createScope(Scope currentScope) {
         return JavaScope.CLASSPATH;
+    }
+
+    @Override
+    protected ArchiveDescriptor createArchive(File file, String path, ScannerContext scannerContext) {
+        return scannerContext.getStore().create(JarArchiveDescriptor.class);
     }
 }
