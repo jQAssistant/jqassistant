f81a08adba6074bd1731c1a847d34f572fca55b5
FalkoKrische <falko.krische@buschmais.com>
2014-10-13 09:56:43 +0200

    #71 added ability to get the JQAssistant version (from a service);

---
+++ core/plugin/src/main/java/com/buschmais/jqassistant/core/plugin/impl/JQAssistantPropertiesImpl.java
@@ -0,0 +1,52 @@
+package com.buschmais.jqassistant.core.plugin.impl;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.slf4j.LoggerFactory;
+
+import com.buschmais.jqassistant.core.plugin.api.JQAssistantProperties;
+
+/**
+ * JQAssistant properties implementation.
+ */
+public class JQAssistantPropertiesImpl implements JQAssistantProperties {
+
+    /** Property key for "{@value} ". */
+    private static final String PROPERTY_KEY_VERSION = "project.version";
+
+    /** The properties. */
+    private final Properties properties;
+
+    /**
+     * Create a new instance.
+     */
+    private JQAssistantPropertiesImpl() {
+
+        properties = new Properties();
+        try {
+            properties.load(JQAssistantPropertiesImpl.class.getResourceAsStream("/jqassistant.properties"));
+        } catch (IOException e) {
+            LoggerFactory.getLogger(JQAssistantPropertiesImpl.class).error("Unable to read jqassistent.properties.", e);
+        }
+    }
+
+    /**
+     * Get the singleton instance of the properties.
+     *
+     * @return the instance
+     */
+    public static JQAssistantPropertiesImpl getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    @Override
+    public String getVersion() {
+        return (String) properties.get(PROPERTY_KEY_VERSION);
+    }
+
+    /** The instance holder. */
+    private static class LazyHolder {
+        private static final JQAssistantPropertiesImpl INSTANCE = new JQAssistantPropertiesImpl();
+    }
+}
