6737bb4b4e9bd9faa57cbcaf4dc9f27dee0e13b3
FalkoKrische <falko.krische@buschmais.com>
2014-10-13 09:56:43 +0200

    #71 added ability to get the JQAssistant version (from a service);

---
+++ core/plugin/src/main/java/com/buschmais/jqassistant/core/plugin/api/JQAssistantProperties.java
@@ -0,0 +1,10 @@
+package com.buschmais.jqassistant.core.plugin.api;
+
+/**
+ * Keeping all JQAssitant properties.
+ */
+public interface JQAssistantProperties {
+
+    /** Get the version of the JQAssistant. */
+    String getVersion();
+}
