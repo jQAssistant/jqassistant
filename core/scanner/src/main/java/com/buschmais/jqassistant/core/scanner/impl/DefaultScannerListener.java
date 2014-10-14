71ad606bfd466945e674419bc05e59c928b28725
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-14 18:32:35 +0200

    #102 added test for CLI scan

---
+++ core/scanner/src/main/java/com/buschmais/jqassistant/core/scanner/impl/DefaultScannerListener.java
@@ -0,0 +1,45 @@
+package com.buschmais.jqassistant.core.scanner.impl;
+
+import com.buschmais.jqassistant.core.scanner.api.ScannerListener;
+import com.buschmais.jqassistant.core.scanner.api.Scope;
+import com.buschmais.jqassistant.core.store.api.Store;
+import com.buschmais.jqassistant.core.store.api.model.FileDescriptor;
+
+/**
+ * The default listener implementation for the scanner.
+ * <p>
+ * Automatically commits transactions if a defined threshold of scanned items is
+ * reached.
+ * </p>
+ */
+public class DefaultScannerListener implements ScannerListener {
+
+    public static final int THRESHOLD = 200;
+
+    private int count = 0;
+    private Store store;
+
+    /**
+     * Constructor.
+     * 
+     * @param store
+     *            The store.
+     */
+    public DefaultScannerListener(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public <I> void before(I item, String relativePath, Scope scope) {
+    }
+
+    @Override
+    public <I> void after(I item, String relativePath, Scope scope, FileDescriptor fileDescriptor) {
+        count++;
+        if (count == THRESHOLD) {
+            store.commitTransaction();
+            store.beginTransaction();
+            count = 0;
+        }
+    }
+}
