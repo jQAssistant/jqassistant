3bc364970b617359d3f66efb0b5adad5a09f774e
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-14 18:32:35 +0200

    #102 added test for CLI scan

--- plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/scanner/ZipScannerIT.java
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/scanner/ZipScannerIT.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertThat;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -17,14 +18,49 @@ import com.buschmais.jqassistant.core.store.api.model.ArchiveDescriptor;
 import com.buschmais.jqassistant.core.store.api.model.FileDescriptor;
 import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
 
+/**
+ * Verifies scanning of ZIP arichves.
+ */
 public class ZipScannerIT extends AbstractPluginIT {
 
+    /**
+     * The strategies for refencing the archive to scan.
+     */
+    enum Strategy {
+        /**
+         * As URL.
+         */
+        Url {
+            Object get(File file) throws MalformedURLException {
+                return file.toURI().toURL();
+            }
+        },
+        /**
+         * As file.
+         */
+        File {
+            Object get(File file) {
+                return file;
+            }
+        };
+
+        abstract <T> T get(File file) throws Exception;
+    }
+
     @Test
-    public void zipArchive() throws IOException {
-        File archive = File.createTempFile("test", ".zip");
-        createZipArchive(archive);
+    public void zipFile() throws Exception {
+        test(Strategy.File);
+    }
+
+    @Test
+    public void zipUrl() throws Exception {
+        test(Strategy.Url);
+    }
+
+    private void test(Strategy strategy) throws Exception {
+        File archive = createZipArchive();
         store.beginTransaction();
-        FileDescriptor descriptor = getScanner().scan(archive, archive.getAbsolutePath(), null);
+        FileDescriptor descriptor = getScanner().scan(strategy.get(archive), archive.getAbsolutePath(), null);
         assertThat(descriptor, instanceOf(ArchiveDescriptor.class));
         ArchiveDescriptor archiveDescriptor = (ArchiveDescriptor) descriptor;
         assertThat(archiveDescriptor.getContains(), hasItem(fileDescriptorMatcher("/test.txt")));
@@ -32,7 +68,14 @@ public class ZipScannerIT extends AbstractPluginIT {
         archive.delete();
     }
 
-    private void createZipArchive(File archive) throws IOException {
+    /**
+     * Creates a ZIP archvie containing a file "test.txt"
+     * 
+     * @return archive The archive.
+     * @throws IOException
+     */
+    private File createZipArchive() throws IOException {
+        File archive = File.createTempFile("test", ".zip");
         ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(archive));
         ZipEntry fileEntry = new ZipEntry("test.txt");
         fileEntry.setTime(System.currentTimeMillis());
@@ -40,5 +83,6 @@ public class ZipScannerIT extends AbstractPluginIT {
         zipOutputStream.write("Hello World!".getBytes());
         zipOutputStream.closeEntry();
         zipOutputStream.close();
+        return archive;
     }
 }
