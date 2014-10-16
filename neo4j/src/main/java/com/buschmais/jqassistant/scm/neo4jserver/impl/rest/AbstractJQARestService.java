4ac2b696ab3d16b6f0c8720b49a678a75798075c
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-16 18:28:14 +0200

    #102 use shell script for running CLI tests

--- neo4j/src/main/java/com/buschmais/jqassistant/scm/neo4jserver/impl/rest/AbstractJQARestService.java
+++ neo4j/src/main/java/com/buschmais/jqassistant/scm/neo4jserver/impl/rest/AbstractJQARestService.java
@@ -1,28 +1,25 @@
 package com.buschmais.jqassistant.scm.neo4jserver.impl.rest;
 
+import java.util.List;
+
 import com.buschmais.jqassistant.core.analysis.api.Analyzer;
 import com.buschmais.jqassistant.core.analysis.api.RuleSelector;
 import com.buschmais.jqassistant.core.analysis.api.RuleSetReader;
 import com.buschmais.jqassistant.core.analysis.api.RuleSetResolverException;
 import com.buschmais.jqassistant.core.analysis.api.rule.RuleSet;
+import com.buschmais.jqassistant.core.analysis.api.rule.RuleSource;
 import com.buschmais.jqassistant.core.analysis.impl.AnalyzerImpl;
 import com.buschmais.jqassistant.core.analysis.impl.RuleSelectorImpl;
 import com.buschmais.jqassistant.core.analysis.impl.RuleSetReaderImpl;
-import com.buschmais.jqassistant.core.plugin.api.ModelPluginRepository;
 import com.buschmais.jqassistant.core.plugin.api.PluginConfigurationReader;
 import com.buschmais.jqassistant.core.plugin.api.PluginRepositoryException;
 import com.buschmais.jqassistant.core.plugin.api.RulePluginRepository;
-import com.buschmais.jqassistant.core.plugin.impl.ModelPluginRepositoryImpl;
 import com.buschmais.jqassistant.core.plugin.impl.PluginConfigurationReaderImpl;
 import com.buschmais.jqassistant.core.plugin.impl.RulePluginRepositoryImpl;
 import com.buschmais.jqassistant.core.report.impl.InMemoryReportWriter;
 import com.buschmais.jqassistant.core.store.api.Store;
 import com.buschmais.jqassistant.scm.common.console.Slf4jConsole;
 import com.buschmais.jqassistant.scm.common.report.ReportHelper;
-
-import java.util.List;
-
-import javax.xml.transform.Source;
 
 public abstract class AbstractJQARestService {
 
@@ -43,20 +40,12 @@ public abstract class AbstractJQARestService {
     }
 
     protected RuleSet getAvailableRules() {
-        List<Source> ruleSources = rulePluginRepository.getRuleSources();
+        List<RuleSource> ruleSources = rulePluginRepository.getRuleSources();
         return ruleSetReader.read(ruleSources);
     }
 
     protected Store getStore() {
         return store;
-    }
-
-    protected ModelPluginRepository getModelPluginRepository() {
-        try {
-            return new ModelPluginRepositoryImpl(pluginConfigurationReader);
-        } catch (PluginRepositoryException e) {
-            throw new IllegalStateException("Cannot get model plugin repository", e);
-        }
     }
 
     protected RuleSet getEffectiveRuleSet(List<String> conceptNames, List<String> constraintNames, List<String> groupNames) throws RuleSetResolverException {
