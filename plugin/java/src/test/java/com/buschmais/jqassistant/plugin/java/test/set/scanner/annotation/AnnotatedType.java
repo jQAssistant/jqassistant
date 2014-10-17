21a0110ee38b89de82f95ba6d7ed8644540a3fc5
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-17 18:16:25 +0200

    #135 added test and fixed OsgiBundleIT

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/set/scanner/annotation/AnnotatedType.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/set/scanner/annotation/AnnotatedType.java
@@ -9,11 +9,25 @@ public class AnnotatedType {
     @Annotation(value = "field")
     private String annotatedField;
 
+    public AnnotatedType(@Annotation("parameter") String parameter) {
+    }
+
     @Annotation("method")
     public void annotatedMethod(@Annotation("parameter") String parameter) {
         @SuppressWarnings("unused")
         @Annotation("localField")
         String annotatedLocalField = "value";
+    }
+
+    public class GenericInnerAnnotatedType<T> {
+        /**
+         * Defines a generic constructor with an annotated parameter.
+         * 
+         * @param parameter
+         *            The paramter.
+         */
+        public GenericInnerAnnotatedType(@Annotation("parameter") T parameter) {
+        }
     }
 
 }
