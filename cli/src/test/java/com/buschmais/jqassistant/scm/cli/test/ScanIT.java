bb849f8fc1a90956d26aea09545b8f36a34e273a
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-18 13:16:11 +0200

    #102 added test for plugin loading (i.e. scan of CDI bean descriptors)

--- cli/src/test/java/com/buschmais/jqassistant/scm/cli/test/ScanIT.java
+++ cli/src/test/java/com/buschmais/jqassistant/scm/cli/test/ScanIT.java
@@ -12,7 +12,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.buschmais.jqassistant.core.store.impl.EmbeddedGraphStore;
@@ -28,7 +27,7 @@ public class ScanIT extends AbstractCLIIT {
         URL file = getResource(ScanTask.class);
         URL directory = ScanIT.class.getResource("/");
         String[] args = new String[] { "scan", "-f", file.getFile() + "," + directory.getFile() };
-        execute(args);
+        assertThat(execute(args), equalTo(0));
         verifyTypesScanned(getDefaultStoreDirectory(), ScanTask.class, ScanIT.class);
     }
 
@@ -37,7 +36,7 @@ public class ScanIT extends AbstractCLIIT {
         URL directory1 = getResource(ScanTask.class);
         URL directory2 = getResource(ScanIT.class);
         String[] args = new String[] { "scan", "-u", directory1 + "," + directory2 };
-        execute(args);
+        assertThat(execute(args), equalTo(0));
         verifyTypesScanned(getDefaultStoreDirectory(), ScanTask.class, ScanIT.class);
     }
 
@@ -46,7 +45,7 @@ public class ScanIT extends AbstractCLIIT {
         URL file = getResource(ScanTask.class);
         String customStoreDirectory = "tmp/customStore";
         String[] args = new String[] { "scan", "-f", file.getFile(), "-s", customStoreDirectory };
-        execute(args);
+        assertThat(execute(args), equalTo(0));
         verifyTypesScanned(new File(getWorkingDirectory(), customStoreDirectory), ScanTask.class);
     }
 
@@ -55,23 +54,29 @@ public class ScanIT extends AbstractCLIIT {
         // Scan a file
         URL file1 = getResource(ScanIT.class);
         String[] args1 = new String[] { "scan", "-f", file1.getFile() };
-        execute(args1);
+        assertThat(execute(args1), equalTo(0));
         verifyTypesScanned(getDefaultStoreDirectory(), ScanIT.class);
         // Scan a second file using reset
         URL file2 = getResource(ScanTask.class);
         String[] args2 = new String[] { "scan", "-f", file2.getFile(), "-reset" };
-        execute(args2);
+        assertThat(execute(args2), equalTo(0));
         verifyTypesScanned(getDefaultStoreDirectory(), ScanTask.class);
         verifyTypesNotScanned(getWorkingDirectory(), ScanIT.class);
         // Scan the first file againg without reset
-        execute(args1);
+        assertThat(execute(args1), equalTo(0));
         verifyTypesScanned(getDefaultStoreDirectory(), ScanIT.class);
     }
 
     @Test
-    @Ignore
-    public void pluginClassloader() {
-
+    public void pluginClassloader() throws IOException, InterruptedException {
+        File testClassDirectory = new File(ScanIT.class.getResource("/").getFile());
+        String[] args = new String[] { "scan", "-f", testClassDirectory.getAbsolutePath() };
+        assertThat(execute(args), equalTo(0));
+        EmbeddedGraphStore store = new EmbeddedGraphStore(getDefaultStoreDirectory().getAbsolutePath());
+        store.start(Collections.<Class<?>> emptyList());
+        Long count = executeQuery(store, "match (b:Cdi:Beans) return count(b) as count", Collections.<String, Object> emptyMap(), "count", Long.class);
+        assertThat("Expecting on beans.xml descriptor.", count, equalTo(1l));
+        store.stop();
     }
 
     /**
@@ -129,14 +134,32 @@ public class ScanIT extends AbstractCLIIT {
      * @return <code>true</code> if the type is represented in the database.
      */
     private boolean isTypeScanned(EmbeddedGraphStore store, Class<?> type) {
-        store.beginTransaction();
         Map<String, Object> params = new HashMap<>();
         params.put("type", type.getName());
-        Result<CompositeRowObject> result = store.executeQuery("match (t:Type) where t.fqn={type} return count(t) as count", params);
-        assertThat(result.hasResult(), equalTo(true));
-        Long count = result.getSingleResult().get("count", Long.class);
-        store.commitTransaction();
+        String query = "match (t:Type) where t.fqn={type} return count(t) as count";
+        Long count = executeQuery(store, query, params, "count", Long.class);
         return count.longValue() == 1;
+    }
+
+    /**
+     * Executes a query with single result and returns it.
+     * 
+     * @param store
+     *            The initialized store.
+     * @param query
+     *            The query.
+     * @param params
+     *            The parameters.
+     * @return The result.
+     */
+
+    private <T> T executeQuery(EmbeddedGraphStore store, String query, Map<String, Object> params, String resultColumn, Class<T> resultType) {
+        store.beginTransaction();
+        Result<CompositeRowObject> result = store.executeQuery(query, params);
+        assertThat(result.hasResult(), equalTo(true));
+        T value = result.getSingleResult().get(resultColumn, resultType);
+        store.commitTransaction();
+        return value;
     }
 
 }
