34fadba5da38bb6c97d8c7689a879b2ffb5cf0fd
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-22 10:01:15 +0200

    #118 added support for additional scan dirs

--- plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenProjectDirectoryDescriptor.java
+++ plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenProjectDirectoryDescriptor.java
@@ -2,11 +2,11 @@ package com.buschmais.jqassistant.plugin.maven3.api.model;
 
 import java.util.List;
 
-import com.buschmais.jqassistant.core.store.api.model.FileDescriptor;
+import com.buschmais.jqassistant.core.store.api.model.FileContainerDescriptor;
 import com.buschmais.jqassistant.plugin.common.api.model.ArtifactDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
-public interface MavenProjectDirectoryDescriptor extends MavenProjectDescriptor, FileDescriptor {
+public interface MavenProjectDirectoryDescriptor extends MavenProjectDescriptor, FileContainerDescriptor {
 
     @Relation("CREATES")
     List<ArtifactDescriptor> getCreatesArtifacts();
