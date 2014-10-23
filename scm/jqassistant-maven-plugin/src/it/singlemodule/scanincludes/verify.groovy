c5d57fcdcb1a870c4aecbbc8c78c57cc47cd856c
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-23 17:56:09 +0200

    #118 activated tests

--- scm/jqassistant-maven-plugin/src/it/singlemodule/scanincludes/verify.groovy
+++ scm/jqassistant-maven-plugin/src/it/singlemodule/scanincludes/verify.groovy
@@ -1,2 +1,7 @@
-// assert !new File(basedir, 'target/jqassistant/store').exists()
-// assert !new File(basedir, 'target/jqassistant/jqassistant-report.xml').exists()
+def reportFile = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
+assert reportFile.exists()
+def report = new XmlSlurper().parse(reportFile)
+def testValueConcept = report.group.concept.find { it.@id == "scanInclude:Properties" }
+assert testValueConcept != null
+assert testValueConcept.result.rows['@count'] == "1";
+assert testValueConcept.result.rows.row[0].column[0].value == "/test.properties";
