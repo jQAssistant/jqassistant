e63010c655a046a673deccd3fd14d1f8c7204dc2
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-24 09:35:45 +0200

    #102 added test for analysis severity

--- core/report/src/main/java/com/buschmais/jqassistant/core/report/api/LanguageHelper.java
+++ core/report/src/main/java/com/buschmais/jqassistant/core/report/api/LanguageHelper.java
@@ -3,7 +3,6 @@ package com.buschmais.jqassistant.core.report.api;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
-import com.buschmais.jqassistant.core.analysis.api.AnalysisListenerException;
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
 import com.buschmais.xo.spi.reflection.AnnotatedType;
 
@@ -22,7 +21,7 @@ public final class LanguageHelper {
      *
      * @throws com.buschmais.jqassistant.core.analysis.api.AnalysisListenerException
      */
-    public static LanguageElement getLanguageElement(Descriptor descriptor) throws AnalysisListenerException {
+    public static LanguageElement getLanguageElement(Descriptor descriptor) {
         for (Class<?> descriptorType : descriptor.getClass().getInterfaces()) {
             AnnotatedType annotatedType = new AnnotatedType(descriptorType);
             Annotation languageAnnotation = annotatedType.getByMetaAnnotation(Language.class);
@@ -45,22 +44,20 @@ public final class LanguageHelper {
      * @param <T>
      *            The expected type.
      * @return The value.
-     * @throws com.buschmais.jqassistant.core.analysis.api.AnalysisListenerException
-     *             If the value cannot be determined from the annotation.
      */
-    private static <T> T getAnnotationValue(Annotation annotation, String value, Class<T> expectedType) throws AnalysisListenerException {
+    private static <T> T getAnnotationValue(Annotation annotation, String value, Class<T> expectedType) {
         Class<? extends Annotation> annotationType = annotation.annotationType();
         Method valueMethod;
         try {
             valueMethod = annotationType.getDeclaredMethod(value);
         } catch (NoSuchMethodException e) {
-            throw new AnalysisListenerException("Cannot resolve required method '" + value + "()' for '" + annotationType + "'.");
+            throw new IllegalStateException("Cannot resolve required method '" + value + "()' for '" + annotationType + "'.");
         }
         Object elementValue;
         try {
             elementValue = valueMethod.invoke(annotation);
         } catch (ReflectiveOperationException e) {
-            throw new AnalysisListenerException("Cannot invoke method value() for " + annotationType);
+            throw new IllegalStateException("Cannot invoke method value() for " + annotationType);
         }
         return elementValue != null ? expectedType.cast(elementValue) : null;
     }
