f369344eacadd619532ceebc6297e882a7a7e910
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-27 07:28:06 +0100

    #102 fixed missing file attributes and line endings for unix shell scripts

--- scm/cli/src/main/bin/jqassistant.sh
+++ scm/cli/src/main/bin/jqassistant.sh
@@ -4,5 +4,5 @@ if [ -z "$JQASSISTANT_HOME" ] ; then
   JQASSISTANT_HOME=`readlink -f "$BIN_DIR/.."`
 fi
 LIB_DIR=$JQASSISTANT_HOME/lib
-java -jar $LIB_DIR/${project.artifactId}-${project.version}.${project.packaging} $*
+java -jar $LIB_DIR/jqassistant.scm.cli-1.0.0-M4-SNAPSHOT.jar $*
 
