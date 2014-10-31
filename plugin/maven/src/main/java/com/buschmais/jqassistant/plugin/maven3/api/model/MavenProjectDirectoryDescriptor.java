75e877de33d20d9d7c2ea6c774d83aef834038a5
Dirk Mahler <dirk.mahler@buschmais.com>
2014-10-31 23:44:38 +0100

    #76 fixed Maven descriptors

--- plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenProjectDirectoryDescriptor.java
+++ plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenProjectDirectoryDescriptor.java
@@ -2,11 +2,11 @@ package com.buschmais.jqassistant.plugin.maven3.api.model;
 
 import java.util.List;
 
-import com.buschmais.jqassistant.core.store.api.model.FileContainerDescriptor;
+import com.buschmais.jqassistant.core.store.api.model.DirectoryDescriptor;
 import com.buschmais.jqassistant.plugin.common.api.model.ArtifactDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
-public interface MavenProjectDirectoryDescriptor extends MavenProjectDescriptor, FileContainerDescriptor {
+public interface MavenProjectDirectoryDescriptor extends MavenProjectDescriptor, DirectoryDescriptor {
 
     @Relation("CREATES")
     List<ArtifactDescriptor> getCreatesArtifacts();
