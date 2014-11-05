1abf4f74ab31ebf5eea2defec4065d9152fb77d1
Dirk Mahler <dirk.mahler@buschmais.com>
2014-11-05 20:00:52 +0100

    #146 fixed build error

---
+++ plugin/rdbms/src/main/java/com/buschmais/jqassistant/plugin/rdbms/api/model/TableOrViewDescriptor.java
@@ -0,0 +1,25 @@
+package com.buschmais.jqassistant.plugin.rdbms.api.model;
+
+import java.util.List;
+
+import com.buschmais.jqassistant.core.store.api.model.NamedDescriptor;
+import com.buschmais.xo.neo4j.api.annotation.Label;
+import com.buschmais.xo.neo4j.api.annotation.Relation;
+
+@Label("Table")
+public interface TableOrViewDescriptor extends RdbmsDescriptor, NamedDescriptor {
+
+    @Relation("HAS_PRIMARY_KEY")
+    PrimaryKeyDescriptor getPrimaryKey();
+
+    void setPrimaryKey(PrimaryKeyDescriptor primaryKeyDescriptor);
+
+    @Relation("HAS_COLUMN")
+    List<ColumnDescriptor> getColumns();
+
+    @Relation("HAS_INDEX")
+    List<IndexDescriptor> getIndices();
+
+    @Relation("HAS_TRIGGER")
+    List<TriggerDescriptor> getTriggers();
+}
