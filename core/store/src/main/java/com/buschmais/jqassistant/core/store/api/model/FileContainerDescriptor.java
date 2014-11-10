57481b521f699f6cf5d520c0be303bbff5fd7b06
Dirk Mahler <dirk.mahler@buschmais.com>
2014-11-10 18:13:27 +0100

    #122 refactored creation of DEPENDS_ON relations

--- core/store/src/main/java/com/buschmais/jqassistant/core/store/api/model/FileContainerDescriptor.java
+++ core/store/src/main/java/com/buschmais/jqassistant/core/store/api/model/FileContainerDescriptor.java
@@ -1,11 +1,7 @@
 package com.buschmais.jqassistant.core.store.api.model;
 
-import static com.buschmais.xo.api.annotation.ResultOf.Parameter;
-
 import java.util.List;
 
-import com.buschmais.xo.api.annotation.ResultOf;
-import com.buschmais.xo.neo4j.api.annotation.Cypher;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
 public interface FileContainerDescriptor extends FileNameDescriptor {
@@ -17,9 +13,5 @@ public interface FileContainerDescriptor extends FileNameDescriptor {
      */
     @Relation("CONTAINS")
     List<FileDescriptor> getContains();
-
-    @ResultOf
-    @Cypher("match (c),(f) where id(c)={this} and id(f)={file} create unique (c)-[:CONTAINS]->(f)")
-    void addContains(@Parameter("file") Descriptor file);
 
 }
