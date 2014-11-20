bd8b9432bffb11ac63a28b9e0b4f8e8f1d27c780
Dirk Mahler <dirk.mahler@buschmais.com>
2014-11-20 13:42:35 +0100

    #153 use realpath instead of readlink

--- scm/cli/src/main/bin/jqassistant.sh
+++ scm/cli/src/main/bin/jqassistant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 BIN_DIR=`dirname "$0"`
 if [ -z "$JQASSISTANT_HOME" ] ; then
-  export JQASSISTANT_HOME=`readlink -f "$BIN_DIR/.."`
+  export JQASSISTANT_HOME=`realpath "$BIN_DIR/.."`
 fi
 LIB_DIR=$JQASSISTANT_HOME/lib
 java $JQASSISTANT_OPTS -jar $LIB_DIR/${project.artifactId}-${project.version}.${project.packaging} $*
