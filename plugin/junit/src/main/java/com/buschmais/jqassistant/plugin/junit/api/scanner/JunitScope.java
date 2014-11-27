9efa880443d5a2d42a02f8a6944f492bd81fda4f
Dirk Mahler <dirk.mahler@buschmais.com>
2014-11-27 17:00:29 +0100

    #156 extend plugin descriptor for scopes

--- plugin/junit/src/main/java/com/buschmais/jqassistant/plugin/junit/api/scanner/JunitScope.java
+++ plugin/junit/src/main/java/com/buschmais/jqassistant/plugin/junit/api/scanner/JunitScope.java
@@ -2,6 +2,20 @@ package com.buschmais.jqassistant.plugin.junit.api.scanner;
 
 import com.buschmais.jqassistant.core.scanner.api.Scope;
 
+/**
+ * Defines the scopes for junit.
+ */
 public enum JunitScope implements Scope {
+
     TESTREPORTS;
+
+    @Override
+    public String getPrefix() {
+        return "junit";
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
 }
