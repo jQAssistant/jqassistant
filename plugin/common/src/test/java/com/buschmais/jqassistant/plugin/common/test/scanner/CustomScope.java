a940a717ef06def357c93765302ef89f6bcb7e34
Dirk Mahler <dirk.mahler@buschmais.com>
2014-11-27 17:00:29 +0100

    #156 extend plugin descriptor for scopes

--- plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/scanner/CustomScope.java
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/scanner/CustomScope.java
@@ -5,4 +5,14 @@ import com.buschmais.jqassistant.core.scanner.api.Scope;
 public enum CustomScope implements Scope {
 
     CUSTOM;
+
+    @Override
+    public String getPrefix() {
+        return "custom";
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
 }
