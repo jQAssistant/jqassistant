8f8632cba432ff25d20fe5f22973528203c14b9a
RonaldKunzmann <ronald.kunzmann@buschmais.com>
2015-01-13 17:08:45 +0100

    Extend PomIT. #166

--- plugin/maven3/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/BaseProfileDescriptor.java
+++ plugin/maven3/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/BaseProfileDescriptor.java
@@ -3,8 +3,10 @@ package com.buschmais.jqassistant.plugin.maven3.api.model;
 import java.util.List;
 
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
+import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
+@Abstract
 public interface BaseProfileDescriptor extends Descriptor {
 
     /**
