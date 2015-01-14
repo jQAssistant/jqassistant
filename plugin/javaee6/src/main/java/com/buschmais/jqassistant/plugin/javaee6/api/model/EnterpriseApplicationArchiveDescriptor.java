2afc6b8f136215b32fae5511eba6801b795447e8
Dirk Mahler <dirk.mahler@buschmais.com>
2015-01-14 18:51:02 +0100

    #124 extend JavaEE docs

--- plugin/javaee6/src/main/java/com/buschmais/jqassistant/plugin/javaee6/api/model/EnterpriseApplicationArchiveDescriptor.java
+++ plugin/javaee6/src/main/java/com/buschmais/jqassistant/plugin/javaee6/api/model/EnterpriseApplicationArchiveDescriptor.java
@@ -4,7 +4,7 @@ import com.buschmais.jqassistant.core.store.api.model.ArchiveDescriptor;
 import com.buschmais.jqassistant.plugin.common.api.model.ApplicationDescriptor;
 
 /**
- * Describes a EAR archive.
+ * Describes an EAR archive.
  */
 public interface EnterpriseApplicationArchiveDescriptor extends EnterpriseDescriptor, ApplicationDescriptor, ArchiveDescriptor {
 }
