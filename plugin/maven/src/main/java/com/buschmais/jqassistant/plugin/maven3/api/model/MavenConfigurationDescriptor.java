1f5c9744336c22330b3b553b8a682294fae023f5
RonaldKunzmann <ronald.kunzmann@buschmais.com>
2015-01-15 15:38:49 +0100

    Added profile activation. Added asciidoc. Completed PomIT. #166

--- plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenConfigurationDescriptor.java
+++ plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenConfigurationDescriptor.java
@@ -2,7 +2,7 @@ package com.buschmais.jqassistant.plugin.maven3.api.model;
 
 import java.util.List;
 
-import com.buschmais.jqassistant.plugin.java.api.model.ValueDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.model.ValueDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
