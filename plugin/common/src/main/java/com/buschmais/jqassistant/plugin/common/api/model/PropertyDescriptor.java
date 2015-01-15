93f72a63517b631bfee2c13853e0ccb6a03f10ee
RonaldKunzmann <ronald.kunzmann@buschmais.com>
2015-01-15 15:38:49 +0100

    Added profile activation. Added asciidoc. Completed PomIT. #166

---
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/PropertyDescriptor.java
@@ -0,0 +1,19 @@
+package com.buschmais.jqassistant.plugin.common.api.model;
+
+import com.buschmais.xo.neo4j.api.annotation.Label;
+import com.buschmais.xo.neo4j.api.annotation.Property;
+
+/**
+ * Represents a property value.
+ */
+@Label("Property")
+public interface PropertyDescriptor extends ValueDescriptor<String> {
+
+    @Property("value")
+    @Override
+    String getValue();
+
+    @Override
+    void setValue(String value);
+
+}
