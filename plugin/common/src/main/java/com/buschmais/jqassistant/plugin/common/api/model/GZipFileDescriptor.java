38813fa056847e14210ab21b2f9d0b07b324af39
Dirk Mahler <dirk.mahler@buschmais.com>
2015-02-23 18:05:11 +0100

    #183 added GZip file scanner plugin

---
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/GZipFileDescriptor.java
@@ -0,0 +1,7 @@
+package com.buschmais.jqassistant.plugin.common.api.model;
+
+import com.buschmais.xo.neo4j.api.annotation.Label;
+
+@Label("GZip")
+public interface GZipFileDescriptor extends FileDescriptor {
+}
