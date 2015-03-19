ecbfd62dd5afda6ea5507e57565a8fc74768521f
Dirk Mahler <dirk.mahler@buschmais.com>
2015-03-19 09:17:19 +0100

    #161 calculate MD5 hashsum of class files during scan

---
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/MD5Descriptor.java
@@ -0,0 +1,12 @@
+package com.buschmais.jqassistant.plugin.common.api.model;
+
+/**
+ * A descriptor providing an MD5 hashsum.
+ */
+public interface MD5Descriptor {
+
+    String getMD5();
+
+    void setMD5(String md5);
+
+}
