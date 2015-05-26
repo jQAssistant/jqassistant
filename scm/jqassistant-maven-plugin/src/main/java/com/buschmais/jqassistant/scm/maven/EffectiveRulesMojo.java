e5d7e53bb95c3ea40324345f9d3f24168d0f0fb5
Dirk Mahler <dirk.mahler@buschmais.com>
2015-05-26 18:12:48 +0200

    #128 added check for minimum maven version

--- scm/jqassistant-maven-plugin/src/main/java/com/buschmais/jqassistant/scm/maven/EffectiveRulesMojo.java
+++ scm/jqassistant-maven-plugin/src/main/java/com/buschmais/jqassistant/scm/maven/EffectiveRulesMojo.java
@@ -1,17 +1,17 @@
 package com.buschmais.jqassistant.scm.maven;
 
-import java.util.List;
-
+import com.buschmais.jqassistant.core.analysis.api.AnalysisException;
+import com.buschmais.jqassistant.core.analysis.api.RuleSelection;
+import com.buschmais.jqassistant.core.analysis.api.rule.RuleSet;
+import com.buschmais.jqassistant.core.store.api.Store;
+import com.buschmais.jqassistant.scm.common.report.RuleHelper;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.project.MavenProject;
 
-import com.buschmais.jqassistant.core.analysis.api.AnalysisException;
-import com.buschmais.jqassistant.core.analysis.api.rule.RuleSet;
-import com.buschmais.jqassistant.core.store.api.Store;
-import com.buschmais.jqassistant.scm.common.report.RuleHelper;
+import java.util.List;
 
 /**
  * Lists all effective rules.
@@ -28,9 +28,10 @@ public class EffectiveRulesMojo extends AbstractProjectMojo {
     public void aggregate(MavenProject rootModule, List<MavenProject> projects, Store store) throws MojoExecutionException, MojoFailureException {
         getLog().info("Effective rules for '" + rootModule.getName() + "'.");
         RuleSet ruleSet = readRules(rootModule);
+        RuleSelection ruleSelection = RuleSelection.Builder.select(ruleSet, groups, constraints, concepts);
         RuleHelper ruleHelper = new RuleHelper(new MavenConsole(getLog()));
         try {
-            ruleHelper.printRuleSet(ruleSet);
+            ruleHelper.printRuleSet(ruleSet, ruleSelection);
         } catch (AnalysisException e) {
             throw new MojoExecutionException("Cannot print effective rules.", e);
         }
