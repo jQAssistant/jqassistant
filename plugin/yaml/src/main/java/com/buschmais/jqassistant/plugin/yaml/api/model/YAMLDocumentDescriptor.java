ab6c2a5e2a7d7f301cf6fb96cb4147628aef661c
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2015-06-06 23:50:43 +0200

    Changed the label for a YAML document from YAMLDocument to Document.

--- plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml/api/model/YAMLDocumentDescriptor.java
+++ plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml/api/model/YAMLDocumentDescriptor.java
@@ -5,7 +5,7 @@ import com.buschmais.xo.neo4j.api.annotation.Relation;
 
 import java.util.List;
 
-@Label("YAMLDocument")
+@Label("Document")
 public interface YAMLDocumentDescriptor extends YAMLDescriptor, YAMLKeyBucket,
                                                 YAMLValueBucket {
 
