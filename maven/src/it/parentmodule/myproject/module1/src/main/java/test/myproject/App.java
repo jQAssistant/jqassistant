b883808d1ad0e9ae7f37921dba0c53d992c949b3
Peter Herklotz <peter.herklotz@buschmais.com>
2015-07-24 14:21:14 +0200

    #218 fix reset on project root bug, add first test

---
+++ maven/src/it/parentmodule/myproject/module1/src/main/java/test/myproject/App.java
@@ -0,0 +1,13 @@
+package test.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
