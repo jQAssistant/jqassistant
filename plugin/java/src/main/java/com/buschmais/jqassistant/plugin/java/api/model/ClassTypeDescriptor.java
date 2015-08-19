8b4a6042c1e1820d0692e20869ffe134f322d6b4
Dirk Mahler <dirk.mahler@buschmais.com>
2015-08-19 18:07:52 +0200

    #225 revert fix and replace it by general language reporting approach

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/ClassTypeDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/ClassTypeDescriptor.java
@@ -1,10 +1,7 @@
 package com.buschmais.jqassistant.plugin.java.api.model;
 
-import com.buschmais.jqassistant.plugin.java.api.report.Java;
-
 /**
  * Denotes a class type.
  */
-@Java(Java.JavaLanguageElement.Type)
 public interface ClassTypeDescriptor extends ClassFileDescriptor, ClassDescriptor {
 }
