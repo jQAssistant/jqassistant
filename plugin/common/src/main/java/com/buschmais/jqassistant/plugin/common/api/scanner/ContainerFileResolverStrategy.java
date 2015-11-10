916c40060063381e30299e6f6aaa715d3d7647a9
Dirk Mahler <dirk.mahler@buschmais.com>
2015-11-10 21:43:50 +0100

    #291 reworked type resolution

--- plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/scanner/ContainerFileResolverStrategy.java
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/scanner/ContainerFileResolverStrategy.java
@@ -1,5 +1,6 @@
 package com.buschmais.jqassistant.plugin.common.api.scanner;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +22,8 @@ public class ContainerFileResolverStrategy extends AbstractFileResolverStrategy 
 
     public ContainerFileResolverStrategy(FileContainerDescriptor fileContainerDescriptor) {
         this.fileContainerDescriptor = fileContainerDescriptor;
+        this.containedFiles = getCache(fileContainerDescriptor.getContains());
+        this.requiredFiles = getCache(fileContainerDescriptor.getRequires());
     }
 
     @Override
@@ -47,10 +50,54 @@ public class ContainerFileResolverStrategy extends AbstractFileResolverStrategy 
         return null;
     }
 
+    /**
+     * Flush the caches to the store.
+     */
     public void flush() {
         createHierarchy();
-        fileContainerDescriptor.getRequires().addAll(requiredFiles.values());
-        fileContainerDescriptor.getContains().addAll(containedFiles.values());
+        sync(fileContainerDescriptor.getRequires(), requiredFiles);
+        sync(fileContainerDescriptor.getContains(), containedFiles);
+    }
+
+    /**
+     * Sync the given target collection with the new state from the cache map.
+     * 
+     * @param target
+     *            The target collection.
+     * @param after
+     *            The new state to sync to.
+     */
+    private void sync(Collection<FileDescriptor> target, Map<String, FileDescriptor> after) {
+        Map<String, FileDescriptor> before = getCache(target);
+        Map<String, FileDescriptor> all = new HashMap<>();
+        all.putAll(before);
+        all.putAll(after);
+        for (Map.Entry<String, FileDescriptor> entry : all.entrySet()) {
+            String key = entry.getKey();
+            FileDescriptor fileDescriptor = entry.getValue();
+            boolean hasBefore = before.containsKey(key);
+            boolean hasAfter = after.containsKey(key);
+            if (hasBefore && !hasAfter) {
+                target.remove(fileDescriptor);
+            } else if (!hasBefore && hasAfter) {
+                target.add(fileDescriptor);
+            }
+        }
+    }
+
+    /**
+     * Creates cache map from the given collection of file descriptors.
+     * 
+     * @param fileDescriptors
+     *            The collection of file descriptors.
+     * @return The cache map.
+     */
+    private Map<String, FileDescriptor> getCache(Iterable<FileDescriptor> fileDescriptors) {
+        Map<String, FileDescriptor> cache = new HashMap<>();
+        for (FileDescriptor fileDescriptor : fileDescriptors) {
+            cache.put(fileDescriptor.getFileName(), fileDescriptor);
+        }
+        return cache;
     }
 
     /**
@@ -72,10 +119,23 @@ public class ContainerFileResolverStrategy extends AbstractFileResolverStrategy 
         }
     }
 
+    /**
+     * Adds a file to the container.
+     * 
+     * @param path
+     *            The path of the file.
+     * @param fileDescriptor
+     *            The file descriptor.
+     */
     public void put(String path, FileDescriptor fileDescriptor) {
         containedFiles.put(path, fileDescriptor);
     }
 
+    /**
+     * Returns the size of the container.
+     * 
+     * @return The size of the container.
+     */
     public int size() {
         return containedFiles.size();
     }
