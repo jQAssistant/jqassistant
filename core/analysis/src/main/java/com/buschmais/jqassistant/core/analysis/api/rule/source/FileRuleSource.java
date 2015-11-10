256c748642e316302d3a29cb0a6bb2e9518acce2
Dirk Mahler <dirk.mahler@buschmais.com>
2015-11-10 21:43:50 +0100

    #291 reworked type resolution

--- core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/api/rule/source/FileRuleSource.java
+++ core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/api/rule/source/FileRuleSource.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.io.DirectoryWalker;
 
 /**
  * A rule source which is provided from a file.
@@ -30,4 +36,26 @@ public class FileRuleSource extends RuleSource {
     public InputStream getInputStream() throws IOException {
         return new FileInputStream(file);
     }
+
+    public static List<RuleSource> getRuleSources(File rulesDirectory) throws IOException {
+        final List<File> ruleFiles = new ArrayList<>();
+        new DirectoryWalker<File>() {
+            @Override
+            protected void handleFile(File file, int depth, Collection<File> results) throws IOException {
+                if (!file.isDirectory()) {
+                    results.add(file);
+                }
+            }
+
+            public void scan(File directory) throws IOException {
+                super.walk(directory, ruleFiles);
+            }
+        }.scan(rulesDirectory);
+        List<RuleSource> ruleSources = new LinkedList<>();
+        for (File ruleFile : ruleFiles) {
+            ruleSources.add(new FileRuleSource(ruleFile));
+        }
+        return ruleSources;
+    }
+
 }
