a77cc839869d77105dec972196bb7ee53435d486
Dirk Mahler <dirk.mahler@buschmais.com>
2015-11-21 20:45:07 +0100

    #301 fail gracefully if a .class file is invalid

---
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/InvalidDescriptor.java
@@ -0,0 +1,12 @@
+package com.buschmais.jqassistant.plugin.common.api.model;
+
+/**
+ * Template for descriptor which indicate that information is invalid.
+ */
+public interface InvalidDescriptor {
+
+    boolean isInvalid();
+
+    void setInvalid(boolean invalid);
+
+}
