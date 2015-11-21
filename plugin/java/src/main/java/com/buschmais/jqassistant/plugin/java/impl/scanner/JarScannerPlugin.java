e25a4b0fe00359d317bc1070109f368dbb3896ca
Dirk Mahler <dirk.mahler@buschmais.com>
2015-11-21 17:29:49 +0100

    #300 add Zip label to all ZIP archives

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/JarScannerPlugin.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/JarScannerPlugin.java
@@ -2,13 +2,13 @@ package com.buschmais.jqassistant.plugin.java.impl.scanner;
 
 import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
 import com.buschmais.jqassistant.core.scanner.api.Scope;
-import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractArchiveScannerPlugin;
+import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractZipArchiveScannerPlugin;
 import com.buschmais.jqassistant.plugin.java.api.model.JarArchiveDescriptor;
 import com.buschmais.jqassistant.plugin.java.api.scanner.ArtifactScopedTypeResolver;
 import com.buschmais.jqassistant.plugin.java.api.scanner.JavaScope;
 import com.buschmais.jqassistant.plugin.java.api.scanner.TypeResolver;
 
-public class JarScannerPlugin extends AbstractArchiveScannerPlugin<JarArchiveDescriptor> {
+public class JarScannerPlugin extends AbstractZipArchiveScannerPlugin<JarArchiveDescriptor> {
 
     @Override
     protected String getExtension() {
