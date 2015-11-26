3aaa283127c36e9c311062af22ee149eca4ccd7f
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2015-11-27 00:10:49 +0100

    Removed the Sonar plugin from the source repository of jQAssistant. See issue #303.

--- sonar/plugin/src/main/java/com/buschmais/jqassistant/sonar/plugin/rule/ConceptTemplateRule.java
+++
@@ -1,9 +0,0 @@
-package com.buschmais.jqassistant.sonar.plugin.rule;
-
-import org.sonar.check.Cardinality;
-import org.sonar.check.Priority;
-import org.sonar.check.Rule;
-
-@Rule(key = "ConceptTemplate", name = "Concept Template", description = "Template for user a defined concept.", priority = Priority.MAJOR, cardinality = Cardinality.MULTIPLE)
-public class ConceptTemplateRule extends AbstractTemplateRule {
-}
--- sonar/plugin/src/main/java/com/buschmais/jqassistant/sonar/plugin/rule/ConstraintTemplateRule.java
+++
@@ -1,9 +0,0 @@
-package com.buschmais.jqassistant.sonar.plugin.rule;
-
-import org.sonar.check.Cardinality;
-import org.sonar.check.Priority;
-import org.sonar.check.Rule;
-
-@Rule(key = "ConstraintTemplate", name = "Constraint Template", description = "Template for user a defined constraint.", priority = Priority.CRITICAL, cardinality = Cardinality.MULTIPLE)
-public class ConstraintTemplateRule extends AbstractTemplateRule {
-}
