7030a0e7d9e6c3ed2004792706a7734dcf3ee803
Dirk Mahler <dirk.mahler@buschmais.com>
2015-12-03 15:21:25 +0100

    #301 set valid flag on POM XML descriptors

--- plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenPomXmlDescriptor.java
+++ plugin/maven/src/main/java/com/buschmais/jqassistant/plugin/maven3/api/model/MavenPomXmlDescriptor.java
@@ -1,6 +1,7 @@
 package com.buschmais.jqassistant.plugin.maven3.api.model;
 
+import com.buschmais.jqassistant.plugin.common.api.model.ValidDescriptor;
 import com.buschmais.jqassistant.plugin.xml.api.model.XmlFileDescriptor;
 
-public interface MavenPomXmlDescriptor extends MavenPomDescriptor, XmlFileDescriptor {
+public interface MavenPomXmlDescriptor extends MavenPomDescriptor, XmlFileDescriptor, ValidDescriptor {
 }
