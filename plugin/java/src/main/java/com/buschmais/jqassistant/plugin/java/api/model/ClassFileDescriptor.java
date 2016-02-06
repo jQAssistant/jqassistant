914a6aae22cbb4eaf39aa5019fe7073b9304e987
Dirk Mahler <dirk.mahler@buschmais.com>
2016-02-06 17:06:06 +0100

    #341 add property soureFileName to Java class file descriptors

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/ClassFileDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/ClassFileDescriptor.java
@@ -23,8 +23,7 @@ public interface ClassFileDescriptor extends TypeDescriptor, FileDescriptor, Dep
     /**
      * Set the super class.
      *
-     * @param superClass
-     *            The super class.
+     * @param superClass The super class.
      */
     void setSuperClass(TypeDescriptor superClass);
 
@@ -37,6 +36,20 @@ public interface ClassFileDescriptor extends TypeDescriptor, FileDescriptor, Dep
     List<TypeDescriptor> getInterfaces();
 
     /**
+     * Return the name of the source file.
+     *
+     * @return The name of the source file.
+     */
+    String getSourceFileName();
+
+    /**
+     * Set the name of the source file.
+     *
+     * @param sourceFileName The name of the source file.
+     */
+    void setSourceFileName(String sourceFileName);
+
+    /**
      * Return the byte code version of the class file.
      *
      * @return The byte code version of the class file.
@@ -46,8 +59,7 @@ public interface ClassFileDescriptor extends TypeDescriptor, FileDescriptor, Dep
     /**
      * Set the byte code version of the class file.
      *
-     * @param byteCodeVersion
-     *            The byte code version of the class file.
+     * @param byteCodeVersion The byte code version of the class file.
      */
     void setByteCodeVersion(int byteCodeVersion);
 }
