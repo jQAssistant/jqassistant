871150a4ac886b8cad54ac54b9d9f85fa9e5a974
Dirk Mahler <dirk.mahler@buschmais.com>
2016-03-17 18:04:30 +0100

    #360 provide details for test cases in "failure" or "error"

--- plugin/junit/src/test/java/com/buschmais/jqassistant/plugin/junit/test/scanner/TestReportScannerIT.java
+++ plugin/junit/src/test/java/com/buschmais/jqassistant/plugin/junit/test/scanner/TestReportScannerIT.java
@@ -1,8 +1,10 @@
 package com.buschmais.jqassistant.plugin.junit.test.scanner;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
@@ -15,6 +17,7 @@ import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
 import com.buschmais.jqassistant.plugin.common.test.scanner.MapBuilder;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
 import com.buschmais.jqassistant.plugin.junit.api.model.TestCaseDescriptor;
+import com.buschmais.jqassistant.plugin.junit.api.model.TestCaseDetailDescriptor;
 import com.buschmais.jqassistant.plugin.junit.api.model.TestSuiteDescriptor;
 import com.buschmais.jqassistant.plugin.junit.api.scanner.JunitScope;
 import com.buschmais.jqassistant.plugin.junit.test.set.report.Example;
@@ -33,7 +36,9 @@ public class TestReportScannerIT extends AbstractJavaPluginIT {
         scanClassPathResource(JunitScope.TESTREPORTS, testReportFile);
         store.beginTransaction();
         Map<String, Object> params = MapBuilder.<String, Object>create().put("fileName", testReportFile).get();
-        List<FileDescriptor> fileDescriptors = query("MATCH (suite:File) where suite.fileName={fileName} RETURN suite", params).getColumn("suite");
+        List<FileDescriptor> fileDescriptors =
+                query("MATCH (suite:JUnit:File) where suite.fileName={fileName} RETURN suite", params).getColumn
+                        ("suite");
         assertThat(fileDescriptors.size(), equalTo(1));
         FileDescriptor fileDescriptor = fileDescriptors.get(0);
         assertThat(fileDescriptor, instanceOf(TestSuiteDescriptor.class));
@@ -47,20 +52,30 @@ public class TestReportScannerIT extends AbstractJavaPluginIT {
         assertThat(testSuiteDescriptor.getTestCases().size(), equalTo(5));
         verifyTestCase("success", TestCaseDescriptor.Result.SUCCESS, 0.001f);
         verifyTestCase("inherited", TestCaseDescriptor.Result.SUCCESS, 0.002f);
-        verifyTestCase("failure", TestCaseDescriptor.Result.FAILURE, 0.003f);
-        verifyTestCase("error", TestCaseDescriptor.Result.ERROR, 0.004f);
+        TestCaseDescriptor failure = verifyTestCase("failure", TestCaseDescriptor.Result.FAILURE, 0.003f);
+        verifyDetailDescriptor(failure.getFailure(), AssertionError.class);
+        TestCaseDescriptor error = verifyTestCase("error", TestCaseDescriptor.Result.ERROR, 0.004f);
+        verifyDetailDescriptor(error.getError(), RuntimeException.class);
         verifyTestCase("skipped", TestCaseDescriptor.Result.SKIPPED, 0.005f);
         store.commitTransaction();
     }
 
-    private void verifyTestCase(String expectedName, TestCaseDescriptor.Result expectedResult, Float expectedTime) {
+    private TestCaseDescriptor verifyTestCase(String expectedName, TestCaseDescriptor.Result expectedResult, Float expectedTime) {
         List<TestCaseDescriptor> testCaseDescriptors =
-                query("MATCH (case:TestCase) WHERE case.name='" + expectedName + "' RETURN case").getColumn("case");
+                query("MATCH (case:JUnit:TestCase) WHERE case.name='" + expectedName + "' RETURN case").getColumn
+                        ("case");
         assertThat(testCaseDescriptors.size(), equalTo(1));
         TestCaseDescriptor testCaseDescriptor = testCaseDescriptors.get(0);
         assertThat(testCaseDescriptor.getName(), equalTo(expectedName));
         assertThat(testCaseDescriptor.getClassName(), equalTo(Example.class.getName()));
         assertThat(testCaseDescriptor.getTime(), equalTo(expectedTime));
         assertThat(testCaseDescriptor.getResult(), equalTo(expectedResult));
+        return testCaseDescriptor;
+    }
+
+    private void verifyDetailDescriptor(TestCaseDetailDescriptor detailDescriptor, Class<?> expectedType) {
+        assertThat(detailDescriptor, notNullValue());
+        assertThat(detailDescriptor.getType(), equalTo(expectedType.getName()));
+        assertThat(detailDescriptor.getDetails(), containsString(expectedType.getName()));
     }
 }
