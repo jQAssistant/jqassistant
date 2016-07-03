fc2f2a7dd511af52229f6c2d66a99a3ca766c9d1
Nicolas Mervaillie <nmervaillie@salto-consulting.com>
2016-07-03 17:26:05 +0200

    #382 add ''-daemon" option to CLI

--- scm/cli/src/main/java/com/buschmais/jqassistant/scm/cli/task/ServerTask.java
+++ scm/cli/src/main/java/com/buschmais/jqassistant/scm/cli/task/ServerTask.java
@@ -24,9 +24,11 @@ public class ServerTask extends AbstractTask {
 
     public static final String CMDLINE_OPTION_SERVERADDRESS = "serverAddress";
     public static final String CMDLINE_OPTION_SERVERPORT = "serverPort";
+    public static final String CMDLINE_OPTION_DAEMON = "daemon";
 
     private String serverAddress;
     private int serverPort;
+    private Boolean runAsDaemon;
 
     @Override
     protected void executeTask(final Store store) throws CliExecutionException {
@@ -38,14 +40,20 @@ public class ServerTask extends AbstractTask {
             throw new CliExecutionException("Cannot get plugins.", e);
         }
         server.start();
-        LOGGER.info("Running server");
-        LOGGER.info("Press <Enter> to finish.");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            throw new CliExecutionException("Cannot read from console.", e);
-        } finally {
-            server.stop();
+
+        if (runAsDaemon) {
+            // let the neo4j daemon do the job
+            LOGGER.info("Running server. Use <Ctrl-c> to stop server.");
+        } else {
+            LOGGER.info("Running server");
+            LOGGER.info("Press <Enter> to finish.");
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                throw new CliExecutionException("Cannot read from console.", e);
+            } finally {
+                server.stop();
+            }
         }
     }
 
@@ -56,11 +64,14 @@ public class ServerTask extends AbstractTask {
                 .create(CMDLINE_OPTION_SERVERADDRESS));
         options.add(OptionBuilder.withArgName(CMDLINE_OPTION_SERVERPORT).withDescription("The binding port of the server.").hasArgs()
                 .create(CMDLINE_OPTION_SERVERPORT));
+        options.add(OptionBuilder.withArgName(CMDLINE_OPTION_DAEMON).withDescription("Do not wait for <Enter> on standard input to stop the server.")
+                .create(CMDLINE_OPTION_DAEMON));
     }
 
     @Override
     public void withOptions(CommandLine options) {
         serverAddress = getOptionValue(options, CMDLINE_OPTION_SERVERADDRESS, ExtendedCommunityNeoServer.DEFAULT_ADDRESS);
         serverPort = Integer.valueOf(getOptionValue(options, CMDLINE_OPTION_SERVERPORT, Integer.toString(ExtendedCommunityNeoServer.DEFAULT_PORT)));
+        runAsDaemon = options.hasOption(CMDLINE_OPTION_DAEMON);
     }
 }
