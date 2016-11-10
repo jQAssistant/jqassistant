2c69735ebae4906fc1602e3382bac0413ca7c1fb
Dirk Mahler <dirk.mahler@buschmais.com>
2016-11-10 19:36:09 +0100

    simplifed scope interface

--- plugin/xml/src/main/java/com/buschmais/jqassistant/plugin/xml/api/scanner/XmlScope.java
+++ plugin/xml/src/main/java/com/buschmais/jqassistant/plugin/xml/api/scanner/XmlScope.java
@@ -1,21 +1,12 @@
 package com.buschmais.jqassistant.plugin.xml.api.scanner;
 
-import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
 import com.buschmais.jqassistant.core.scanner.api.Scope;
 
 /**
  * Defines the scopes related to XML documents.
  */
 public enum XmlScope implements Scope {
-    DOCUMENT {
-        @Override
-        public void onEnter(ScannerContext context) {
-        }
-
-        @Override
-        public void onLeave(ScannerContext context) {
-        }
-    };
+    DOCUMENT;
 
     @Override
     public String getPrefix() {
