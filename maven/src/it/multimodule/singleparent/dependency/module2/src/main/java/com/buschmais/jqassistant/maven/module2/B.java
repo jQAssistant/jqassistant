4a34acee225b7e7ebcc51081418923b9ba9f877c
dmahler <dirk.mahler@globalfoundries.com>
2017-03-06 15:17:22 +0100

    added IT to ensure type resolution between different dependent artifacts in Maven reactors

---
+++ maven/src/it/multimodule/singleparent/dependency/module2/src/main/java/com/buschmais/jqassistant/maven/module2/B.java
@@ -0,0 +1,16 @@
+package com.buschmais.jqassistant.maven;
+
+import com.buschmais.jqassistant.maven.module1.A;
+
+/**
+ * Created by dmahler on 3/6/17.
+ */
+public class B {
+
+    private A a;
+
+    public B(A a) {
+        this.a = a;
+    }
+
+}
