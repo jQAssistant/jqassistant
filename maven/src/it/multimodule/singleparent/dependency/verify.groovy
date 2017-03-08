161243c2f846c35e98891842d1f89d3c2fc8566c
Dirk Mahler <dirk.mahler@buschmais.com>
2017-03-08 08:49:06 +0100

    added IT for defaultSeverity and updated documentation

--- maven/src/it/multimodule/singleparent/dependency/verify.groovy
+++ maven/src/it/multimodule/singleparent/dependency/verify.groovy
@@ -4,5 +4,3 @@ def jqassistantReport = new XmlSlurper().parse(reportFile)
 def defaultGroup = jqassistantReport.group.find { it.@id = 'default' }
 def row = defaultGroup.concept.find { it.@id == 'test:Dependency' }.result.rows.row[0]
 assert row.column.find { it.@name == 'count' }.value == 1
-
-
--- maven/src/it/singlemodule/defaultseverity/verify.groovy
+++ maven/src/it/singlemodule/defaultseverity/verify.groovy
@@ -3,5 +3,6 @@ def reportFile = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
 assert reportFile.exists()
 def jqassistantReport = new XmlSlurper().parse(reportFile)
 def defaultGroup = jqassistantReport.group.find { it.@id = 'default' }
-assert defaultGroup.concept.find { it.@id == 'severity:Major' }.status == 'failure'
-assert defaultGroup.constraint.find { it.@id == 'severity:Blocker' }.status == 'failure'
+def constraint = defaultGroup.constraint.find { it.@id == 'severity:Blocker' }
+assert constraint.status == 'failure'
+assert constraint.severity == 'info'
