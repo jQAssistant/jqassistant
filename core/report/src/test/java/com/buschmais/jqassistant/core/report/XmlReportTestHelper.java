1d818465b54282939619c1f828daa5af51ab33fb
Dirk Mahler <dirk.mahler@buschmais.com>
2017-03-13 07:57:15 +0100

    force encoding of XML report to UTF-8

--- core/report/src/test/java/com/buschmais/jqassistant/core/report/XmlReportTestHelper.java
+++ core/report/src/test/java/com/buschmais/jqassistant/core/report/XmlReportTestHelper.java
@@ -50,6 +50,27 @@ public final class XmlReportTestHelper {
         return writer.toString();
     }
 
+    public static String createXmlWithUmlauts(String description) throws ReportException {
+        StringWriter writer = new StringWriter();
+        XmlReportWriter xmlReportWriter = new XmlReportWriter(writer);
+        xmlReportWriter.begin();
+        Concept concept = Concept.Builder.newConcept().id("mein:Konzept").description(description).severity(Severity.MAJOR)
+                .executable(new CypherExecutable("match...")).verification(new RowCountVerification()).report(Report.Builder.newInstance().primaryColumn("c2").get()).get();
+        Map<String, Severity> concepts = new HashMap<>();
+        concepts.put("mein:Konzept", Severity.INFO);
+        Group group = Group.Builder.newGroup().id("default").description("Meine Gruppe").conceptIds(concepts).get();
+        xmlReportWriter.beginGroup(group);
+        xmlReportWriter.beginConcept(concept);
+        List<Map<String, Object>> rows = new ArrayList<>();
+        rows.add(createRow());
+        Result<Concept> result = new Result<>(concept, Result.Status.SUCCESS, Severity.CRITICAL, Arrays.asList(C1, C2), rows);
+        xmlReportWriter.setResult(result);
+        xmlReportWriter.endConcept();
+        xmlReportWriter.endGroup();
+        xmlReportWriter.end();
+        return writer.toString();
+    }
+
     /**
      * Creates a test report with {@link Constraint}.
      *
