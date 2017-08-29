aba9b4845287ebd694300501ac9aea52d5e32c70
Dirk Mahler <dirk.mahler@buschmais.com>
2017-08-29 21:42:35 +0200

    fixed broken IT

--- maven/src/it/singlemodule/export-rules/verify.groovy
+++ maven/src/it/singlemodule/export-rules/verify.groovy
@@ -1,6 +1,7 @@
 def rulesFile = new File(basedir, 'target/jqassistant/jqassistant-rules.xml')
 assert rulesFile.exists()
 def rulesNode = new XmlSlurper().parse(rulesFile)
-assert rulesNode.group.size() == 2 // "default", "junit4:Default"
-assert rulesNode.constraint.size() >= 2
-assert rulesNode.concept.size() > 0
+def exportDefault = rulesNode.group.find { it.@id = 'export-default' }
+assert exportDefault != null
+assert exportDefault.includeConstraint.find { it.@refId == 'dependency:PackageCycles' } != null
+assert exportDefault.includeConstraint.find { it.@refId == 'dependency:ArtifactCycles' } != null
