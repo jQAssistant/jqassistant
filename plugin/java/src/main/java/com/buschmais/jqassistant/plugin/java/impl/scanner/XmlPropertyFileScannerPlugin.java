5fdd65dab1627d879fa0e482cd3c5fd3171e69fc
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2017-09-25 13:36:59 +0200

    [buschmais/jqa-java-plugin#1] Added a plugin for scanning XML based property files and a descriptor for such files.

---
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/impl/scanner/XmlPropertyFileScannerPlugin.java
@@ -0,0 +1,64 @@
+package com.buschmais.jqassistant.plugin.java.impl.scanner;
+
+import com.buschmais.jqassistant.core.scanner.api.Scanner;
+import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
+import com.buschmais.jqassistant.core.scanner.api.ScannerPlugin.Requires;
+import com.buschmais.jqassistant.core.scanner.api.Scope;
+import com.buschmais.jqassistant.core.store.api.Store;
+import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.model.PropertyDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin;
+import com.buschmais.jqassistant.plugin.common.api.scanner.filesystem.FileResource;
+import com.buschmais.jqassistant.plugin.java.api.model.PropertyFileDescriptor;
+import com.buschmais.jqassistant.plugin.xml.api.model.XmlDocumentDescriptor;
+import com.buschmais.jqassistant.plugin.xml.api.scanner.XMLFileFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Implementation of a {@link AbstractScannerPlugin} for XML based property files.
+ */
+@Requires({FileDescriptor.class, XmlDocumentDescriptor.class})
+public class XmlPropertyFileScannerPlugin
+    extends AbstractScannerPlugin<FileResource, PropertyFileDescriptor> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(XmlPropertyFileScannerPlugin.class);
+
+    @Override
+    public boolean accepts(FileResource item, String path, Scope scope) throws IOException {
+        boolean hasXMLExtension = path.toLowerCase().endsWith(".xml");
+
+        boolean isPropertyFile = hasXMLExtension ? XMLFileFilter.rootElementMatches(item, path, "properties")
+                                                 : false;
+
+        return hasXMLExtension && isPropertyFile;
+    }
+
+    @Override
+    public PropertyFileDescriptor scan(FileResource item, String path, Scope scope, Scanner scanner) throws IOException {
+        ScannerContext context = scanner.getContext();
+        Store store = context.getStore();
+        FileDescriptor fileDescriptor = context.getCurrentDescriptor();
+        PropertyFileDescriptor propertyFileDescriptor = store.addDescriptorType(fileDescriptor, PropertyFileDescriptor.class);
+        Properties properties = new Properties();
+        try (InputStream stream = item.createStream()) {
+            properties.loadFromXML(stream);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn("Cannot load properties from '" + path + "': " + e.getMessage());
+        }
+
+        for (String name : properties.stringPropertyNames()) {
+            String value = properties.getProperty(name);
+            PropertyDescriptor propertyDescriptor = store.create(PropertyDescriptor.class);
+            propertyDescriptor.setName(name);
+            propertyDescriptor.setValue(value);
+            propertyFileDescriptor.getProperties().add(propertyDescriptor);
+        }
+        return propertyFileDescriptor;
+    }
+
+}
