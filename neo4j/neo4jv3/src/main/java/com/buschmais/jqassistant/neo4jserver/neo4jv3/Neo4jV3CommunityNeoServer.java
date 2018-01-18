71841d87a44d91c70f72381218f4e284886a30dc
Dirk Mahler <dirk.mahler@buschmais.com>
2018-01-18 17:10:49 +0100

    upgraded to Neo4j 2.3.12 and 3.3.1

--- neo4j/neo4jv3/src/main/java/com/buschmais/jqassistant/neo4jserver/neo4jv3/Neo4jV3CommunityNeoServer.java
+++ neo4j/neo4jv3/src/main/java/com/buschmais/jqassistant/neo4jserver/neo4jv3/Neo4jV3CommunityNeoServer.java
@@ -1,12 +1,8 @@
 package com.buschmais.jqassistant.neo4jserver.neo4jv3;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.GraphDatabaseDependencies;
 import org.neo4j.kernel.configuration.Config;
@@ -15,7 +11,6 @@ import org.neo4j.kernel.impl.factory.GraphDatabaseFacadeFactory;
 import org.neo4j.logging.FormattedLogProvider;
 import org.neo4j.logging.Level;
 import org.neo4j.server.CommunityNeoServer;
-import org.neo4j.server.configuration.ServerSettings;
 import org.neo4j.server.database.Database;
 import org.neo4j.server.database.WrappedDatabase;
 
@@ -29,8 +24,6 @@ public class Neo4jV3CommunityNeoServer implements Server {
     private String httpAddress;
     private int httpPort;
 
-    private Path tempDirectory;
-
     private CommunityNeoServer communityNeoServer;
 
     public Neo4jV3CommunityNeoServer(EmbeddedGraphStore store, String address, int port) {
@@ -41,19 +34,13 @@ public class Neo4jV3CommunityNeoServer implements Server {
 
     @Override
     public void start() {
-        tempDirectory = createTempDirectory();
         Map<String, String> opts = new HashMap<>();
         // Neo4j 3.x
         opts.put("dbms.connector.http.type", "HTTP");
         opts.put("dbms.connector.http.enabled", "true");
         opts.put("dbms.connector.http.listen_address", httpAddress + ":" + httpPort);
 
-        // Neo4j 2.x/3.x
-        String sslDir = tempDirectory.toFile().getAbsolutePath() + "neo4j-home/";
-        opts.put(ServerSettings.tls_key_file.name(), sslDir + "/ssl/snakeoil.key");
-        opts.put(ServerSettings.tls_certificate_file.name(), sslDir + "/ssl/snakeoil.cert");
-
-        Config defaults = Config.empty().with(opts);
+        Config defaults = Config.defaults(opts);
         FormattedLogProvider logProvider = FormattedLogProvider.withDefaultLogLevel(Level.INFO).toOutputStream(System.out);
         GraphDatabaseDependencies graphDatabaseDependencies = GraphDatabaseDependencies.newDependencies().userLogProvider(logProvider);
         Database.Factory factory = new Database.Factory() {
@@ -66,21 +53,8 @@ public class Neo4jV3CommunityNeoServer implements Server {
         communityNeoServer.start();
     }
 
-    private Path createTempDirectory() {
-        try {
-            return Files.createTempDirectory("neo4j-home");
-        } catch (IOException e) {
-            throw new IllegalStateException("Cannot create temp directory.", e);
-        }
-    }
-
     @Override
     public void stop() {
         communityNeoServer.stop();
-        try {
-            FileUtils.deleteDirectory(tempDirectory.toFile());
-        } catch (IOException e) {
-            throw new IllegalStateException("Cannot delete temp directory.", e);
-        }
     }
 }
