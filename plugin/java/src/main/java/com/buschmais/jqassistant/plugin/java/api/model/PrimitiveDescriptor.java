1a22c2bd65b9cc677813493dd04a9b158dc5784b
Dirk Mahler <dirk.mahler@buschmais.com>
2018-04-06 16:19:07 +0200

    fixed checkstyle violations

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/PrimitiveDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/PrimitiveDescriptor.java
@@ -1,8 +1,6 @@
 package com.buschmais.jqassistant.plugin.java.api.model;
 
-import com.buschmais.jqassistant.plugin.common.api.model.ValueDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
-import com.buschmais.xo.neo4j.api.annotation.Property;
 
 /**
  * Represents a primitive value.
