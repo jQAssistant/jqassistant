440a6cb0b55bba71bba72fa5985f752e3ccf8fd0
Dirk Mahler <dirk.mahler@buschmais.com>
2018-04-10 16:21:27 +0200

    refactored Neo4j bootstrapping and added registration of APOC classes

---
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/AbstractEmbeddedNeo4jServerTest.java
@@ -0,0 +1,31 @@
+package com.buschmais.jqassistant.plugin.common.test;
+
+import com.buschmais.jqassistant.core.store.impl.EmbeddedGraphStore;
+import com.buschmais.jqassistant.neo4j.backend.bootstrap.EmbeddedNeo4jServer;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Abstract base class for server tests.
+ */
+public class AbstractEmbeddedNeo4jServerTest extends AbstractPluginIT {
+
+    public static final int SERVER_PORT = 17474;
+
+    private EmbeddedNeo4jServer server;
+
+    @Before
+    public void startServer() {
+        EmbeddedGraphStore embeddedGraphStore = (EmbeddedGraphStore) store;
+        server = embeddedGraphStore.getServer();
+        server.start(EmbeddedNeo4jServer.DEFAULT_ADDRESS, SERVER_PORT);
+    }
+
+    @After
+    public void stopServer() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+}
---
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/EmbeddedNeo4jServerMT.java
@@ -0,0 +1,17 @@
+package com.buschmais.jqassistant.plugin.common.test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * Manual test to start a server.
+ */
+public class EmbeddedNeo4jServerMT extends AbstractEmbeddedNeo4jServerTest {
+
+    @Test
+    public void server() throws IOException {
+        System.out.println("Hit Enter to continue.");
+        System.in.read();
+    }
+}
