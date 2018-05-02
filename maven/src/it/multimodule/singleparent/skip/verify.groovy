305ef40350e866a56c9ee87d0ed96d47607d4977
Dirk Mahler <dirk.mahler@buschmais.com>
2018-05-02 11:03:02 +0200

    improved Maven plugin IT for skipping modules

--- maven/src/it/multimodule/singleparent/skip/verify.groovy
+++ maven/src/it/multimodule/singleparent/skip/verify.groovy
@@ -1,2 +1,5 @@
 def reportFile = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
 assert reportFile.exists()
+def mavenModules = report.group.concept.find { it.@id == "test:MavenModules" }
+assert mavenModules != null
+assert mavenModules.result.rows['@count'] == "2";
