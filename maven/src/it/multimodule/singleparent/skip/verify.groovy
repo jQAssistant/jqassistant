315c8b43bddf2485d6a2fa09f3eedae38af44247
Dirk Mahler <dirk.mahler@buschmais.com>
2018-05-04 12:47:27 +0200

    fixed IT

--- maven/src/it/multimodule/singleparent/skip/verify.groovy
+++ maven/src/it/multimodule/singleparent/skip/verify.groovy
@@ -1,5 +1,6 @@
 def reportFile = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
 assert reportFile.exists()
+def report = new XmlSlurper().parse(reportFile)
 def mavenModules = report.group.concept.find { it.@id == "test:MavenModules" }
 assert mavenModules != null
 assert mavenModules.result.rows['@count'] == "2";
