261db1727ff324530d0d2fa18db86519b1ca6359
Dirk Mahler <dirk.mahler@buschmais.com>
2018-05-11 15:32:59 +0200

    added @Abstract label to Enum, Member and Primitive descriptors

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/MemberDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/MemberDescriptor.java
@@ -2,6 +2,7 @@ package com.buschmais.jqassistant.plugin.java.api.model;
 
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
 import com.buschmais.jqassistant.plugin.common.api.model.NamedDescriptor;
+import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 import static com.buschmais.xo.neo4j.api.annotation.Relation.Incoming;
@@ -10,6 +11,7 @@ import static com.buschmais.xo.neo4j.api.annotation.Relation.Incoming;
  * Defines a member of Java type.
  */
 @Label("Member")
+@Abstract
 public interface MemberDescriptor extends JavaByteCodeDescriptor, NamedDescriptor, SignatureDescriptor, AnnotatedDescriptor, AccessModifierDescriptor, Descriptor {
 
     /**
--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/PrimitiveDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/PrimitiveDescriptor.java
@@ -1,10 +1,12 @@
 package com.buschmais.jqassistant.plugin.java.api.model;
 
+import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 /**
  * Represents a primitive value.
  */
 @Label("Primitive")
+@Abstract
 public interface PrimitiveDescriptor extends JavaByteCodeDescriptor {
 }
