1eb2e54696dc4c4a96b52bfa889a4776c87f115d
Dirk Mahler <dirk.mahler@buschmais.com>
2018-05-12 18:00:13 +0200

    simplified ReportIT

--- cli/test/src/test/java/com/buschmais/jqassistant/commandline/test/ReportIT.java
+++ cli/test/src/test/java/com/buschmais/jqassistant/commandline/test/ReportIT.java
@@ -1,5 +1,8 @@
 package com.buschmais.jqassistant.commandline.test;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -7,9 +10,6 @@ import com.buschmais.jqassistant.commandline.Task;
 import com.buschmais.jqassistant.commandline.task.ReportTask;
 
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 /**
  * Verifies command line reporting.
@@ -23,11 +23,9 @@ public class ReportIT extends com.buschmais.jqassistant.commandline.test.Abstrac
     @Test
     public void report() throws IOException, InterruptedException {
         String rulesDirectory = ReportIT.class.getResource("/rules").getFile();
-        String[] args1 = new String[] { "analyze", "-r", rulesDirectory };
-        assertThat(execute(args1).getExitCode(), equalTo(2));
+        String[] args1 = new String[] { "analyze", "report", "-r", rulesDirectory, "-concepts", "default:TestConcept" };
+        assertThat(execute(args1).getExitCode(), equalTo(0));
         assertThat(new File(getDefaultReportDirectory(), Task.REPORT_FILE_XML).exists(), equalTo(true));
-        String[] args2 = new String[] { "report" };
-        assertThat(execute(args2).getExitCode(), equalTo(0));
         assertThat(new File(getDefaultReportDirectory(), ReportTask.REPORT_FILE_HTML).exists(), equalTo(true));
     }
 
