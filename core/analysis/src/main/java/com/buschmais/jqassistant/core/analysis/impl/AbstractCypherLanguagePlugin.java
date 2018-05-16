179b16507778680aa2332571ace2669ec7978a41
Dirk Mahler <dirk.mahler@buschmais.com>
2018-05-16 18:17:08 +0200

    allow overriding of determining result status in AbstractCypherLanguagePlugin

--- core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/impl/AbstractCypherLanguagePlugin.java
+++ core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/impl/AbstractCypherLanguagePlugin.java
@@ -14,6 +14,8 @@ import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
 import com.buschmais.jqassistant.core.analysis.api.rule.Severity;
 import com.buschmais.xo.api.Query;
 
+import static com.buschmais.jqassistant.core.analysis.api.Result.Status;
+
 /**
  * Abstract base class for {@link RuleLanguagePlugin}s executing cypher queries.
  */
@@ -23,8 +25,8 @@ public abstract class AbstractCypherLanguagePlugin extends AbstractRuleLanguageP
             AnalyzerContext context) throws RuleException {
         List<Map<String, Object>> rows = new ArrayList<>();
         context.getLogger().debug("Executing query '" + cypher + "' with parameters [" + parameters + "]");
+        List<String> columnNames = null;
         try (Query.Result<Query.Result.CompositeRowObject> compositeRowObjects = context.getStore().executeQuery(cypher, parameters)) {
-            List<String> columnNames = null;
             for (Query.Result.CompositeRowObject rowObject : compositeRowObjects) {
                 if (columnNames == null) {
                     columnNames = new ArrayList<>(rowObject.getColumns());
@@ -35,11 +37,33 @@ public abstract class AbstractCypherLanguagePlugin extends AbstractRuleLanguageP
                 }
                 rows.add(row);
             }
-            Result.Status status = context.verify(executableRule, columnNames, rows, context);
-            return new Result<>(executableRule, status, severity, columnNames, rows);
         } catch (Exception e) {
             throw new RuleException("Cannot execute query for rule '" + executableRule + "'.", e);
         }
+        Status status = getStatus(executableRule, columnNames, rows, context);
+        return Result.<T>builder().rule(executableRule).status(status).severity(severity).columnNames(columnNames).rows(rows).build();
+    }
+
+    /**
+     * Evaluate the status of the result, may be overridden by sub-classes.
+     *
+     * @param executableRule
+     *            The {@link ExecutableRule}.
+     * @param columnNames
+     *            The column names.
+     * @param rows
+     *            The rows.
+     * @param context
+     *            The {@link AnalyzerContext}.
+     * @param <T>
+     *            The rule type.
+     * @return The {@link Status}.
+     * @throws RuleException
+     *             If evaluation fails.
+     */
+    protected <T extends ExecutableRule<?>> Status getStatus(T executableRule, List<String> columnNames, List<Map<String, Object>> rows,
+            AnalyzerContext context) throws RuleException {
+        return context.verify(executableRule, columnNames, rows, context);
     }
 
 }
