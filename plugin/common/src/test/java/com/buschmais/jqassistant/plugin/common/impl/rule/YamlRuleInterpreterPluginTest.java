c67df9c9f3a3e5f45e0a763e5453ac1234a7e7d6
Dirk Mahler <dirk.mahler@buschmais.com>
2018-10-06 00:19:59 +0200

    added YamlRuleInterpreterPlugin

---
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/impl/rule/YamlRuleInterpreterPluginTest.java
@@ -0,0 +1,86 @@
+package com.buschmais.jqassistant.plugin.common.impl.rule;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.buschmais.jqassistant.core.analysis.api.AnalyzerContext;
+import com.buschmais.jqassistant.core.analysis.api.Result;
+import com.buschmais.jqassistant.core.analysis.api.rule.Concept;
+import com.buschmais.jqassistant.core.analysis.api.rule.ExecutableRule;
+import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
+import com.buschmais.jqassistant.core.analysis.api.rule.Severity;
+import com.buschmais.jqassistant.core.rule.impl.SourceExecutable;
+import com.buschmais.jqassistant.plugin.common.api.rule.JavaRule;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyMap;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+
+@RunWith(MockitoJUnitRunner.class)
+public class YamlRuleInterpreterPluginTest {
+
+    @Mock
+    private AnalyzerContext analyzerContext;
+
+    private YamlRuleInterpreterPlugin plugin = new YamlRuleInterpreterPlugin();
+
+    @Before
+    public void setUp() {
+        doAnswer(invocation -> {
+            ExecutableRule<?> rule = (ExecutableRule<?>) invocation.getArguments()[0];
+            Severity severity = (Severity) invocation.getArguments()[1];
+            return Result.builder().rule(rule).severity(severity);
+        }).when(analyzerContext).resultBuilder(any(ExecutableRule.class), any(Severity.class));
+        plugin.initialize();
+        plugin.configure(emptyMap());
+    }
+
+    @Test
+    public void javaRule() throws RuleException {
+        String source = "java-rule: " + TestJavaRule.class.getName() + "\nconfiguration:\n  testProperty: testValue";
+        SourceExecutable<String> executable = new SourceExecutable("yaml", source, String.class);
+        Concept concept = Concept.builder().id("test-java-rule").executable(executable).severity(Severity.MINOR).build();
+
+        Result<Concept> result = plugin.execute(concept, emptyMap(), Severity.MAJOR, analyzerContext);
+
+        assertThat(result, notNullValue());
+        assertThat(result.getRule(), is(concept));
+        assertThat(result.getSeverity(), is(Severity.MAJOR));
+        assertThat(result.getStatus(), equalTo(Result.Status.SUCCESS));
+        assertThat(result.getColumnNames(), equalTo(asList("Property", "Value")));
+        List<Map<String, Object>> rows = result.getRows();
+        assertThat(rows.size(), equalTo(1));
+        Map<String, Object> row = rows.get(0);
+        assertThat(row.get("Property"), equalTo("testProperty"));
+        assertThat(row.get("Value"), equalTo("testValue"));
+    }
+
+    /**
+     * Test {@link JavaRule} that returns the given configuration.
+     */
+    public static class TestJavaRule implements JavaRule {
+        @Override
+        public <T extends ExecutableRule<?>> Result<T> execute(T executableRule, Map<String, Object> configuration, Map<String, Object> ruleParameters,
+                Severity severity, AnalyzerContext context) {
+            List<Map<String, Object>> rows = new ArrayList<>();
+            for (Map.Entry<String, Object> entry : configuration.entrySet()) {
+                Map<String, Object> row = new HashMap<>();
+                row.put("Property", entry.getKey());
+                row.put("Value", entry.getValue());
+                rows.add(row);
+            }
+            return context.resultBuilder(executableRule, severity).status(Result.Status.SUCCESS).columnNames(asList("Property", "Value")).rows(rows).build();
+        }
+    }
+}
---
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/rule/YamlRuleIT.java
@@ -0,0 +1,54 @@
+package com.buschmais.jqassistant.plugin.common.test.rule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.buschmais.jqassistant.core.analysis.api.AnalyzerContext;
+import com.buschmais.jqassistant.core.analysis.api.Result;
+import com.buschmais.jqassistant.core.analysis.api.rule.ExecutableRule;
+import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
+import com.buschmais.jqassistant.core.analysis.api.rule.Severity;
+import com.buschmais.jqassistant.plugin.common.api.model.PropertyDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.rule.JavaRule;
+import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
+
+import org.junit.Test;
+
+import static com.buschmais.jqassistant.core.analysis.api.Result.Status.SUCCESS;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Verifies YAML rule execution.
+ */
+public class YamlRuleIT extends AbstractPluginIT {
+
+    @Test
+    public void adocJavaRule() throws RuleException {
+        verify("adoc-java-rule:Concept");
+    }
+
+    @Test
+    public void xmlJavaRule() throws RuleException {
+        verify("xml-java-rule:Concept");
+    }
+
+    private void verify(String conceptId) throws RuleException {
+        assertThat(applyConcept(conceptId).getStatus(), equalTo(SUCCESS));
+    }
+
+    public static class Concept implements JavaRule {
+
+        @Override
+        public <T extends ExecutableRule<?>> Result<T> execute(T executableRule, Map<String, Object> properties, Map<String, Object> ruleParameters,
+                Severity severity, AnalyzerContext context) {
+            PropertyDescriptor propertyDescriptor = context.getStore().create(PropertyDescriptor.class);
+            propertyDescriptor.setName("testProperty");
+            propertyDescriptor.setValue("testValue");
+            Map<String, Object> row = new HashMap<>();
+            row.put("Property", propertyDescriptor);
+            return context.resultBuilder(executableRule, severity).columnNames(singletonList("Property")).rows(singletonList(row)).status(SUCCESS).build();
+        }
+    }
+}
