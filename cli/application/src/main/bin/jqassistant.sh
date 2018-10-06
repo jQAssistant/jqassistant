f6dd0781dd1d2e240c361fed44c335690b9b139e
Dirk Mahler <dirk.mahler@buschmais.com>
2018-10-06 08:16:02 +0200

    fixed Linux start script for CLI

--- cli/application/src/main/bin/jqassistant.sh
+++ cli/application/src/main/bin/jqassistant.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 BIN_DIR=`dirname "$0"`
-JQASSISTANT_HOME=`cd "$BIN_DIR/.." && pwd -P`
+export JQASSISTANT_HOME=`cd "$BIN_DIR/.." && pwd -P`
 java $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/${project.groupId}-${project.artifactId}-${project.version}.${project.packaging}" $*
 
