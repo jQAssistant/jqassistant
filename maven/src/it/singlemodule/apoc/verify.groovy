24f86123cecc1ba7c73d6c6ddc7ce5c65f7bbc08
Dirk Mahler <dirk.mahler@buschmais.com>
2018-10-14 08:32:33 +0200

    added IT for APOC configuration

---
+++ maven/src/it/singlemodule/apoc/verify.groovy
@@ -0,0 +1,11 @@
+def reportFile = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
+assert reportFile.exists()
+def report = new XmlSlurper().parse(reportFile)
+
+verifyConcept(report, "test:APOC")
+
+def verifyConcept(report, conceptId) {
+    def conceptResult = report.group.concept.find { it.@id == conceptId }
+    assert conceptResult != null
+    assert conceptResult.status == "success";
+}
