c16aedf4782145fb32e80788dee3461b94441a4c
Dirk Mahler <dirk.mahler@buschmais.com>
2018-10-27 18:47:46 +0200

    make plugins aware of their lifecycle to allow initialization and destruction

--- maven/src/main/java/com/buschmais/jqassistant/scm/maven/provider/PluginRepositoryProvider.java
+++ maven/src/main/java/com/buschmais/jqassistant/scm/maven/provider/PluginRepositoryProvider.java
@@ -3,28 +3,45 @@ package com.buschmais.jqassistant.scm.maven.provider;
 
 import javax.inject.Singleton;
 
-import com.buschmais.jqassistant.core.plugin.api.*;
+import com.buschmais.jqassistant.core.plugin.api.PluginConfigurationReader;
+import com.buschmais.jqassistant.core.plugin.api.PluginRepository;
+import com.buschmais.jqassistant.core.plugin.api.PluginRepositoryException;
 import com.buschmais.jqassistant.core.plugin.impl.PluginConfigurationReaderImpl;
 import com.buschmais.jqassistant.core.plugin.impl.PluginRepositoryImpl;
 
-import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Disposable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
-public class PluginRepositoryProvider {
+public class PluginRepositoryProvider implements Initializable, Disposable {
 
-    private PluginConfigurationReader pluginConfigurationReader = new PluginConfigurationReaderImpl();
+    private static final Logger LOGGER = LoggerFactory.getLogger(PluginRepositoryProvider.class);
 
     private PluginRepository pluginRepository;
 
-    PluginRepositoryProvider() throws MojoExecutionException {
+    public void initialize() {
         try {
+            PluginConfigurationReader pluginConfigurationReader = new PluginConfigurationReaderImpl();
             pluginRepository = new PluginRepositoryImpl(pluginConfigurationReader);
+            this.pluginRepository.initialize();
         } catch (PluginRepositoryException e) {
-            throw new MojoExecutionException("Cannot create plugin repository.", e);
+            LOGGER.warn("Cannot initialize plugin repository.", e);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        try {
+            this.pluginRepository.destroy();
+        } catch (PluginRepositoryException e) {
+            LOGGER.warn("Cannot destroy plugin repository.", e);
         }
     }
 
     public PluginRepository getPluginRepository() {
         return pluginRepository;
     }
+
 }
