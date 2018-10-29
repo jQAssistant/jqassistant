2236b87eddd022cfe095442098ad5a495cfc3616
Dirk Mahler <dirk.mahler@buschmais.com>
2018-10-29 06:39:54 +0100

    added rules regarding registration of XO model types in jQA plugin descriptor

--- plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/JavaByteCodeDescriptor.java
+++ plugin/java/src/main/java/com/buschmais/jqassistant/plugin/java/api/model/JavaByteCodeDescriptor.java
@@ -1,12 +1,10 @@
 package com.buschmais.jqassistant.plugin.java.api.model;
 
 import com.buschmais.xo.api.annotation.Abstract;
-import com.buschmais.xo.neo4j.api.annotation.Label;
 
 /**
- * Defines label "ByteCode"
+ * Defines the combination of labels "Java" and "ByteCode".
  */
 @Abstract
-@Label("ByteCode")
-public interface JavaByteCodeDescriptor extends JavaDescriptor {
+public interface JavaByteCodeDescriptor extends JavaDescriptor, ByteCodeDescriptor {
 }
