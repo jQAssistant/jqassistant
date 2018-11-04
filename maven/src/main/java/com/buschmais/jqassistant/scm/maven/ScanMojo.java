0b31e8ae42c91c67622e22a64d2a3f8a9bb8ec50
Dirk Mahler <dirk.mahler@buschmais.com>
2018-11-04 14:55:41 +0100

    resolve full dependency while scanning Maven projects

--- maven/src/main/java/com/buschmais/jqassistant/scm/maven/ScanMojo.java
+++ maven/src/main/java/com/buschmais/jqassistant/scm/maven/ScanMojo.java
@@ -18,23 +18,24 @@ import com.buschmais.jqassistant.core.store.api.Store;
 import com.buschmais.jqassistant.plugin.maven3.api.scanner.MavenScope;
 import com.buschmais.jqassistant.plugin.maven3.api.scanner.ScanInclude;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder;
 
 /**
  * Scans the the output directory and test output directory.
  */
-@Mojo(name = "scan", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST, threadSafe = true,
-      configurator = "custom")
+@Mojo(name = "scan", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST, threadSafe = true, configurator = "custom")
 public class ScanMojo extends AbstractModuleMojo {
 
     /**
-     * Specifies a list of directory names relative to the root module
-     * containing additional rule files.
+     * Specifies a list of directory names relative to the root module containing
+     * additional rule files.
      */
     @Parameter(property = "jqassistant.scan.includes")
     protected List<ScanInclude> scanIncludes;
@@ -56,6 +57,12 @@ public class ScanMojo extends AbstractModuleMojo {
      */
     @Parameter(property = "jqassistant.store.reset")
     protected boolean reset = true;
+
+    @Parameter(defaultValue = "${session}", readonly = true, required = true)
+    private MavenSession session;
+
+    @Component(hint = "default")
+    private DependencyGraphBuilder dependencyGraphBuilder;
 
     @Override
     protected boolean isResetStoreBeforeExecution() {
@@ -82,11 +89,23 @@ public class ScanMojo extends AbstractModuleMojo {
     }
 
     @Override
-    public void execute(MavenProject mavenProject, Store store) throws MojoExecutionException, MojoFailureException {
+    public void execute(MavenProject mavenProject, Store store) throws MojoExecutionException {
         ScannerConfiguration configuration = new ScannerConfiguration();
         configuration.setContinueOnError(continueOnError);
         ScannerContext scannerContext = new ScannerContextImpl(store);
+        scannerContext.push(MavenSession.class, session);
+        scannerContext.push(DependencyGraphBuilder.class, dependencyGraphBuilder);
         PluginRepository pluginRepository = pluginRepositoryProvider.getPluginRepository();
+        Map<String, ScannerPlugin<?, ?>> scannerPlugins = getStringScannerPlugins(pluginRepository, scannerContext);
+        ScopePluginRepository scopePluginRepository = pluginRepository.getScopePluginRepository();
+        Scanner scanner = new ScannerImpl(configuration, scannerContext, scannerPlugins, scopePluginRepository.getScopes());
+        scanner.scan(mavenProject, mavenProject.getFile().getAbsolutePath(), MavenScope.PROJECT);
+        scannerContext.pop(DependencyGraphBuilder.class);
+        scannerContext.pop(MavenSession.class);
+    }
+
+    private Map<String, ScannerPlugin<?, ?>> getStringScannerPlugins(PluginRepository pluginRepository, ScannerContext scannerContext)
+            throws MojoExecutionException {
         ScannerPluginRepository scannerPluginRepository = pluginRepository.getScannerPluginRepository();
         Map<String, ScannerPlugin<?, ?>> scannerPlugins;
         try {
@@ -94,13 +113,6 @@ public class ScanMojo extends AbstractModuleMojo {
         } catch (PluginRepositoryException e) {
             throw new MojoExecutionException("Cannot determine scanner plugins.", e);
         }
-        ScopePluginRepository scopePluginRepository = pluginRepository.getScopePluginRepository();
-        Scanner scanner = new ScannerImpl(configuration, scannerContext, scannerPlugins, scopePluginRepository.getScopes());
-        store.beginTransaction();
-        try {
-            scanner.scan(mavenProject, mavenProject.getFile().getAbsolutePath(), MavenScope.PROJECT);
-        } finally {
-            store.commitTransaction();
-        }
+        return scannerPlugins;
     }
 }
