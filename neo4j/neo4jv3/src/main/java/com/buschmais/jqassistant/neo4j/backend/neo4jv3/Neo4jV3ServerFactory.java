24d7ee59ecd70bc9dd1158d5afe19ce69de19414
Dirk Mahler <dirk.mahler@buschmais.com>
2019-01-11 11:17:20 +0100

    removed Neo4j v2 backend

--- neo4j/neo4jv3/src/main/java/com/buschmais/jqassistant/neo4j/backend/neo4jv3/Neo4jV3ServerFactory.java
+++ neo4j/neo4jv3/src/main/java/com/buschmais/jqassistant/neo4j/backend/neo4jv3/Neo4jV3ServerFactory.java
@@ -11,6 +11,8 @@ public class Neo4jV3ServerFactory extends AbstractEmbeddedNeo4jServerFactory {
     private static final String PROPERTY_NEO4J_DBMS_CONNECTOR_BOLT_ENABLED = "neo4j.dbms.connector.bolt.enabled";
     private static final String PROPERTY_NEO4J_DBMS_CONNECTOR_BOLT_LISTEN_ADDRESS = "neo4j.dbms.connector.bolt.listen_address";
     private static final String PROPERTY_NEO4J_DBMS_SECURITY_PROCEDURES_UNRESTRICTED = "neo4j.dbms.security.procedures.unrestricted";
+    private static final String PROPERTY_NEO4J_DBMS_TX_LOG_ROTATION_SIZE = "neo4j.dbms.tx_log.rotation.size";
+    private static final String PROPERTY_NEO4J_DBMS_TX_LOG_ROTATION_RETENTION_POLICY = "neo4j.dbms.tx_log.rotation.retention_policy";
 
     @Override
     public EmbeddedNeo4jServer getServer() {
@@ -23,6 +25,8 @@ public class Neo4jV3ServerFactory extends AbstractEmbeddedNeo4jServerFactory {
         commonProperties.putIfAbsent(PROPERTY_NEO4J_DBMS_CONNECTOR_BOLT_ENABLED, Boolean.toString(embedded.isConnectorEnabled()));
         commonProperties.putIfAbsent(PROPERTY_NEO4J_DBMS_CONNECTOR_BOLT_LISTEN_ADDRESS, embedded.getListenAddress() + ":" + embedded.getBoltPort());
         commonProperties.putIfAbsent(PROPERTY_NEO4J_DBMS_SECURITY_PROCEDURES_UNRESTRICTED, "apoc.*");
+        commonProperties.putIfAbsent(PROPERTY_NEO4J_DBMS_TX_LOG_ROTATION_SIZE, "50M");
+        commonProperties.putIfAbsent(PROPERTY_NEO4J_DBMS_TX_LOG_ROTATION_RETENTION_POLICY, Boolean.FALSE.toString());
         return commonProperties;
     }
 }
