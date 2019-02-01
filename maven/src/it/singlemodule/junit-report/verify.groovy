b99f14645808a87f867be4142c728e9942c3fd75
Dirk Mahler <dirk.mahler@buschmais.com>
2019-02-02 00:10:44 +0100

    changed test suite names for JUnit reports

--- maven/src/it/singlemodule/junit-report/verify.groovy
+++ maven/src/it/singlemodule/junit-report/verify.groovy
@@ -1,4 +1,4 @@
-def reportFile = new File(basedir, 'target/jqassistant/report/junit/TEST-jQAssistant-nestedGroup.xml')
+def reportFile = new File(basedir, 'target/jqassistant/report/junit/TEST-jqassistant.Group_nestedGroup.xml')
 assert reportFile.exists()
 def testsuiteNode = new XmlSlurper().parse(reportFile)
 assert testsuiteNode.testcase.size() >= 2
