c315575258c4ceca9a28cd555b372838c6636361
Dirk Mahler <dirk.mahler@buschmais.com>
2019-03-07 17:31:15 +0100

    upgraded APOC library to 3.5.0.2

--- neo4j/neo4jv3/src/main/java/com/buschmais/jqassistant/neo4j/backend/neo4jv3/APOCActivator.java
+++ neo4j/neo4jv3/src/main/java/com/buschmais/jqassistant/neo4j/backend/neo4jv3/APOCActivator.java
@@ -30,18 +30,19 @@ public class APOCActivator {
             apoc.generate.Generate.class, apoc.gephi.Gephi.class, apoc.get.Get.class, apoc.graph.Graphs.class, apoc.help.Help.class,
             apoc.index.FreeTextSearch.class, apoc.index.FulltextIndex.class, apoc.index.SchemaIndex.class, apoc.load.LoadHtml.class, apoc.load.Jdbc.class,
             apoc.load.LoadCsv.class, apoc.load.LoadJson.class, apoc.load.LoadLdap.class, apoc.load.LoadXls.class, apoc.load.Xml.class, apoc.lock.Lock.class,
-            apoc.log.Logging.class, apoc.math.Regression.class, apoc.merge.Merge.class, apoc.meta.Meta.class, apoc.mongodb.MongoDB.class,
-            apoc.monitor.Ids.class, apoc.monitor.Kernel.class, apoc.monitor.Locks.class, apoc.monitor.Store.class, apoc.monitor.Transaction.class,
-            apoc.neighbors.Neighbors.class, apoc.nodes.Grouping.class, apoc.nodes.Nodes.class, apoc.path.PathExplorer.class, apoc.periodic.Periodic.class,
-            apoc.refactor.GraphRefactoring.class, apoc.refactor.rename.Rename.class, apoc.schema.Schemas.class, apoc.search.ParallelNodeSearch.class,
-            apoc.spatial.Distance.class, apoc.spatial.Geocode.class, apoc.stats.DegreeDistribution.class, apoc.text.Phonetic.class, apoc.trigger.Trigger.class,
-            apoc.util.Utils.class, apoc.warmup.Warmup.class);
+            apoc.log.Logging.class, apoc.log.Neo4jLogStream.class, apoc.math.Regression.class, apoc.merge.Merge.class, apoc.meta.Meta.class,
+            apoc.metrics.Metrics.class, apoc.model.Model.class, apoc.mongodb.MongoDB.class, apoc.monitor.Ids.class, apoc.monitor.Kernel.class,
+            apoc.monitor.Locks.class, apoc.monitor.Store.class, apoc.monitor.Transaction.class, apoc.neighbors.Neighbors.class, apoc.nodes.Grouping.class,
+            apoc.nodes.Nodes.class, apoc.path.PathExplorer.class, apoc.periodic.Periodic.class, apoc.refactor.GraphRefactoring.class,
+            apoc.refactor.rename.Rename.class, apoc.schema.Schemas.class, apoc.search.ParallelNodeSearch.class, apoc.spatial.Distance.class,
+            apoc.spatial.Geocode.class, apoc.stats.DegreeDistribution.class, apoc.text.Phonetic.class, apoc.trigger.Trigger.class, apoc.util.Utils.class,
+            apoc.warmup.Warmup.class);
     private final List<Class<?>> FUNCTION_TYPES = asList(apoc.algo.Similarity.class, apoc.bitwise.BitwiseOperations.class, apoc.coll.Coll.class,
             apoc.convert.Convert.class, apoc.convert.Json.class, apoc.create.Create.class, apoc.cypher.CypherFunctions.class, apoc.data.Extract.class,
-            apoc.data.email.ExtractEmail.class, apoc.data.url.ExtractURL.class, apoc.date.Date.class, apoc.diff.Diff.class, apoc.label.Label.class,
-            apoc.map.Maps.class, apoc.math.Maths.class, apoc.meta.Meta.class, apoc.nodes.Nodes.class, apoc.number.ArabicRoman.class, apoc.number.Numbers.class,
-            apoc.number.exact.Exact.class, apoc.path.Paths.class, apoc.schema.Schemas.class, apoc.scoring.Scoring.class, apoc.text.Strings.class,
-            apoc.temporal.TemporalProcedures.class, apoc.trigger.Trigger.class, apoc.util.Utils.class, apoc.version.Version.class);
+            apoc.data.email.ExtractEmail.class, apoc.data.url.ExtractURL.class, apoc.date.Date.class, apoc.diff.Diff.class, apoc.hashing.Fingerprinting.class,
+            apoc.label.Label.class, apoc.map.Maps.class, apoc.math.Maths.class, apoc.meta.Meta.class, apoc.nodes.Nodes.class, apoc.number.ArabicRoman.class,
+            apoc.number.Numbers.class, apoc.number.exact.Exact.class, apoc.path.Paths.class, apoc.schema.Schemas.class, apoc.scoring.Scoring.class,
+            apoc.text.Strings.class, apoc.temporal.TemporalProcedures.class, apoc.trigger.Trigger.class, apoc.util.Utils.class, apoc.version.Version.class);
 
     private GraphDatabaseAPI graphDatabaseAPI;
 
