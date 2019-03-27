522fc2068ee3fa557816d57c11af88f36d4ab7cf
Dirk Mahler <dirk.mahler@buschmais.com>
2019-03-27 22:35:13 +0100

    fixed duplicate DEPENDS_ON relations between artifacts created for Maven modules

--- plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/ArtifactDescriptor.java
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/ArtifactDescriptor.java
@@ -3,6 +3,9 @@ package com.buschmais.jqassistant.plugin.common.api.model;
 import java.util.List;
 
 import com.buschmais.jqassistant.core.store.api.model.FullQualifiedNameDescriptor;
+import com.buschmais.xo.api.annotation.ResultOf;
+import com.buschmais.xo.api.annotation.ResultOf.Parameter;
+import com.buschmais.xo.neo4j.api.annotation.Cypher;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Property;
 import com.buschmais.xo.neo4j.api.annotation.Relation.Incoming;
@@ -70,4 +73,18 @@ public interface ArtifactDescriptor extends NamedDescriptor, FullQualifiedNameDe
 
     @Incoming
     List<DependsOnDescriptor> getDependents();
+
+    /**
+     * Create a dependency to another {@link ArtifactDescriptor}.
+     * 
+     * @param dependency
+     *            The {@link ArtifactDescriptor} representing the dependency.
+     * @param scope
+     *            The scope.
+     * @param optional
+     *            <code>true</code> if the dependency is optional.
+     */
+    @ResultOf
+    @Cypher("MATCH (artifact:Artifact),(dependency:Artifact) WHERE id(artifact)={this} and id(dependency)={dependency} MERGE (artifact)-[dependsOn:DEPENDS_ON{scope:{scope},optional:{optional}}]->(dependency) RETURN dependsOn")
+    void addDependency(@Parameter("dependency") ArtifactDescriptor dependency, @Parameter("scope") String scope, @Parameter("optional") boolean optional);
 }
