e28b72bde4cb2d08cdedeb28c7fc9b96b751c39c
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2019-06-01 10:33:18 +0200

    [buschmais/jqa-yaml2-plugin#1] Add test classes for the added examples.

---
+++ plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/spec12/ExampleC2E05Test.java
@@ -0,0 +1,13 @@
+package com.buschmais.jqassistant.plugin.yaml2.impl.scanner.spec12;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class ExampleC2E05Test {
+    @Disabled("Test and scanner are not yet implemented.")
+    @Test
+    void scannerCanReadDocument() {
+        throw new RuntimeException("Please implement me!");
+    }
+
+}
---
+++ plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/spec12/ExampleC2E25Test.java
@@ -0,0 +1,12 @@
+package com.buschmais.jqassistant.plugin.yaml2.impl.scanner.spec12;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class ExampleC2E25Test {
+    @Disabled("Test and scanner are not yet implemented.")
+    @Test
+    void scannerCanReadDocument() {
+        throw new RuntimeException("Please implement me!");
+    }
+}
