6dfc0fffc0c96f7b27f6b06489e7fbbf26915b0f
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2019-12-08 10:44:39 +0100

    [jqassistant/jqa-yaml2-plugin#1] Fixed wrong label on one descriptor

--- plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YAML2AnkerDescriptor.java
+++ plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YAML2AnkerDescriptor.java
@@ -2,6 +2,6 @@ package com.buschmais.jqassistant.plugin.yaml2.api.model;
 
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
-@Label("Reference")
+@Label("Anker")
 public interface YAML2AnkerDescriptor extends YAML2Descriptor {
 }
