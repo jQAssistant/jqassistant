a222122b65b72ef1f0ece455fc459509f98ef0c3
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2019-12-23 20:01:33 +0100

    [jqassistant/jqa-yaml2-plugin#1] Added descriptors for simple and complex YAML keys

---
+++ plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YMLCompleyKeyDescriptor.java
@@ -0,0 +1,7 @@
+package com.buschmais.jqassistant.plugin.yaml2.api.model;
+
+import com.buschmais.xo.neo4j.api.annotation.Label;
+
+@Label("Complex")
+public interface YMLCompleyKeyDescriptor extends YMLKeyDescriptor {
+}
