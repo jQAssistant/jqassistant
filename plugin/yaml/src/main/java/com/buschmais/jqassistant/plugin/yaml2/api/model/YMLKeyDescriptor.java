3d64da8c4a0b065f92b9bf721f97b1f25b3fc112
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2019-12-28 21:35:35 +0100

    [jqassistant/jqa-yaml2-plugin#1] Moved annotation Property from setters to getters

--- plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YMLKeyDescriptor.java
+++ plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YMLKeyDescriptor.java
@@ -1,12 +1,13 @@
 package com.buschmais.jqassistant.plugin.yaml2.api.model;
 
+import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Property;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
+@Abstract
 @Label("Key")
 public interface YMLKeyDescriptor extends YMLDescriptor {
-
     @Property("name")
     String getName();
 
--- plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YMLScalarDescriptor.java
+++ plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YMLScalarDescriptor.java
@@ -5,7 +5,6 @@ import com.buschmais.xo.neo4j.api.annotation.Property;
 
 @Label("Scalar")
 public interface YMLScalarDescriptor extends YMLDescriptor {
-
     @Property("value")
     String getValue();
 
