834a48aed43be7a8254afa3a2e3bdd6e96c4050c
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2020-01-05 23:59:39 +0100

    [jqassistant/jqa-yaml2-plugin#1] Enabled more tests.

--- plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/spec12/ExampleC2E14IT.java
+++ plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/spec12/ExampleC2E14IT.java
@@ -1,6 +1,15 @@
 package com.buschmais.jqassistant.plugin.yaml2.impl.scanner.spec12;
 
+import java.util.function.Consumer;
+
+import com.buschmais.jqassistant.plugin.yaml2.api.model.YMLDocumentDescriptor;
+import com.buschmais.jqassistant.plugin.yaml2.api.model.YMLFileDescriptor;
+import com.buschmais.jqassistant.plugin.yaml2.api.model.YMLScalarDescriptor;
+
 import org.junit.jupiter.api.Test;
+
+import static com.buschmais.jqassistant.plugin.yaml2.helper.TestHelper.getDocuments;
+import static com.buschmais.jqassistant.plugin.yaml2.helper.YMLPluginAssertions.assertThat;
 
 class ExampleC2E14IT extends AbstractYAMLPluginIT {
     private static String YAML_FILE = "/probes/example-c2-e14-in-the-folded-scalars-newlines-become spaces.yaml";
@@ -15,6 +24,20 @@ class ExampleC2E14IT extends AbstractYAMLPluginIT {
         readSourceDocument();
     }
 
-    /* Todo Write more specific tests */
+    @Test
+    void documentContainsOnlyAScalar() {
+        Consumer<YMLScalarDescriptor> condition = (scalar) -> {
+            assertThat(scalar).hasValue("Mark McGwire's year was crippled by " +
+                                        "a knee injury.\n");
+        };
 
+        YMLFileDescriptor fileDescriptor = readSourceDocument();
+
+        YMLDocumentDescriptor document = getDocuments(fileDescriptor).getDocument(0);
+
+        assertThat(document).hasNoSequences();
+        assertThat(document).hasNoMaps();
+        assertThat(document).hasScalars();
+        assertThat(document.getScalars()).element(0).satisfies(condition);
+    }
 }
