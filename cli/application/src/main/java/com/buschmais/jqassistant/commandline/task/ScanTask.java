a626451137a675733ec161f2c88d1c35c27886e8
Dirk Mahler <dirk.mahler@buschmais.com>
2020-01-07 09:04:10 +0100

    extracted parsing of scoped resources (URLs, files) CLI ScanTask to ScopeHelper

--- cli/application/src/main/java/com/buschmais/jqassistant/commandline/task/ScanTask.java
+++ cli/application/src/main/java/com/buschmais/jqassistant/commandline/task/ScanTask.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.buschmais.jqassistant.commandline.CliConfigurationException;
 import com.buschmais.jqassistant.commandline.CliExecutionException;
@@ -35,8 +33,10 @@ public class ScanTask extends AbstractStoreTask {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScanTask.class);
 
-    private Map<String, String> files = Collections.emptyMap();
-    private Map<String, String> urls = Collections.emptyMap();
+    private ScopeHelper scopeHelper = new ScopeHelper(LOGGER);
+
+    private List<ScopeHelper.ScopedResource> files = null;
+    private List<ScopeHelper.ScopedResource> urls = null;
     private boolean reset = false;
     private boolean continueOnError = false;
 
@@ -63,61 +63,32 @@ public class ScanTask extends AbstractStoreTask {
     @Override
     protected void executeTask(Store store) throws CliExecutionException {
         ScannerContext scannerContext = new ScannerContextImpl(store);
-        Map<String, ScannerPlugin<?, ?>> scannerPlugins = pluginRepository.getScannerPluginRepository().getScannerPlugins(scannerContext, pluginProperties);
         if (reset) {
             store.reset();
         }
-        for (Map.Entry<String, String> entry : files.entrySet()) {
-            String fileName = entry.getKey();
-            String scopeName = entry.getValue();
-            final File file = new File(fileName);
+        for (ScopeHelper.ScopedResource scopedResource : files) {
+            String fileName = scopedResource.getResource();
+            String scopeName = scopedResource.getScopeName();
+            File file = new File(fileName);
             String absolutePath = file.getAbsolutePath();
             if (!file.exists()) {
                 LOGGER.info(absolutePath + "' does not exist, skipping scan.");
             } else {
-                scan(scannerContext, file, file.getAbsolutePath(), scopeName, scannerPlugins);
+                scan(scannerContext, file, file.getAbsolutePath(), scopeName);
             }
         }
-        for (Map.Entry<String, String> entry : urls.entrySet()) {
-            String uri = entry.getKey();
-            String scopeName = entry.getValue();
+        for (ScopeHelper.ScopedResource scopedResource : urls) {
+            String uri = scopedResource.getResource();
+            String scopeName = scopedResource.getScopeName();
             try {
-                scan(scannerContext, new URI(uri), uri, scopeName, scannerPlugins);
+                scan(scannerContext, new URI(uri), uri, scopeName);
             } catch (URISyntaxException e) {
                 throw new CliConfigurationException("Cannot parse URI " + uri, e);
             }
         }
     }
 
-    /**
-     * Parses the given list of option values into a map of resources and their
-     * associated (optional) scopes.
-     *
-     * Example: `maven:repository::http://my-host/repo` will be an entry with key
-     * `maven:repository` and value `http://my-host/repo`.
-     *
-     * @param optionValues
-     *            The value.
-     * @return The map of resources and scopes.
-     */
-    private Map<String, String> parseResources(List<String> optionValues) {
-        Map<String, String> resources = new HashMap<>();
-        for (String file : optionValues) {
-            String[] parts = file.split("::");
-            String fileName;
-            String scopeName = null;
-            if (parts.length == 2) {
-                scopeName = parts[0];
-                fileName = parts[1];
-            } else {
-                fileName = parts[0];
-            }
-            resources.put(fileName, scopeName);
-        }
-        return resources;
-    }
-
-    private <T> void scan(ScannerContext scannerContext, T element, String path, String scopeName, Map<String, ScannerPlugin<?, ?>> scannerPlugins) {
+    private <T> void scan(ScannerContext scannerContext, T element, String path, String scopeName) {
         ScannerConfiguration configuration = new ScannerConfiguration();
         configuration.setContinueOnError(continueOnError);
         Scanner scanner = new ScannerImpl(configuration, pluginProperties, scannerContext, pluginRepository.getScannerPluginRepository());
@@ -127,8 +98,8 @@ public class ScanTask extends AbstractStoreTask {
 
     @Override
     public void withOptions(final CommandLine options) throws CliConfigurationException {
-        files = parseResources(getOptionValues(options, CMDLINE_OPTION_FILES, Collections.<String> emptyList()));
-        urls = parseResources(getOptionValues(options, CMDLINE_OPTION_URLS, Collections.<String> emptyList()));
+        files = scopeHelper.getScopedResources(getOptionValues(options, CMDLINE_OPTION_FILES, Collections.emptyList()));
+        urls = scopeHelper.getScopedResources(getOptionValues(options, CMDLINE_OPTION_URLS, Collections.<String> emptyList()));
         if (files.isEmpty() && urls.isEmpty()) {
             throw new CliConfigurationException("No files, directories or urls given.");
         }
