e4516aee5acdd8c1343f90d601645bc3a755677c
Dirk Mahler <dirk.mahler@buschmais.com>
2020-01-17 18:39:29 +0100

    fixed compilation error

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/TypeAssignableFromIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/TypeAssignableFromIT.java
@@ -12,6 +12,7 @@ import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCC
 import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertThat;
 
 /**
