a576fed6cd1c2257650c9d29d4ccfe25d7e423b1
Dirk Mahler <dirk.mahler@buschmais.com>
2020-02-04 18:32:32 +0100

    moved MapBuilder to shared module and improved SubGraphFactory to handle collections of nodes/relationships

--- core/report/src/main/java/com/buschmais/jqassistant/core/report/api/graph/SubGraphFactory.java
+++ core/report/src/main/java/com/buschmais/jqassistant/core/report/api/graph/SubGraphFactory.java
@@ -11,6 +11,7 @@ import com.buschmais.jqassistant.core.report.api.graph.model.Relationship;
 import com.buschmais.jqassistant.core.report.api.graph.model.SubGraph;
 import com.buschmais.jqassistant.core.report.api.model.Result;
 import com.buschmais.jqassistant.core.rule.api.model.ExecutableRule;
+import com.buschmais.jqassistant.core.shared.annotation.ToBeRemovedInVersion;
 import com.buschmais.xo.api.CompositeObject;
 import com.buschmais.xo.neo4j.api.model.Neo4jLabel;
 import com.buschmais.xo.neo4j.api.model.Neo4jNode;
@@ -24,8 +25,8 @@ public class SubGraphFactory {
     private static final String GRAPH = "graph";
 
     private static final String LABEL = "label";
-    private static final String PROPERTIES = "properties";
 
+    private static final String PROPERTIES = "properties";
     private static final String LABELS = "labels";
 
     private static final String TYPE = "type";
@@ -52,27 +53,12 @@ public class SubGraphFactory {
     public SubGraph createSubGraph(Result<? extends ExecutableRule> result) throws ReportException {
         SubGraph graph = new SubGraph();
         graph.setId(subgraphId--);
-        graph.setId(0);
         for (Map<String, Object> row : result.getRows()) {
             for (Object value : row.values()) {
-                convert(graph, value);
+                addToValueGraph(graph, value);
             }
         }
         return graph;
-    }
-
-    private <I extends Identifiable> I convert(SubGraph parent, Object value) throws ReportException {
-        I identifiable = convert(value);
-        if (identifiable != null) {
-            if (identifiable instanceof Node) {
-                parent.getNodes().put(identifiable.getId(), (Node) identifiable);
-            } else if (identifiable instanceof Relationship) {
-                parent.getRelationships().put(identifiable.getId(), (Relationship) identifiable);
-            } else if (identifiable instanceof SubGraph) {
-                parent.getSubGraphs().put(identifiable.getId(), (SubGraph) identifiable);
-            }
-        }
-        return identifiable;
     }
 
     /**
@@ -81,13 +67,11 @@ public class SubGraphFactory {
      *
      * @param value
      *            The value.
-     * @param <I>
-     *            The {@link Identifiable} type.
      * @return The {@link Identifiable}.
      * @throws ReportException
      *             If value that cannot be converted to an {@link Identifiable}.
      */
-    public <I extends Identifiable> I convert(Object value) throws ReportException {
+    public <I extends Identifiable> I toIdentifiable(Object value) throws ReportException {
         if (value == null) {
             return null;
         } else if (value instanceof Map) {
@@ -96,43 +80,45 @@ public class SubGraphFactory {
             if (role != null) {
                 Map<String, Object> properties = (Map<String, Object>) virtualObject.get(PROPERTIES);
                 switch (role.toString().toLowerCase()) {
-                case NODE:
-                    Node node = new Node();
-                    node.setId(nodeId--);
-                    Collection<String> labels = (Collection<String>) virtualObject.get(LABELS);
-                    node.getLabels().addAll(labels);
-                    node.getProperties().putAll(properties);
-                    node.setLabel((String) virtualObject.get(LABEL));
-                    return (I) node;
-                case RELATIONSHIP:
-                    Relationship relationship = new Relationship();
-                    relationship.setId(relationshipId--);
-                    Node startNode = convert(virtualObject.get(START_NODE));
-                    Node endNode = convert(virtualObject.get(END_NODE));
-                    String type = (String) virtualObject.get(TYPE);
-                    relationship.setType(type);
-                    relationship.setStartNode(startNode);
-                    relationship.setEndNode(endNode);
-                    relationship.getProperties().putAll(properties);
-                    relationship.setLabel((String) virtualObject.get(LABEL));
-                    if (startNode == null || endNode == null || type == null) {
-                        throw new ReportException("The virtual relationship does not contain either start node, end node or type: " + relationship);
-                    }
-                    return (I) relationship;
-                case GRAPH:
-                    SubGraph subgraph = new SubGraph();
-                    subgraph.setId(subgraphId--);
-                    subgraph.setParent(convert(subgraph, virtualObject.get(PARENT)));
-                    addSubGraphChildren(subgraph, virtualObject, NODES, subgraph.getNodes());
-                    addSubGraphChildren(subgraph, virtualObject, RELATIONSHIPS, subgraph.getRelationships());
-                    return (I) subgraph;
+                    case NODE:
+                        Node node = new Node();
+                        node.setId(nodeId--);
+                        Collection<String> labels = (Collection<String>) virtualObject.get(LABELS);
+                        node.getLabels().addAll(labels);
+                        node.getProperties().putAll(properties);
+                        node.setLabel((String) virtualObject.get(LABEL));
+                        return (I) node;
+                    case RELATIONSHIP:
+                        Relationship relationship = new Relationship();
+                        relationship.setId(relationshipId--);
+                        Node startNode = toIdentifiable(virtualObject.get(START_NODE));
+                        Node endNode = toIdentifiable(virtualObject.get(END_NODE));
+                        String type = (String) virtualObject.get(TYPE);
+                        relationship.setType(type);
+                        relationship.setStartNode(startNode);
+                        relationship.setEndNode(endNode);
+                        relationship.getProperties().putAll(properties);
+                        relationship.setLabel((String) virtualObject.get(LABEL));
+                        if (startNode == null || endNode == null || type == null) {
+                            throw new ReportException("The virtual relationship does not contain either start node, end node or type: " + relationship);
+                        }
+                        return (I) relationship;
+                    case GRAPH:
+                        SubGraph subgraph = new SubGraph();
+                        subgraph.setId(subgraphId--);
+                        Node parent = toIdentifiable(virtualObject.get(PARENT));
+                        subgraph.setParent(parent);
+                        subgraph.setLabel((String) virtualObject.get(LABEL));
+                        addToValueGraph(subgraph, virtualObject.get(NODES));
+                        addToValueGraph(subgraph, virtualObject.get(RELATIONSHIPS));
+                        return (I) subgraph;
                 }
             }
         } else if (value instanceof CompositeObject) {
             CompositeObject compositeObject = (CompositeObject) value;
-            I identifiable = convert(compositeObject.getDelegate());
+            Identifiable identifiable = toIdentifiable(compositeObject.getDelegate());
             identifiable.setLabel(ReportHelper.getLabel(value));
-            return identifiable;
+            return (I) identifiable;
         } else if (value instanceof Neo4jNode) {
             Neo4jNode<Neo4jLabel, ?, ?, ?> neo4jNode = (Neo4jNode<Neo4jLabel, ?, ?, ?>) value;
             Node node = new Node();
@@ -147,22 +133,42 @@ public class SubGraphFactory {
             Relationship relationship = new Relationship();
             relationship.setId(neo4jRelationship.getId());
             relationship.setType(neo4jRelationship.getType().getName());
-            relationship.setStartNode(convert(neo4jRelationship.getStartNode()));
-            relationship.setEndNode(convert(neo4jRelationship.getEndNode()));
+            relationship.setStartNode(toIdentifiable(neo4jRelationship.getStartNode()));
+            relationship.setEndNode(toIdentifiable(neo4jRelationship.getEndNode()));
             relationship.getProperties().putAll(neo4jRelationship.getProperties());
             return (I) relationship;
         }
         throw new ReportException("Element type not supported: " + value);
     }
 
-    private <I extends Identifiable> void addSubGraphChildren(SubGraph subgraph, Map<String, Object> virtualObject, String key, Map<Long, I> childMap)
-            throws ReportException {
-        Collection<Object> children = (Collection<Object>) virtualObject.get(key);
-        if (children != null) {
-            for (Object child : children) {
-                I identifiable = convert(subgraph, child);
-                childMap.put(identifiable.getId(), identifiable);
+    private void addToValueGraph(SubGraph parent, Object value) throws ReportException {
+        if (value != null) {
+            if (value instanceof Iterable<?>) {
+                Iterable<?> values = (Iterable<?>) value;
+                for (Object singleValue : values) {
+                    addIdentifiableToGraph(parent, toIdentifiable(singleValue));
+                }
+            } else {
+                addIdentifiableToGraph(parent, toIdentifiable(value));
             }
         }
+    }
+
+    private void addIdentifiableToGraph(SubGraph parent, Identifiable identifiable) {
+        if (identifiable != null) {
+            if (identifiable instanceof Node) {
+                parent.getNodes().put(identifiable.getId(), (Node) identifiable);
+            } else if (identifiable instanceof Relationship) {
+                parent.getRelationships().put(identifiable.getId(), (Relationship) identifiable);
+            } else if (identifiable instanceof SubGraph) {
+                parent.getSubGraphs().put(identifiable.getId(), (SubGraph) identifiable);
+            }
+        }
+    }
+
+    @Deprecated
+    @ToBeRemovedInVersion(major = 1, minor = 10)
+    public <I extends Identifiable> I convert(Object value) throws ReportException {
+        return toIdentifiable(value);
     }
 }
--- core/report/src/test/java/com/buschmais/jqassistant/core/report/api/graph/SubGraphFactoryTest.java
+++ core/report/src/test/java/com/buschmais/jqassistant/core/report/api/graph/SubGraphFactoryTest.java
@@ -1,14 +1,237 @@
 package com.buschmais.jqassistant.core.report.api.graph;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
+import com.buschmais.jqassistant.core.report.api.ReportException;
+import com.buschmais.jqassistant.core.report.api.graph.model.Node;
+import com.buschmais.jqassistant.core.report.api.graph.model.Relationship;
+import com.buschmais.jqassistant.core.report.api.graph.model.SubGraph;
+import com.buschmais.jqassistant.core.report.api.model.Result;
+import com.buschmais.jqassistant.core.rule.api.model.ExecutableRule;
+import com.buschmais.jqassistant.core.shared.map.MapBuilder;
+import com.buschmais.xo.api.CompositeObject;
+import com.buschmais.xo.neo4j.api.model.Neo4jLabel;
+import com.buschmais.xo.neo4j.api.model.Neo4jNode;
+import com.buschmais.xo.neo4j.api.model.Neo4jRelationship;
+import com.buschmais.xo.neo4j.api.model.Neo4jRelationshipType;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 @ExtendWith(MockitoExtension.class)
 public class SubGraphFactoryTest {
 
-    @Test
-    public void compositeObjects() {
+    private SubGraphFactory factory = new SubGraphFactory();
 
+    @Test
+    public void nodeAndRelationship() throws ReportException {
+        MapBuilder<String, Object> builder = MapBuilder.builder();
+
+        Map<String, Object> nodeProperties = MapBuilder.<String, Object> builder().entry("nodeKey", "value").build();
+        builder.entry("node", getNeo4jNode(1l, nodeProperties, "Test1", "Test2"));
+        Map<String, Object> relationshipProperties = MapBuilder.<String, Object> builder().entry("relationshipKey", "value").build();
+        builder.entry("relation", getNeo4jRelationship(1l, relationshipProperties, "TEST"));
+        Result<ExecutableRule> result = Result.builder().rows(singletonList(builder.build())).build();
+
+        SubGraph graph = factory.createSubGraph(result);
+
+        assertThat(graph.getId()).isEqualTo(-1l);
+        assertThat(graph.getParent()).isNull();
+        assertThat(graph.getSubGraphs()).isEmpty();
+
+        Map<Long, Node> nodes = graph.getNodes();
+        assertThat(nodes).hasSize(1);
+        Node node = nodes.get(1l);
+        assertThat(node.getId()).isEqualTo(1l);
+        assertThat(node.getLabels()).containsExactly("Test1", "Test2");
+        assertThat(node.getProperties()).isEqualTo(nodeProperties);
+
+        Map<Long, Relationship> relationships = graph.getRelationships();
+        assertThat(relationships).hasSize(1);
+        Relationship relationship = relationships.get(1l);
+        assertThat(relationship.getId()).isEqualTo(1l);
+        assertThat(relationship.getType()).isEqualTo("TEST");
+        assertThat(relationship.getProperties()).isEqualTo(relationshipProperties);
+    }
+
+    @Test
+    public void collectionOfNodesAndCollectionOfRelationships() throws ReportException {
+        MapBuilder<String, Object> builder = MapBuilder.builder();
+
+        builder.entry("nodes", asList(getNeo4jNode(1l), getNeo4jNode(2l)));
+        builder.entry("relations", asList(getNeo4jRelationship(1l, "TEST"), getNeo4jRelationship(2l, "TEST")));
+        Result<ExecutableRule> result = Result.builder().rows(singletonList(builder.build())).build();
+
+        SubGraph graph = factory.createSubGraph(result);
+
+        assertThat(graph.getId()).isEqualTo(-1l);
+        assertThat(graph.getParent()).isNull();
+        assertThat(graph.getSubGraphs()).isEmpty();
+
+        Map<Long, Node> nodes = graph.getNodes();
+        assertThat(nodes).hasSize(2);
+        Node node1 = nodes.get(1l);
+        assertThat(node1.getId()).isEqualTo(1l);
+        Node node2 = nodes.get(2l);
+        assertThat(node2.getId()).isEqualTo(2l);
+
+        Map<Long, Relationship> relationships = graph.getRelationships();
+        assertThat(relationships).hasSize(2);
+        Relationship relationship1 = relationships.get(1l);
+        assertThat(relationship1.getId()).isEqualTo(1l);
+        Relationship relationship2 = relationships.get(2l);
+        assertThat(relationship2.getId()).isEqualTo(2l);
+    }
+
+    @Test
+    public void virtualNode() throws ReportException {
+        Map<Object, Object> properties = MapBuilder.builder().entry("key", "value").build();
+        Map<String, Object> virtualNode = MapBuilder.<String, Object> builder() //
+                .entry("role", "node") //
+                .entry("label", "Virtual Node") //
+                .entry("labels", singletonList("Test")) //
+                .entry("properties", properties) //
+                .build();
+        MapBuilder<String, Object> builder = MapBuilder.builder();
+        builder.entry("nodes", singletonList(virtualNode));
+        Result<ExecutableRule> result = Result.builder().rows(singletonList(builder.build())).build();
+
+        SubGraph graph = factory.createSubGraph(result);
+
+        Map<Long, Node> nodes = graph.getNodes();
+        assertThat(nodes.size()).isEqualTo(1);
+        Node node = nodes.get(-1l);
+        assertThat(node).isNotNull();
+        assertThat(node.getId()).isEqualTo(-1l);
+        assertThat(node.getLabel()).isEqualTo("Virtual Node");
+        assertThat(node.getLabels()).containsExactly("Test");
+        assertThat(node.getProperties()).isEqualTo(properties);
+    }
+
+    @Test
+    public void virtualRelationship() throws ReportException {
+        Map<Object, Object> properties = MapBuilder.builder().entry("key", "value").build();
+        Map<String, Object> virtualNode = MapBuilder.<String, Object> builder() //
+                .entry("role", "relationship") //
+                .entry("label", "Virtual Relationship") //
+                .entry("type", "TEST") //
+                .entry("properties", properties) //
+                .entry("startNode", getNeo4jNode(1l)) //
+                .entry("endNode", getNeo4jNode(2l)) //
+                .build();
+        MapBuilder<String, Object> builder = MapBuilder.builder();
+        builder.entry("relationships", singletonList(virtualNode));
+        Result<ExecutableRule> result = Result.builder().rows(singletonList(builder.build())).build();
+
+        SubGraph graph = factory.createSubGraph(result);
+
+        Map<Long, Relationship> relationships = graph.getRelationships();
+        assertThat(relationships.size()).isEqualTo(1);
+        Relationship relationship = relationships.get(-1l);
+        assertThat(relationship).isNotNull();
+        assertThat(relationship.getId()).isEqualTo(-1l);
+        assertThat(relationship.getLabel()).isEqualTo("Virtual Relationship");
+        assertThat(relationship.getType()).isEqualTo("TEST");
+        assertThat(relationship.getProperties()).isEqualTo(properties);
+        Node startNode = relationship.getStartNode();
+        assertThat(startNode).isNotNull();
+        assertThat(startNode.getId()).isEqualTo(1l);
+        Node endNode = relationship.getEndNode();
+        assertThat(endNode).isNotNull();
+        assertThat(endNode.getId()).isEqualTo(2l);
+    }
+
+    @Test
+    public void subGraph() throws ReportException {
+        Map<String, Object> virtualGraph = MapBuilder.<String, Object> builder() //
+                .entry("role", "graph") //
+                .entry("label", "Virtual Graph") //
+                .entry("parent", getNeo4jNode(0l)) //
+                .entry("nodes", singletonList(getNeo4jNode(1l))) //
+                .entry("relationships", singletonList(getNeo4jRelationship(1l, "TEST"))) //
+                .build();
+
+        MapBuilder<String, Object> rowBuilder = MapBuilder.builder();
+        Map<String, Object> row = rowBuilder.entry("graph", virtualGraph).build();
+
+        Result<ExecutableRule> result = Result.builder().rows(singletonList(row)).build();
+
+        SubGraph graph = factory.createSubGraph(result);
+
+        assertThat(graph.getParent()).isNull();
+        assertThat(graph.getNodes()).isEmpty();
+        assertThat(graph.getRelationships()).isEmpty();
+
+        Map<Long, SubGraph> subGraphs = graph.getSubGraphs();
+        assertThat(subGraphs).hasSize(1);
+        SubGraph subGraph = subGraphs.get(-2l);
+        assertThat(subGraph.getId()).isEqualTo(-2l);
+        assertThat(subGraph.getLabel()).isEqualTo("Virtual Graph");
+        Node parent = subGraph.getParent();
+        assertThat(parent).isNotNull();
+        assertThat(parent.getId()).isEqualTo(0l);
+
+        Map<Long, Node> nodes = subGraph.getNodes();
+        assertThat(nodes).hasSize(1);
+        Node node1 = nodes.get(1l);
+        assertThat(node1.getId()).isEqualTo(1l);
+
+        Map<Long, Relationship> relationships = subGraph.getRelationships();
+        assertThat(relationships).hasSize(1);
+        Relationship relationship1 = relationships.get(1l);
+        assertThat(relationship1.getId()).isEqualTo(1l);
+    }
+
+    @Test
+    public void compositeObject() throws ReportException {
+        CompositeObject compositeObject = mock(CompositeObject.class);
+        Neo4jNode neo4jNode = getNeo4jNode(1l, "Test");
+        doReturn(neo4jNode).when(compositeObject).getDelegate();
+        MapBuilder<String, Object> builder = MapBuilder.builder();
+        builder.entry("nodes", singletonList(compositeObject));
+        Result<ExecutableRule> result = Result.builder().rows(singletonList(builder.build())).build();
+
+        SubGraph graph = factory.createSubGraph(result);
+
+        Map<Long, Node> nodes = graph.getNodes();
+        assertThat(nodes).isNotEmpty();
+        Node node = nodes.get(1l);
+        assertThat(node).isNotNull();
+        assertThat(node.getId()).isEqualTo(1l);
+        assertThat(node.getLabels()).containsExactly("Test");
+    }
+
+    private Neo4jNode getNeo4jNode(long id, String... labels) {
+        return getNeo4jNode(id, emptyMap(), labels);
+    }
+
+    private Neo4jNode getNeo4jNode(long id, Map<String, Object> properties, String... labels) {
+        Neo4jNode node = mock(Neo4jNode.class);
+        doReturn(id).when(node).getId();
+        doReturn(properties).when(node).getProperties();
+        doReturn(Stream.of(labels).map(label -> (Neo4jLabel) () -> label).collect(toList())).when(node).getLabels();
+        return node;
+    }
+
+    private Neo4jRelationship getNeo4jRelationship(long id, String type) {
+        return getNeo4jRelationship(id, emptyMap(), type);
+    }
+
+    private Neo4jRelationship getNeo4jRelationship(long id, Map<String, Object> properties, String type) {
+        Neo4jRelationship relationship = mock(Neo4jRelationship.class);
+        doReturn(id).when(relationship).getId();
+        doReturn(properties).when(relationship).getProperties();
+        doReturn((Neo4jRelationshipType) () -> type).when(relationship).getType();
+        return relationship;
     }
 }
