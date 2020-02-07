b70f6c5fe1e3983047bce6322f017e54f04f95b6
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2020-02-07 23:02:10 +0100

    [jqassistant/jqa-yaml2-plugin#1] Removed deprecated descriptor

--- plugin/yaml/src/main/java/com/buschmais/jqassistant/plugin/yaml2/api/model/YMLReferenceDescriptor.java
+++
@@ -1,7 +0,0 @@
-package com.buschmais.jqassistant.plugin.yaml2.api.model;
-
-import com.buschmais.xo.neo4j.api.annotation.Label;
-
-@Label("Reference")
-public interface YMLReferenceDescriptor extends YMLDescriptor {
-}
