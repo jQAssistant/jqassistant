89b470ddfc3607bdc4e1f6c6c92315b31e4b9e3b
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2020-02-20 21:33:01 +0100

    [jQAssistant/jqa-yaml2-plugin#1] Added tests if missing content for a node is handled properly

--- plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/ContextTypeTest.java
+++ plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/ContextTypeTest.java
@@ -3,8 +3,6 @@ package com.buschmais.jqassistant.plugin.yaml2.impl.scanner;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ContextTypeTest {
 
     @Disabled
---
+++ plugin/yaml/src/test/java/com/buschmais/jqassistant/plugin/yaml2/impl/scanner/NullValueHandlingIT.java
@@ -0,0 +1,46 @@
+package com.buschmais.jqassistant.plugin.yaml2.impl.scanner;
+
+import java.util.List;
+
+import com.buschmais.jqassistant.plugin.yaml2.api.model.YMLScalarDescriptor;
+import com.buschmais.jqassistant.plugin.yaml2.impl.scanner.spec12.AbstractYAMLPluginIT;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnabledIfSystemProperty(named = "jqassistant.yaml2.activate", matches = "^true$")
+public class NullValueHandlingIT extends AbstractYAMLPluginIT {
+
+
+    @Test
+    void propertyValueIsAlwaysPresetIfValueForANodeIsMissing() {
+        readSourceDocument("/nullvalue/010-null-value-in-list.yaml");
+
+
+        String query = "MATCH (:Yaml:Sequence)-[HAS_ITEM]->(item:Yaml:Scalar) " +
+                       "WHERE EXISTS(item.value) " +
+                       "RETURN item";
+
+        TestResult testResult = query(query);
+        assertThat(testResult.getColumns()).containsKeys("item");
+
+        List<YMLScalarDescriptor> result = testResult.getColumn("item");
+
+        assertThat(result).hasSize(3);
+    }
+
+    @Test
+    void valueOfAMissingContentForANodeMustBeAnEmptyString() {
+        readSourceDocument("/nullvalue/010-null-value-in-list.yaml");
+
+
+        String query = "MATCH (:Yaml:Sequence)-[HAS_ITEM]->(item:Yaml:Scalar { value: '' }) " +
+                       "RETURN item";
+
+        List<YMLScalarDescriptor> result = query(query).getColumn("item");
+
+        assertThat(result).hasSize(1);
+    }
+}
