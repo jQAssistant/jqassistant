50bf9d9ba96091006b5ce29b82cde1acf8ca5e60
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2021-06-26 12:09:53 +0200

    [jQAssistant/jQA-Idea-Hub#18] Added task to list all plugins known to jQA

---
+++ maven/src/main/java/com/buschmais/jqassistant/scm/maven/ListPluginsMojo.java
@@ -0,0 +1,46 @@
+package com.buschmais.jqassistant.scm.maven;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.buschmais.jqassistant.core.plugin.api.PluginInfo;
+import com.buschmais.jqassistant.core.plugin.api.PluginRepository;
+import com.buschmais.jqassistant.core.store.api.Store;
+
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Lists all plugins known based on the current configuration
+ * to jQAssistant.
+ */
+@Mojo(name = "list-plugins", threadSafe = true, configurator = "custom")
+public class ListPluginsMojo extends AbstractProjectMojo {
+
+    @Override
+    protected boolean isResetStoreBeforeExecution() {
+        return false;
+    }
+
+    @Override
+    protected boolean isConnectorRequired() {
+        return false;
+    }
+
+    @Override
+    protected void aggregate(MavenProject rootModule, List<MavenProject> projects, Store store) {
+        getLog().info("Available plugins for '" + rootModule.getName() + "'.");
+
+        PluginRepository pluginRepository = pluginRepositoryProvider.getPluginRepository();
+        List<PluginInfo> sortedInfos = pluginRepository.getPluginOverview().stream()
+                                                       .sorted(PluginInfo.NAME_COMPARATOR)
+                                                       .collect(Collectors.toList());
+
+        sortedInfos.forEach(info -> {
+            CharSequence name = info.getName();
+            CharSequence id = info.getId();
+            String output = String.format("%s (%s)", name, id);
+            System.out.println(output);
+        });
+    }
+}
