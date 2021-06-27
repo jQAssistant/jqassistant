ed62c42bb5338d2ae0531c983e3d728c6d593a7c
Dirk Mahler <dirk.mahler@buschmais.com>
2021-06-27 16:55:24 +0200

    provide default implementations in SourceProvider

--- core/report/src/main/java/com/buschmais/jqassistant/core/report/api/SourceProvider.java
+++ core/report/src/main/java/com/buschmais/jqassistant/core/report/api/SourceProvider.java
@@ -3,35 +3,39 @@ package com.buschmais.jqassistant.core.report.api;
 import com.buschmais.xo.api.CompositeObject;
 
 /**
- * Defines a provider which allows looking up source code information from {@link CompositeObject}s.
+ * Defines a provider which allows looking up source code information from
+ * {@link CompositeObject}s.
  */
 public interface SourceProvider<D extends CompositeObject> {
 
     /**
      * Return the name representing the language element.
-     * 
+     *
      * @return The name.
      */
     String getName(D descriptor);
 
     /**
-     * Return the name of the resource providing the source code, e.g. a file
-     * name.
-     * 
+     * Return the name of the resource providing the source code, e.g. a file name.
+     *
      * @param descriptor
      *            The descriptor.
      * @return The name of the resource.
      */
-    String getSourceFile(D descriptor);
+    default String getSourceFile(D descriptor) {
+        return null;
+    }
 
     /**
      * Return the line number where the descriptor is represented in the source
      * code.
-     * 
+     *
      * @param descriptor
      *            The descriptor.
      * @return The line numbers.
      */
-    Integer getLineNumber(D descriptor);
+    default Integer getLineNumber(D descriptor) {
+        return null;
+    };
 
 }
