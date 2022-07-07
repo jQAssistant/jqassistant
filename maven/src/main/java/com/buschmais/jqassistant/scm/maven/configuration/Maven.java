99e33d7e6c2c913b44771c39454f38ce7a68412f
Dirk Mahler <dirk.mahler@buschmais.com>
2022-07-07 16:10:06 +0200

    added documentation for the configuration of the CLI and Maven plugin

--- maven/src/main/java/com/buschmais/jqassistant/scm/maven/configuration/Maven.java
+++ maven/src/main/java/com/buschmais/jqassistant/scm/maven/configuration/Maven.java
@@ -10,7 +10,7 @@ public interface Maven {
 
     String USE_EXECUTION_ROOT_AS_PROJECT_ROOT = "use-execution-root-as-project-root";
 
-    @Description("Torce the module where 'mvn' is being executed to be used as root module. The database will be created in this module and contain all information of the reactor. Rules will be read from the rules of this module.")
+    @Description("Force the module where 'mvn' is being executed to be used as root module. The database will be created in this module and contain all information of the reactor. Rules will be read from the rules of this module.")
     @WithDefault("false")
     boolean useExecutionRootAsProjectRoot();
 
