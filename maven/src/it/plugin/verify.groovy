96dd0912df7668a73427758e17d82b06082589fb
Stephan Pirnbaum <stephan.pirnbaum@buschmais.com>
2022-11-30 10:12:10 +0100

    Adapt IT test for removed asciidoc report plugin

--- maven/src/it/plugin/verify.groovy
+++ maven/src/it/plugin/verify.groovy
@@ -1,17 +1,8 @@
 assert !new File(basedir, 'site/target/customReport.txt.1').exists()
 assert new File(basedir, 'site/target/customReport.txt.2').exists()
 
-// Asciidoc report from  plugin (with includes)
-def asciidocReportFile = new File(basedir, 'site/target/jqassistant/report/asciidoc/custom/index.html')
-assert asciidocReportFile.exists()
-String asciidocReport = asciidocReportFile.getText("UTF-8")
-assert asciidocReport.contains("<img src=\"../../plantuml/custom_ClassDiagram.svg\">")
-assert asciidocReport.contains("<img src=\"../../plantuml/custom_ComponentDiagram.svg\">")
-assert asciidocReport.contains('<a href="../../csv/custom_CSVReport.csv"')
-assert new File(basedir, 'site/target/jqassistant/report/plantuml/custom_ClassDiagram.svg').exists()
-assert new File(basedir, 'site/target/jqassistant/report/plantuml/custom_ComponentDiagram.svg').exists()
 assert new File(basedir, 'site/target/jqassistant/report/csv/custom_CSVReport.csv').exists()
-assert new File(basedir, 'site/target/jqassistant/report/asciidoc/custom-embedded-plantuml.svg').exists()
+assert new File(basedir, 'site/target/jqassistant/report/csv/custom_CSVReportRelative.csv').exists()
 
 // XML report
 def reportFile = new File(basedir, 'site/target/jqassistant/jqassistant-report.xml')
