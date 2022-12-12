62b1c1ba41618972737572e2d945c60a8e2c7433
Dirk Mahler <dirk.mahler@buschmais.com>
2022-12-12 11:32:58 +0100

    Feature/java11 neo4jv4 (#40)
    Migrate to Neo4j 4.x
    
    * prepared migration to Neo4j 4.x
    
    * fixed concept "java:TypeAssignableFrom" and improved rules for using :Java:Type instead of :Type

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/scanner/ByteCodeVersionIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/scanner/ByteCodeVersionIT.java
@@ -1,6 +1,5 @@
 package com.buschmais.jqassistant.plugin.java.test.scanner;
 
-import java.io.IOException;
 import java.util.List;
 
 import com.buschmais.jqassistant.plugin.java.api.model.ClassFileDescriptor;
@@ -13,6 +12,7 @@ import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCC
 import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 
 /**
  * Verifies functionality related to byte code and java versions.
@@ -20,13 +20,13 @@ import static org.hamcrest.Matchers.equalTo;
 class ByteCodeVersionIT extends AbstractJavaPluginIT {
 
     @Test
-    void byteCodeVersion() throws IOException {
+    void byteCodeVersion() {
         scanClasses(Pojo.class);
         store.beginTransaction();
         List<ClassFileDescriptor> types = query("MATCH (t:Type) WHERE t.name='Pojo' RETURN t").getColumn("t");
         assertThat(types.size(), equalTo(1));
         ClassFileDescriptor pojo = types.get(0);
-        assertThat(pojo.getByteCodeVersion(), equalTo(52)); // Java 8
+        assertThat(pojo.getByteCodeVersion(), greaterThan(52)); // Java 8
         store.commitTransaction();
 
     }
@@ -36,7 +36,7 @@ class ByteCodeVersionIT extends AbstractJavaPluginIT {
         scanClasses(Pojo.class);
         assertThat(applyConcept("java:JavaVersion").getStatus(), equalTo(SUCCESS));
         store.beginTransaction();
-        List<ClassFileDescriptor> types = query("MATCH (t:Type) WHERE t.name='Pojo' and t.javaVersion='Java 8' RETURN t").getColumn("t");
+        List<ClassFileDescriptor> types = query("MATCH (t:Type) WHERE t.name='Pojo' and t.javaVersion starts with 'Java ' RETURN t").getColumn("t");
         assertThat(types.size(), equalTo(1));
         ClassFileDescriptor pojo = types.get(0);
         assertThat(pojo, typeDescriptor(Pojo.class));
