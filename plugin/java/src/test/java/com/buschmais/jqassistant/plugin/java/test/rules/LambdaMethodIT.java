fd0cfe5016fe86974d686b79d3b1baa04e766630
Dirk Mahler <dirk.mahler@buschmais.com>
2023-03-13 09:36:49 +0100

    fixed checkstyle violation

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/LambdaMethodIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/LambdaMethodIT.java
@@ -8,7 +8,6 @@ import com.buschmais.jqassistant.plugin.java.test.set.rules.java.LambdaMethod;
 
 import org.junit.jupiter.api.Test;
 
-import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
