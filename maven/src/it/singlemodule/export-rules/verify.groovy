93e50d6c770d7283d3dfa5980eea3db040bf9e1e
Dirk Mahler <dirk.mahler@buschmais.com>
2023-03-19 19:34:24 +0100

    adopted changes in Java concepts for ITs

--- maven/src/it/singlemodule/export-rules/verify.groovy
+++ maven/src/it/singlemodule/export-rules/verify.groovy
@@ -3,5 +3,5 @@ assert rulesFile.exists()
 def rulesNode = new XmlSlurper().parse(rulesFile)
 def exportDefault = rulesNode.group.find { it.@id = 'export-default' }
 assert exportDefault != null
-assert exportDefault.includeConstraint.find { it.@refId == 'dependency:PackageCycles' } != null
-assert exportDefault.includeConstraint.find { it.@refId == 'dependency:ArtifactCycles' } != null
+assert exportDefault.includeConstraint.find { it.@refId == 'java:AvoidCyclicPackageDependencies' } != null
+assert exportDefault.includeConstraint.find { it.@refId == 'java:AvoidCyclicArtifactDependencies' } != null
