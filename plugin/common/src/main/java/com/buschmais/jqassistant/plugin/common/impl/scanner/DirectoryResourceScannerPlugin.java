98e11b15411e82721e702526f1773d48f2093833
Dirk Mahler <dirk.mahler@buschmais.com>
2023-04-17 14:03:12 +0200

    use file resolver for creating directory nodes

--- plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/impl/scanner/DirectoryResourceScannerPlugin.java
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/impl/scanner/DirectoryResourceScannerPlugin.java
@@ -6,10 +6,10 @@ import com.buschmais.jqassistant.core.scanner.api.Scanner;
 import com.buschmais.jqassistant.core.scanner.api.Scope;
 import com.buschmais.jqassistant.plugin.common.api.model.DirectoryDescriptor;
 import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin;
+import com.buschmais.jqassistant.plugin.common.api.scanner.FileResolver;
 import com.buschmais.jqassistant.plugin.common.api.scanner.filesystem.DirectoryResource;
 
-public class DirectoryResourceScannerPlugin
-        extends AbstractScannerPlugin<DirectoryResource, DirectoryDescriptor> {
+public class DirectoryResourceScannerPlugin extends AbstractScannerPlugin<DirectoryResource, DirectoryDescriptor> {
 
     @Override
     public boolean accepts(DirectoryResource item, String path, Scope scope) throws IOException {
@@ -18,9 +18,9 @@ public class DirectoryResourceScannerPlugin
 
     @Override
     public DirectoryDescriptor scan(DirectoryResource item, String path, Scope scope, Scanner scanner) throws IOException {
-        DirectoryDescriptor directoryDescriptor = scanner.getContext().getStore().create(DirectoryDescriptor.class);
-        directoryDescriptor.setFileName(path);
-        return directoryDescriptor;
+        return scanner.getContext()
+            .peek(FileResolver.class)
+            .match(path, DirectoryDescriptor.class, scanner.getContext());
     }
 
 }
