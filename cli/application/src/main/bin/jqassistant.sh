138349d758ebce32d8e6007ca743f87dd4e6c051
Dirk Mahler <dirk.mahler@buschmais.com>
2023-07-30 14:13:33 +0200

    use exec in linux shell script to execute Java (to enable graceful shutdown in Docker container)

--- cli/application/src/main/bin/jqassistant.sh
+++ cli/application/src/main/bin/jqassistant.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BIN_DIR=`dirname "$0"`
 export JQASSISTANT_HOME=`cd "$BIN_DIR/.." && pwd -P`
-java $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/${project.groupId}-${project.artifactId}-${project.version}.${project.packaging}" "$@"
+exec java $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/${project.groupId}-${project.artifactId}-${project.version}.${project.packaging}" "$@"
