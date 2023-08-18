c7885c7a29f8896d4cac5c310f793c240d4921e4
Dirk Mahler <dirk.mahler@buschmais.com>
2023-08-18 08:48:42 +0200

    fixed MapperIT

--- plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/mapper/ModelMapper.java
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/mapper/ModelMapper.java
@@ -2,5 +2,8 @@ package com.buschmais.jqassistant.plugin.common.test.mapper;
 
 import com.buschmais.jqassistant.plugin.common.api.mapper.DescriptorMapper;
 
+import org.mapstruct.Mapper;
+
+@Mapper
 public interface ModelMapper extends DescriptorMapper<Model, ModelDescriptor> {
 }
