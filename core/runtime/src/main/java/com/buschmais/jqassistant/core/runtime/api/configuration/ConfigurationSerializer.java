108479c6e235ee08eb568610722588b30edecb0e
Dirk Mahler <dirk.mahler@buschmais.com>
2023-08-24 18:30:32 +0200

    added effective-configuration task/goal to print YAML representation on the console

--- core/runtime/src/main/java/com/buschmais/jqassistant/core/runtime/api/configuration/ConfigurationSerializer.java
+++ core/runtime/src/main/java/com/buschmais/jqassistant/core/runtime/api/configuration/ConfigurationSerializer.java
@@ -1,6 +1,17 @@
 package com.buschmais.jqassistant.core.runtime.api.configuration;
 
+/**
+ * Defines the interface for serializing an existing configuration.
+ *
+ * @param <C>
+ *     The configurationt type.
+ */
 public interface ConfigurationSerializer<C> {
 
+    /**
+     * Serialize the configuration to a YAML representation.
+     * @param configuration The configuration.
+     * @return The YAML representation.
+     */
     String toYaml(C configuration);
 }
