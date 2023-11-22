ac46af146df2c8fb03ac58fad3f1c706eb2881fd
Dirk Mahler <dirk.mahler@buschmais.com>
2023-11-22 22:11:34 +0100

    use Neo4j settings to create XO properties

--- neo4j/src/main/java/com/buschmais/jqassistant/neo4j/embedded/neo4jv4/Neo4jV4ServerFactory.java
+++ neo4j/src/main/java/com/buschmais/jqassistant/neo4j/embedded/neo4jv4/Neo4jV4ServerFactory.java
@@ -1,29 +1,21 @@
 package com.buschmais.jqassistant.neo4j.embedded.neo4jv4;
 
+import java.util.List;
 import java.util.Properties;
 
 import com.buschmais.jqassistant.neo4j.embedded.EmbeddedNeo4jServer;
 import com.buschmais.jqassistant.neo4j.embedded.EmbeddedNeo4jServerFactory;
 import com.buschmais.jqassistant.neo4j.embedded.configuration.Embedded;
 
+import org.neo4j.configuration.GraphDatabaseInternalSettings;
+import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.configuration.connectors.BoltConnector;
+import org.neo4j.configuration.helpers.SocketAddress;
+import org.neo4j.graphdb.config.Setting;
+
 import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 
 public class Neo4jV4ServerFactory implements EmbeddedNeo4jServerFactory {
-
-    private static final String ALLOW_STORE_UPGRADE = "neo4j.allow_store_upgrade";
-    private static final String KEEP_LOGICAL_LOGS = "neo4j.keep_logical_logs";
-
-    private static final String DBMS_ALLOW_FORMAT_MIGRATION = "neo4j.dbms.allow_format_migration";
-    private static final String DBMS_SECURITY_PROCEDURES_UNRESTRICTED = "neo4j.dbms.security.procedures.unrestricted";
-    private static final String DBMS_TX_LOG_ROTATION_SIZE = "neo4j.dbms.tx_log.rotation.size";
-    private static final String DBMS_TX_LOG_ROTATION_RETENTION_POLICY = "neo4j.dbms.tx_log.rotation.retention_policy";
-
-    private static final String DBMS_CONNECTOR_BOLT_ENABLED = "neo4j.dbms.connector.bolt.enabled";
-
-    private static final String DBMS_CONNECTOR_BOLT_LISTEN_ADDRESS = "neo4j.dbms.connector.bolt.listen_address";
-
-    private static final String UNSUPPORTED_DUMP_DIAGNOSTICS = "neo4j.unsupported.dbms.dump_diagnostics";
 
     @Override
     public EmbeddedNeo4jServer getServer() {
@@ -32,18 +24,36 @@ public class Neo4jV4ServerFactory implements EmbeddedNeo4jServerFactory {
 
     @Override
     public Properties getProperties(Embedded embedded) {
-        Properties properties = new Properties();
-        properties.setProperty(ALLOW_STORE_UPGRADE, TRUE.toString());
-        properties.setProperty(KEEP_LOGICAL_LOGS, FALSE.toString());
-        properties.setProperty(DBMS_ALLOW_FORMAT_MIGRATION, TRUE.toString());
-        properties.setProperty(DBMS_SECURITY_PROCEDURES_UNRESTRICTED, "*");
-        properties.setProperty(DBMS_TX_LOG_ROTATION_SIZE, "50M");
-        properties.setProperty(DBMS_TX_LOG_ROTATION_RETENTION_POLICY, FALSE.toString());
-        properties.setProperty(UNSUPPORTED_DUMP_DIAGNOSTICS, FALSE.toString());
+        Neo4jPropertiesBuilder builder = Neo4jPropertiesBuilder.builder();
+        builder.property(GraphDatabaseSettings.allow_upgrade, true);
+        builder.property(GraphDatabaseSettings.keep_logical_logs, FALSE.toString());
+        builder.property(GraphDatabaseSettings.logical_log_rotation_threshold, 5 * 10124 * 1024L);
+        builder.property(GraphDatabaseSettings.procedure_unrestricted, List.of("*"));
+        builder.property(GraphDatabaseInternalSettings.dump_diagnostics, false);
         if (embedded.connectorEnabled()){
-            properties.setProperty(DBMS_CONNECTOR_BOLT_ENABLED, TRUE.toString());
-            properties.setProperty(DBMS_CONNECTOR_BOLT_LISTEN_ADDRESS, embedded.listenAddress() + ":" + embedded.boltPort());
+            builder.property(BoltConnector.enabled, true);
+            builder.property(BoltConnector.listen_address, new SocketAddress(embedded.listenAddress(), embedded.boltPort()));
         }
-        return properties;
+        return builder.build();
+    }
+
+    private static class Neo4jPropertiesBuilder {
+
+        private static final String NEO4J_PROPERTY_PREFIX = "neo4j.";
+
+        private Properties properties = new Properties();
+
+        static Neo4jPropertiesBuilder builder() {
+            return new Neo4jPropertiesBuilder();
+        }
+
+        <T> Neo4jPropertiesBuilder property(Setting<T> setting, T value) {
+            properties.setProperty(NEO4J_PROPERTY_PREFIX + setting.name(), value.toString());
+            return this;
+        }
+
+        Properties build() {
+            return properties;
+        }
     }
 }
