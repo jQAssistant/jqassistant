d63a20e4459d366ddfc6411b81c96c061db8776d
Dirk Mahler <dirk.mahler@buschmais.com>
2023-11-26 19:23:59 +0100

    moved AbstractServerPluginIT to jQA core test module

---
+++ core/test/src/main/java/com/buschmais/jqassistant/core/test/plugin/AbstractServerPluginIT.java
@@ -0,0 +1,36 @@
+package com.buschmais.jqassistant.core.test.plugin;
+
+import com.buschmais.jqassistant.core.runtime.api.configuration.ConfigurationBuilder;
+import com.buschmais.jqassistant.core.store.impl.EmbeddedGraphStore;
+import com.buschmais.jqassistant.neo4j.embedded.EmbeddedNeo4jServer;
+import com.buschmais.jqassistant.neo4j.embedded.configuration.Embedded;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Abstract base class for server tests.
+ */
+public abstract class AbstractServerPluginIT extends AbstractPluginIT {
+
+    private EmbeddedNeo4jServer server;
+
+    @Override
+    protected void configure(ConfigurationBuilder configurationBuilder) {
+        configurationBuilder.with(Embedded.class, Embedded.CONNECTOR_ENABLED, true);
+    }
+
+    @BeforeEach
+    void startServer() {
+        EmbeddedGraphStore embeddedGraphStore = (EmbeddedGraphStore) store;
+        server = embeddedGraphStore.getServer();
+        server.start();
+    }
+
+    @AfterEach
+    void stopServer() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+}
