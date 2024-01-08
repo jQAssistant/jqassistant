f59f59fcee9b1abfaedcf4cfb0bf139b8297169f
Dirk Mahler <dirk.mahler@buschmais.com>
2024-01-08 18:00:44 +0100

    #43 improved IT for JPMS

--- plugin/java/src/it/jpms/module1/src/main/java/module-info.java
+++ plugin/java/src/it/jpms/module1/src/main/java/module-info.java
@@ -1,4 +1,5 @@
-module module1 {
+module it.jpms.module1 {
     exports com.buschmais.jqassistant.plugin.java.it.jpms.module1.api;
     provides com.buschmais.jqassistant.plugin.java.it.jpms.module1.api.MyService with com.buschmais.jqassistant.plugin.java.it.jpms.module1.impl.MyServiceImpl;
+    opens com.buschmais.jqassistant.plugin.java.it.jpms.module1.api to it.jpms.module2;
 }
--- plugin/java/src/it/jpms/module2/src/main/java/module-info.java
+++ plugin/java/src/it/jpms/module2/src/main/java/module-info.java
@@ -1,4 +1,6 @@
-module module2 {
-    requires module1;
+module it.jpms.module2 {
+    requires transitive it.jpms.module1;
+    requires static java.se;
     uses com.buschmais.jqassistant.plugin.java.it.jpms.module1.api.MyService;
+    opens com.buschmais.jqassistant.plugin.java.it.jpms.module2.impl;
 }
