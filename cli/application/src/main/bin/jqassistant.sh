686b57f3898d7701076b973a610d2482db2c4176
Dirk Mahler <dirk.mahler@buschmais.com>
2024-03-03 18:14:04 +0100

    fixed jqassistant.sh

--- cli/application/src/main/bin/jqassistant.sh
+++ cli/application/src/main/bin/jqassistant.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-BIN_DIR=`dirname "$0"`
+BIN_DIR=$(dirname "$0")
 
-if [ "$JAVA_HOME" == "" ]; then
+if [ -z "$JAVA_HOME" ] ; then
   JAVA_CMD="java"
 else
   JAVA_CMD="$JAVA_HOME/bin/java"
 fi
 
-export JQASSISTANT_HOME=`cd "$BIN_DIR/.." && pwd -P`
+JQASSISTANT_HOME=$(cd "$BIN_DIR/.." && pwd -P)
 
 exec "$JAVA_CMD" $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/${project.groupId}-${project.artifactId}-${project.version}.${project.packaging}" "$@"
