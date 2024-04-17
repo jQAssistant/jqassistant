bb79cd51fef2cb409d50999baf5377a8757c7cf1
Dirk Mahler <dirk.mahler@buschmais.com>
2024-04-17 21:12:50 +0200

    #94 fixed checkstyle violation

--- core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/impl/AnalyzerRuleVisitor.java
+++ core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/impl/AnalyzerRuleVisitor.java
@@ -1,7 +1,6 @@
 package com.buschmais.jqassistant.core.analysis.impl;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
