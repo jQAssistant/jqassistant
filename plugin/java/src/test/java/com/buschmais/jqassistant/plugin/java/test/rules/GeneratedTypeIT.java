cc23b602ffa461a4819b1683df063844864777df
Dirk Mahler <dirk.mahler@buschmais.com>
2024-04-17 21:38:17 +0200

    #94 add support for abstract concepts

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/GeneratedTypeIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/GeneratedTypeIT.java
@@ -3,9 +3,7 @@ package com.buschmais.jqassistant.plugin.java.test.rules;
 import java.util.List;
 import java.util.Map;
 
-import com.buschmais.jqassistant.core.report.api.model.Column;
 import com.buschmais.jqassistant.core.report.api.model.Result;
-import com.buschmais.jqassistant.core.report.api.model.Row;
 import com.buschmais.jqassistant.core.rule.api.model.Concept;
 import com.buschmais.jqassistant.plugin.java.api.model.JavaArtifactFileDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
@@ -25,16 +23,17 @@ class GeneratedTypeIT extends AbstractJavaPluginIT {
     @Test
     void generatedTypes() throws Exception {
         scanClasses(Generated.class, NotGenerated.class);
+
         Result<Concept> result = applyConcept("java:GeneratedType"); // implicitly executes concept "test:GeneratedType"
+
         assertThat(result.getStatus()).isEqualTo(SUCCESS);
-        List<Row> rows = result.getRows();
+        assertThat(result.getRows()).isEmpty();
+        TestResult query = query(
+            "MATCH (artifact:Artifact)-[:CONTAINS]->(generatedType:Java:Type:Generated) RETURN artifact as Artifact, count(generatedType) as GeneratedTypes ORDER BY GeneratedTypes desc");
+        List<Map<String, Object>> rows = query.getRows();
         assertThat(rows).hasSize(1);
-        Map<String, Column<?>> row = rows.get(0).getColumns();
-        Column artifactColumn = row.get("Artifact");
-        assertThat(artifactColumn).isNotNull();
-        assertThat(artifactColumn.getValue()).isInstanceOf(JavaArtifactFileDescriptor.class);
-        Column generatedTypesColumn = row.get("GeneratedTypes");
-        assertThat(generatedTypesColumn).isNotNull();
-        assertThat(generatedTypesColumn.getValue()).isEqualTo(1l);
+        Map<String, Object> row = rows.get(0);
+        assertThat(row.get("Artifact")).isInstanceOf(JavaArtifactFileDescriptor.class);
+        assertThat(row.get("GeneratedTypes")).isEqualTo(1l);
     }
 }
