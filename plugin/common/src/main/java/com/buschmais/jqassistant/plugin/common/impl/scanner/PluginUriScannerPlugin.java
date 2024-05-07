16df9346e4c2a9c22150a594c6cb329927b30d4a
Dirk Mahler <dirk.mahler@buschmais.com>
2024-05-07 15:46:57 +0200

    Fail if a plugin resource is included for scanning but does not exist

--- plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/impl/scanner/PluginUriScannerPlugin.java
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/impl/scanner/PluginUriScannerPlugin.java
@@ -15,6 +15,15 @@ public class PluginUriScannerPlugin extends AbstractUriScannerPlugin<URL> {
 
     public static final String JQASSISTANT_PLUGIN = "jqassistant-plugin";
 
+    public static final String PROPERTY_IGNORE_NON_EXISTING_RESOURCES = "jqassistant-plugin.ignore-non-existing-resources";
+
+    private boolean ignoreNonExistingResources;
+
+    @Override
+    protected void configure() {
+        this.ignoreNonExistingResources = getBooleanProperty(PROPERTY_IGNORE_NON_EXISTING_RESOURCES, false);
+    }
+
     @Override
     public boolean accepts(URI uri, String path, Scope scope) throws IOException {
         return JQASSISTANT_PLUGIN.equalsIgnoreCase(uri.getScheme());
@@ -22,7 +31,14 @@ public class PluginUriScannerPlugin extends AbstractUriScannerPlugin<URL> {
 
     @Override
     protected Optional<URL> getResource(URI uri, ScannerContext context) {
-        return resolve(uri, () -> context.getClassLoader().getResource(uri.getSchemeSpecificPart()), context);
+        return resolve(uri, () -> {
+            URL resource = context.getClassLoader()
+                .getResource(uri.getSchemeSpecificPart());
+            if (resource == null && !ignoreNonExistingResources) {
+                throw new IllegalArgumentException("Cannot resolve resource '" + uri + "' from plugins.");
+            }
+            return resource;
+        }, context);
     }
 
 }
--- plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/scanner/PluginUriScannerPluginIT.java
+++ plugin/common/src/test/java/com/buschmais/jqassistant/plugin/common/test/scanner/PluginUriScannerPluginIT.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import com.buschmais.jqassistant.core.scanner.api.DefaultScope;
+import com.buschmais.jqassistant.core.scanner.impl.UnrecoverableScannerException;
 import com.buschmais.jqassistant.core.test.plugin.AbstractPluginIT;
 import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
 import com.buschmais.jqassistant.plugin.common.api.model.URIDescriptor;
@@ -11,11 +12,13 @@ import com.buschmais.jqassistant.plugin.common.api.model.URIDescriptor;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PluginUriScannerPluginIT extends AbstractPluginIT {
 
     private static final String DIRECTORY_RESOURCE = "jqassistant-plugin:test-resource.txt";
     private static final String JAR_RESOURCE = "jqassistant-plugin:java/lang/Object.class";
+    private static final String NON_EXISTING_RESOURCE = "jqassistant-plugin:non-existing-resource.txt";
 
     @Test
     void classpathDirectoryPluginResource() throws URISyntaxException {
@@ -34,6 +37,13 @@ class PluginUriScannerPluginIT extends AbstractPluginIT {
     @Test
     void jarPluginResource() throws URISyntaxException {
         verify(JAR_RESOURCE);
+    }
+
+    @Test
+    void nonExistingResource() {
+        UnrecoverableScannerException unrecoverableScannerException = assertThrows(UnrecoverableScannerException.class,
+            () -> getScanner().scan(new URI(NON_EXISTING_RESOURCE), NON_EXISTING_RESOURCE, DefaultScope.NONE));
+        assertThat(unrecoverableScannerException).hasMessageContaining(NON_EXISTING_RESOURCE);
     }
 
     private void verify(String uri) throws URISyntaxException {
