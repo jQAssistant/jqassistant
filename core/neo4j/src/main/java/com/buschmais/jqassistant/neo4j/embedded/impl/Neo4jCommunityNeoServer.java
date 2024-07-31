a84c1056dc78b1271c2b60571e37ab8f71215abc
Dirk Mahler <dirk.mahler@buschmais.com>
2024-07-31 19:14:48 +0200

    #563 fixed checkstyle violation

--- core/neo4j/src/main/java/com/buschmais/jqassistant/neo4j/embedded/impl/Neo4jCommunityNeoServer.java
+++ core/neo4j/src/main/java/com/buschmais/jqassistant/neo4j/embedded/impl/Neo4jCommunityNeoServer.java
@@ -3,20 +3,15 @@ package com.buschmais.jqassistant.neo4j.embedded.impl;
 import java.net.InetSocketAddress;
 
 import com.buschmais.jqassistant.neo4j.embedded.EmbeddedNeo4jServer;
-import com.buschmais.xo.neo4j.embedded.impl.datastore.EmbeddedDatastore;
 
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Slf4j
 class Neo4jCommunityNeoServer implements EmbeddedNeo4jServer {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jCommunityNeoServer.class);
 
     private ClassLoader classLoader;
 
@@ -39,13 +34,13 @@ class Neo4jCommunityNeoServer implements EmbeddedNeo4jServer {
         WebAppContext rootContext = getWebAppContext("/", "browser/");
         WebAppContext pluginContext = getWebAppContext("/jqassistant", "META-INF/jqassistant-static-content/");
         server.setHandler(new HandlerCollection(rootContext, pluginContext));
-        LOGGER.info("Starting HTTP server.");
+        log.info("Starting HTTP server.");
         try {
             server.start();
         } catch (Exception e) {
             throw new IllegalStateException("Cannot start embedded server.", e);
         }
-        LOGGER.info("Neo4j browser available at http://{}:{}?dbms=bolt://{}:{}&preselectAuthMethod=NO_AUTH.", listenAddress, httpPort, listenAddress, boltPort);
+        log.info("Neo4j browser available at http://{}:{}?dbms=bolt://{}:{}&preselectAuthMethod=NO_AUTH.", listenAddress, httpPort, listenAddress, boltPort);
     }
 
     private WebAppContext getWebAppContext(String contextPath, String resourceRoot) {
@@ -59,7 +54,7 @@ class Neo4jCommunityNeoServer implements EmbeddedNeo4jServer {
     @Override
     public void stop() {
         if (server != null) {
-            LOGGER.info("Stopping HTTP server.");
+            log.info("Stopping HTTP server.");
             try {
                 server.stop();
             } catch (Exception e) {
