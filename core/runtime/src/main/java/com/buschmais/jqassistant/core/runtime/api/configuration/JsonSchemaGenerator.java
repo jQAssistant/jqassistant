f9382262a966575bfad2b866cfd1c6bf702e1c42
Maria Brauer <maria.brauer@aksa-gmbh.de>
2024-11-18 16:14:54 +0100

    Adjusted schema generating logic

--- core/runtime/src/main/java/com/buschmais/jqassistant/core/runtime/api/configuration/JsonSchemaGenerator.java
+++ core/runtime/src/main/java/com/buschmais/jqassistant/core/runtime/api/configuration/JsonSchemaGenerator.java
@@ -1,7 +1,6 @@
 package com.buschmais.jqassistant.core.runtime.api.configuration;
 
 import com.fasterxml.classmate.ResolvedType;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.victools.jsonschema.generator.*;
@@ -13,85 +12,75 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-
 public class JsonSchemaGenerator {
 
     public ObjectNode generateSchema(Class<?> clazz) {
-        SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2020_12, OptionPreset.JAVA_OBJECT)
-            .with(Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES, Option.NONSTATIC_NONVOID_NONGETTER_METHODS,
-                Option.FIELDS_DERIVED_FROM_ARGUMENTFREE_METHODS, Option.FLATTENED_ENUMS, Option.ALLOF_CLEANUP_AT_THE_END, Option.ALLOF_CLEANUP_AT_THE_END).without(Option.VOID_METHODS, Option.GETTER_METHODS);
+        SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2019_09, OptionPreset.JAVA_OBJECT)
+            .with(Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES, Option.NONSTATIC_NONVOID_NONGETTER_METHODS, Option.PLAIN_DEFINITION_KEYS,
+                Option.FIELDS_DERIVED_FROM_ARGUMENTFREE_METHODS, Option.FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT, Option.FLATTENED_ENUMS, Option.ALLOF_CLEANUP_AT_THE_END).without(Option.VOID_METHODS, Option.GETTER_METHODS);
 
         configBuilder.forMethods()
             .withTargetTypeOverridesResolver(target -> {
                 ResolvedType resolvedType = target.getType();
                 return getResolvedTypes(target, resolvedType);
             });
-
         configBuilder.forFields()
             .withIgnoreCheck(field -> field.getName().startsWith("PREFIX") || field.getName().startsWith("SKIP"));
         configBuilder.forMethods().withPropertyNameOverrideResolver((member) -> mapToKebabCase(member.getName()));
         configBuilder.forFields().withPropertyNameOverrideResolver((member) -> mapToKebabCase(member.getName()));
-
+        configBuilder.forTypesInGeneral()
+            .withIdResolver(scope -> scope.getType().getErasedType().isInstance(Configuration.class) ? "core/runtime/target/test-classes/jsonSchema.json" : null);
         configBuilder.forTypesInGeneral()
             .withCustomDefinitionProvider((javaType, context) -> {
                 if (javaType.isInstanceOf(Map.class)) {
                     ObjectNode mapNode = context.getGeneratorConfig().createObjectNode();
                     mapNode.put("type", "object");
-                    StringBuilder types = null;
-                    for (int i = 0; i < javaType.getTypeBindings().size(); i++) {
-                        if (types == null) {
-                            types = new StringBuilder(javaType.getTypeBindings().getBoundType(i).getErasedType().getSimpleName().toLowerCase());
-                        }else{
-                            types.append(", ").append(javaType.getTypeBindings().getBoundType(i).getErasedType().getSimpleName().toLowerCase());
-                        }
-                    }
                     ObjectNode addNode = context.getGeneratorConfig().createObjectNode();
-                    addNode.put("type", types.toString());
+                    addNode.withArrayProperty("type");
                     mapNode.set("additionalProperties", addNode);
                     return new CustomDefinition(mapNode);
-            }
-        return null;
-    });
+                }
+                return null;
+            });
 
-    SchemaGenerator generator = new SchemaGenerator(configBuilder.build());
-    ObjectNode jsonSchema = generator.generateSchema(clazz);
-
-    saveSchemaToFile(jsonSchema, "target","jsonSchema.json");
+        SchemaGenerator generator = new SchemaGenerator(configBuilder.build());
+        ObjectNode jsonSchema = generator.generateSchema(clazz);
+        saveSchemaToFile(jsonSchema, "src/test/resources", "jsonSchema.json");
         return jsonSchema;
-}
+    }
 
-private static List<ResolvedType> getResolvedTypes(MethodScope target, ResolvedType resolvedType) {
-    if (resolvedType != null) {
-        if (resolvedType.getErasedType().equals(URI.class)) {
-            return List.of(target.getContext().resolve(String.class));
+    private static List<ResolvedType> getResolvedTypes(MethodScope target, ResolvedType resolvedType) {
+        if (resolvedType != null) {
+            if (resolvedType.getErasedType().equals(URI.class)) {
+                return List.of(target.getContext().resolve(String.class));
+            }
+            if (resolvedType.getErasedType().equals(Map.class) && resolvedType.getErasedType().getComponentType() != null) {
+                return List.of(target.getContext().resolve(Map.class));
+            }
+            if (resolvedType.getErasedType().equals(Optional.class)) {
+                return getResolvedTypes(target, resolvedType.getTypeParameters().get(0));
+            }
         }
-        if (resolvedType.getErasedType().equals(Map.class) && resolvedType.getErasedType().getComponentType() != null) {
-            return List.of(target.getContext().resolve(Map.class));
-        }
-        if (resolvedType.getErasedType().equals(Optional.class)) {
-            return getResolvedTypes(target, resolvedType.getTypeParameters().get(0));
+        return List.of(target.getContext().resolve(resolvedType));
+    }
+
+    private void saveSchemaToFile(ObjectNode schema, String targetFolder, String fileName) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File targetFile = new File(targetFolder, fileName);
+        try {
+            new File(targetFolder);
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(targetFile, schema);
+            System.out.println("Schema saved: " + targetFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    return List.of(target.getContext().resolve(resolvedType));
-}
 
-private void saveSchemaToFile(ObjectNode schema, String targetFolder, String fileName) {
-    ObjectMapper objectMapper = new ObjectMapper();
-    File targetFile = new File(targetFolder, fileName);
-    try {
-        new File(targetFolder);
-        objectMapper.writerWithDefaultPrettyPrinter().writeValue(targetFile, schema);
-        System.out.println("Schema saved: " + targetFile.getAbsolutePath());
-    } catch (IOException e) {
-        e.printStackTrace();
+    private static String mapToKebabCase(String name) {
+        if (name.matches("^[A-Z0-9_]+$")) {
+            return name.toLowerCase().replace("_", "-");
+        }
+        return name.replaceAll("([a-z0-9])([A-Z])", "$1-$2").toLowerCase();
     }
-}
-
-private static String mapToKebabCase(String name) {
-    if (name.matches("^[A-Z0-9_]+$")) {
-        return name.toLowerCase().replace("_", "-");
-    }
-    return name.replaceAll("([a-z0-9])([A-Z])", "$1-$2").toLowerCase();
-}
 
 }
--- core/runtime/src/test/java/com/buschmais/jqassistant/core/runtime/api/configuration/JsonSchemaGeneratorTest.java
+++ core/runtime/src/test/java/com/buschmais/jqassistant/core/runtime/api/configuration/JsonSchemaGeneratorTest.java
@@ -1,7 +1,19 @@
 package com.buschmais.jqassistant.core.runtime.api.configuration;
 
-import com.buschmais.jqassistant.core.runtime.impl.configuration.Maptest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import io.smallrye.config.ConfigMapping;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonSchemaGeneratorTest {
@@ -9,9 +21,25 @@ public class JsonSchemaGeneratorTest {
     private final JsonSchemaGenerator generator = new JsonSchemaGenerator();
 
     @Test
-    public void testJsonSchemaGenerating(){
-        assertThat(generator.generateSchema(Maptest.class)).isNotNull();
+    public void testJsonSchemaGenerating() throws IOException {
+        JsonNode node = generator.generateSchema(JQAssistant.class);
+        assertThat(node).isNotNull();
+
+        File file = new File("target/test-classes/jsonSchema.json");
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        JsonSchemaFactory bluePrintFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
+        JsonSchemaFactory schemaFactory = JsonSchemaFactory.builder(bluePrintFactory).build();
+        JsonSchema schema = schemaFactory.getSchema(node);
+        JsonNode rootNode = mapper.readTree(file);
+        Set<ValidationMessage> validationMessages = schema.validate(rootNode);
+        System.out.println(validationMessages);
 
     }
 
+    @ConfigMapping
+    private interface JQAssistant {
+
+        Configuration jqassistant();
+
+    }
 }
