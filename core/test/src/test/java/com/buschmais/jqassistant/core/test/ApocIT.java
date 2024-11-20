594f7d0cacd1c17f13f4ac4551911216bb3ff3d4
Dirk Mahler <dirk.mahler@buschmais.com>
2024-11-20 14:10:18 +0100

    #432 skip ApocIT in Java 11

--- core/test/src/test/java/com/buschmais/jqassistant/core/test/ApocIT.java
+++ core/test/src/test/java/com/buschmais/jqassistant/core/test/ApocIT.java
@@ -4,6 +4,7 @@ import com.buschmais.jqassistant.core.test.plugin.AbstractPluginIT;
 import com.buschmais.xo.api.Query;
 import com.buschmais.xo.api.Query.Result.CompositeRowObject;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,10 +12,14 @@ import static org.assertj.core.api.Assumptions.assumeThat;
 
 class ApocIT extends AbstractPluginIT {
 
-    @Test
-    void apocTest() {
+    @BeforeAll
+    static void setUp() {
         assumeThat(Runtime.version()
             .feature()).isGreaterThanOrEqualTo(17);
+    }
+
+    @Test
+    void apocTest() {
         store.beginTransaction();
         Query.Result<CompositeRowObject> rows = store.executeQuery("call apoc.help('apoc')");
         assertThat(rows).isNotEmpty();
