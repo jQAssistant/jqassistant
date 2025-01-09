c9832a8ef69cee164236cca09f4a74bbe4a5f8d1
Candy Lohse <candy.lohse@buschmais.com>
2025-01-09 15:55:14 +0100

    #756 Split tests for provided concept into rule-specific ones

--- plugin/java-testing/src/test/java/com/buschmais/jqassistant/plugin/java_testing/concept/MockitoIT.java
+++ plugin/java-testing/src/test/java/com/buschmais/jqassistant/plugin/java_testing/concept/MockitoIT.java
@@ -1,8 +1,14 @@
 package com.buschmais.jqassistant.plugin.java_testing.concept;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.buschmais.jqassistant.core.report.api.model.Column;
 import com.buschmais.jqassistant.core.report.api.model.Result;
+import com.buschmais.jqassistant.core.report.api.model.Row;
 import com.buschmais.jqassistant.core.rule.api.model.Concept;
 import com.buschmais.jqassistant.plugin.java.api.model.MethodDescriptor;
+import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +16,7 @@ import org.mockito.Mockito;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static com.buschmais.jqassistant.plugin.java.test.assertj.MethodDescriptorCondition.methodDescriptor;
+import static com.buschmais.jqassistant.plugin.java.test.assertj.TypeDescriptorCondition.typeDescriptor;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.type;
 
@@ -32,16 +39,44 @@ class MockitoIT extends AbstractJavaPluginIT {
             .getValue()).asInstanceOf(type(MethodDescriptor.class))
             .is(methodDescriptor(Mockito.class, "verify", Object.class));
 
-        final TestResult methodQueryResult = query(
-            "MATCH (testMethod:Method)-[:INVOKES]->(assertMethod:Method) "
-                + "WHERE assertMethod:Mockito:Assert "
-                + "RETURN testMethod, assertMethod");
-        assertThat(methodQueryResult.getRows().size()).isEqualTo(1);
-        assertThat(methodQueryResult.<MethodDescriptor>getColumn("testMethod"))
-            .haveExactly(1, methodDescriptor(AssertExample.class, "mockitoVerifyExampleMethod"));
-        assertThat(methodQueryResult.<MethodDescriptor>getColumn("assertMethod"))
-            .haveExactly(1, methodDescriptor(Mockito.class, "verify", Object.class));
+        verifyResultGraph();
 
         store.commitTransaction();
     }
+
+    @Test
+    void providedConceptAssertMethod() throws Exception {
+        scanClasses(AssertExample.class);
+
+        final Result<Concept> conceptResult = applyConcept("java:AssertMethod");
+        assertThat(conceptResult.getStatus()).isEqualTo(SUCCESS);
+
+        store.beginTransaction();
+
+        final List<TypeDescriptor> declaringTypes = conceptResult.getRows().stream()
+            .map(Row::getColumns)
+            .map(columns -> columns.get("DeclaringType"))
+            .map(Column::getValue)
+            .map(TypeDescriptor.class::cast)
+            .collect(Collectors.toList());
+        assertThat(declaringTypes).haveExactly(1, typeDescriptor(Mockito.class));
+
+        verifyResultGraph();
+
+        store.commitTransaction();
+    }
+
+    // Expects an open transaction
+    private void verifyResultGraph() throws NoSuchMethodException {
+        final TestResult methodQueryResultForMockito = query(
+            "MATCH (testMethod:Method)-[:INVOKES]->(assertMethod:Method) "
+                + "WHERE assertMethod:Mockito:Assert "
+                + "RETURN testMethod, assertMethod");
+        assertThat(methodQueryResultForMockito.getRows().size()).isEqualTo(1);
+        assertThat(methodQueryResultForMockito.<MethodDescriptor>getColumn("testMethod"))
+            .haveExactly(1, methodDescriptor(AssertExample.class, "mockitoVerifyExampleMethod"));
+        assertThat(methodQueryResultForMockito.<MethodDescriptor>getColumn("assertMethod"))
+            .haveExactly(1, methodDescriptor(Mockito.class, "verify", Object.class));
+    }
+
 }
