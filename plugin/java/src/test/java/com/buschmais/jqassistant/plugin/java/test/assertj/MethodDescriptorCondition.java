204e9cd1f5f17676da411478cd019f27a11bc00f
cl90 <c.lohse90@gmail.com>
2025-01-10 11:30:20 +0100

    756-plugin-java-testing (#760)
    * #756 Add java-testing plugin
    
    * #756 Fix javadoc and methodDescriptor(...)
    
    * #756 Add java-testing to doc
    
    * #756 Fix format of query strings
    
    * #756 Remove obsolete TODO comment
    
    * #756 Fix package name for integration tests
    
    * #756 Split tests for provided concept into rule-specific ones
    
    ---------
    
    Co-authored-by: Candy Lohse <candy.lohse@buschmais.com>

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/assertj/MethodDescriptorCondition.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/assertj/MethodDescriptorCondition.java
@@ -5,14 +5,13 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import com.buschmais.jqassistant.plugin.java.api.model.MethodDescriptor;
-import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
 
 import org.assertj.core.api.Condition;
 
 import static java.util.stream.Collectors.joining;
 
 /**
- * A {@link Condition} for asserting a {@link TypeDescriptor} by its name.
+ * A {@link Condition} for asserting a {@link MethodDescriptor} by its name.
  */
 public class MethodDescriptorCondition extends Condition<MethodDescriptor> {
 
@@ -37,7 +36,7 @@ public class MethodDescriptorCondition extends Condition<MethodDescriptor> {
     public static MethodDescriptorCondition methodDescriptor(Class<?> type, String name, Class<?>... parameterTypes) throws NoSuchMethodException {
         Method method = type.getDeclaredMethod(name, parameterTypes);
         return new MethodDescriptorCondition(type.getName(), method.getReturnType()
-            .getTypeName() + " " + name + getDeclaredParameters(type.getDeclaredConstructor(parameterTypes)));
+            .getTypeName() + " " + name + getDeclaredParameters(type.getDeclaredMethod(name, parameterTypes)));
     }
 
     public static MethodDescriptorCondition constructorDescriptor(Class<?> type, Class<?>... parameterTypes) throws NoSuchMethodException {
