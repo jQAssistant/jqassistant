c8015983e83f21a0a034a59ae4dc80485dc9b679
Dirk Mahler <dirk.mahler@buschmais.com>
2025-03-24 18:32:31 +0100

    #745 provide CLI script without .sh extension (#846)

--- cli/application/src/main/bin/jqassistant.sh
+++ cli/application/src/main/bin/jqassistant.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-BIN_DIR=$(dirname "$0")
+echo "Info: 'jqassistant.sh' has been deprecated and will be removed, please use 'jqassistant'"
+$(dirname "$0")/jqassistant $@
 
-if [ -z "$JAVA_HOME" ] ; then
-  JAVA_CMD="java"
-else
-  JAVA_CMD="$JAVA_HOME/bin/java"
-fi
-
-JQASSISTANT_HOME=$(cd "$BIN_DIR/.." && pwd -P)
-
-exec "$JAVA_CMD" $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/${project.groupId}-${project.artifactId}-${project.version}.${project.packaging}" "$@"
