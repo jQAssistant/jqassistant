6c9a370c162e195ef99ebc7ad5908f4e7841cafa
Dirk Mahler <dirk.mahler@buschmais.com>
2025-04-10 22:53:53 +0200

    #863 validate rule configuration for analyze task in first Maven modu… (#864)
    * #863 validate rule configuration for analyze task in first Maven module to provide an fail-fast behavior
    
    * #863 cleanup in AbstractProjectMojo
    
    * #863 cleanup in AbstractProjectMojo

--- maven/src/main/java/com/buschmais/jqassistant/scm/maven/AvailableScopesMojo.java
+++ maven/src/main/java/com/buschmais/jqassistant/scm/maven/AvailableScopesMojo.java
@@ -3,7 +3,6 @@ package com.buschmais.jqassistant.scm.maven;
 import com.buschmais.jqassistant.core.scanner.api.ScopeHelper;
 import com.buschmais.jqassistant.core.scanner.spi.ScannerPluginRepository;
 
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.slf4j.Logger;
@@ -18,7 +17,12 @@ public class AvailableScopesMojo extends AbstractProjectMojo {
     private Logger logger = LoggerFactory.getLogger(AvailableScopesMojo.class);
 
     @Override
-    public void aggregate(MojoExecutionContext mojoExecutionContext) throws MojoExecutionException {
+    protected void beforeProject(MojoExecutionContext mojoExecutionContext) {
+        // nothing to do here
+    }
+
+    @Override
+    public void afterProject(MojoExecutionContext mojoExecutionContext) {
         getLog().info("Available scopes for '" + mojoExecutionContext.getRootModule()
             .getName() + "'.");
         ScopeHelper scopeHelper = new ScopeHelper(logger);
