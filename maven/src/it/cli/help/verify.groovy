836e4825688986527600b24536065f7b80397186
Maria Brauer <maria.brauer@aksa-gmbh.de>
2025-04-10 10:47:11 +0200

    Fixed maven help goal groovy test

--- maven/src/it/cli/help/verify.groovy
+++ maven/src/it/cli/help/verify.groovy
@@ -1,4 +1,3 @@
-def outputFile = new File(basedir, "target/invoker/cli/help/build.log")
-assert outputFile.exists()
-def logContent = outputFile.text
-assert logContent.contains("Display help information on jqassistant-maven-plugin.").status == "success"
+File buildLog = new File(basedir, "build.log")
+assert buildLog.exists()
+assert buildLog.text.contains("Display help information on jqassistant-maven-plugin.")
