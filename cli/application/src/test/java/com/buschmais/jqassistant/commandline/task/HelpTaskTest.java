bd1fea23399eb76bd373525c2288c3c7d9554b69
Maria Brauer <maria.brauer@aksa-gmbh.de>
2025-04-24 16:33:19 +0200

    Adjusted cli help test

--- cli/application/src/test/java/com/buschmais/jqassistant/commandline/task/HelpTaskTest.java
+++ cli/application/src/test/java/com/buschmais/jqassistant/commandline/task/HelpTaskTest.java
@@ -3,26 +3,26 @@ package com.buschmais.jqassistant.commandline.task;
 import com.buschmais.jqassistant.commandline.CliExecutionException;
 import com.buschmais.jqassistant.commandline.Main;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.StdErr;
 import org.junitpioneer.jupiter.StdIo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Verifies functionality of the main class.
  */
-// might be given as system properties from Maven
 class HelpTaskTest {
 
     private final Main main = new Main();
 
     @Test
-    @StdIo("jqassistant help")
+    @StdIo
     void skipByCommandLineOption(StdErr stdErr) throws CliExecutionException {
         main.run(new String[] { "help" });
-        Assertions.assertTrue(stdErr.capturedString()
-            .contains("---- Available Tasks: ----"));
-        Assertions.assertTrue(stdErr.capturedString()
-            .contains("help': Lists all available tasks."));
+
+        String capturedOutput = stdErr.capturedString();
+        assertThat(capturedOutput).contains("---- Available Tasks: ----");
+        assertThat(capturedOutput).contains("help': Lists all available tasks.");
     }
 }
