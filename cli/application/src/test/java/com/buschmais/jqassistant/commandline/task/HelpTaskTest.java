8a054fed5d383663783ac831a3e429c37ef6816b
Dirk Mahler <dirk.mahler@buschmais.com>
2025-05-08 17:48:29 +0200

    #744 fixed CLI HelpTaskTest for "mvn verify" only

--- cli/application/src/test/java/com/buschmais/jqassistant/commandline/task/HelpTaskTest.java
+++ cli/application/src/test/java/com/buschmais/jqassistant/commandline/task/HelpTaskTest.java
@@ -1,25 +1,45 @@
 package com.buschmais.jqassistant.commandline.task;
 
 import com.buschmais.jqassistant.commandline.CliExecutionException;
-import com.buschmais.jqassistant.commandline.Main;
+import com.buschmais.jqassistant.core.shared.configuration.ConfigurationBuilder;
 
+import org.apache.commons.cli.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junitpioneer.jupiter.StdErr;
 import org.junitpioneer.jupiter.StdIo;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Verifies functionality of the main class.
  */
-class HelpTaskTest {
+@ExtendWith(MockitoExtension.class)
+class HelpTaskTest extends AbstractTaskTest {
 
-    private final Main main = new Main();
+    private HelpTask helpTask;
+
+    @BeforeEach
+    final void setUp() {
+        helpTask = new HelpTask();
+        helpTask.initialize(pluginRepository, storeFactory);
+    }
 
     @Test
     @StdIo
-    void skipByCommandLineOption(StdErr stdErr) throws CliExecutionException {
-        main.run(new String[] { "help" });
+    void help(StdErr stdErr) throws CliExecutionException, ParseException {
+        Options options = new Options();
+        for (Option option : helpTask.getOptions()) {
+            options.addOption(option);
+        }
+        CommandLineParser parser = new BasicParser();
+        CommandLine commandLine = parser.parse(options, new String[] {});
+
+        helpTask.configure(commandLine, mock(ConfigurationBuilder.class));
+        helpTask.run(configuration, options);
 
         String capturedOutput = stdErr.capturedString();
         assertThat(capturedOutput).contains("---- Available Tasks: ----");
