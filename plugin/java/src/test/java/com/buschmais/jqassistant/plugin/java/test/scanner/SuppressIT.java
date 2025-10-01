d4366402c54637427db42570f7d792b2e30fe3c3
Maria Brauer <maria.brauer@aksa-gmbh.de>
2025-10-01 13:22:06 +0200

    #927 added IT tests and constraints for suppress until value

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/scanner/SuppressIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/scanner/SuppressIT.java
@@ -1,5 +1,8 @@
 package com.buschmais.jqassistant.plugin.java.test.scanner;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import com.buschmais.jqassistant.core.report.api.model.Result;
 import com.buschmais.jqassistant.core.report.api.model.Row;
 import com.buschmais.jqassistant.core.rule.api.model.Concept;
@@ -11,6 +14,7 @@ import com.buschmais.jqassistant.plugin.java.test.set.scanner.suppress.Suppress;
 
 import org.junit.jupiter.api.Test;
 
+import static com.buschmais.jqassistant.core.report.api.model.Result.Status.FAILURE;
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,7 +40,56 @@ class SuppressIT extends AbstractJavaPluginIT {
 
     @Test
     void suppressedField() throws RuleException {
-        verifySuppress("suppress:Field", "suppress:SuppressedField", "field");
+
+        scanClasses(Suppress.class);
+        Result<Constraint> result = validateConstraint("suppress:Field");
+        assertThat(result.getStatus()).isEqualTo(FAILURE);
+        assertThat(result.getRows()
+            .size()).isEqualTo(2); // two not suppressed values due to expired and no expirationDates
+
+        Result<Concept> supressedItems = applyConcept("suppress:SuppressedField");
+        store.beginTransaction();
+        assertThat(supressedItems.getStatus()).isEqualTo(SUCCESS);
+        assertThat(supressedItems.getRows()
+            .size()).isEqualTo(3);
+        for (Row row : supressedItems.getRows()) {
+            JavaSuppressDescriptor suppressDescriptor = (JavaSuppressDescriptor) row.getColumns()
+                .get("field")
+                .getValue();
+            assertThat(asList(suppressDescriptor.getSuppressIds()), hasItem("suppress:Field"));
+        }
+        store.commitTransaction();
+    }
+
+    @Test
+    void expirationDateShouldNotBeTooFarInTheFuture() throws RuleException {
+        scanClasses(Suppress.class);
+        Result<Constraint> result = validateConstraint("suppress:ExpirationDateShouldNotBeTooFarInTheFuture");
+        assertThat(result.getStatus()).isEqualTo(FAILURE);
+        assertThat(result.getRows()
+            .size()).isEqualTo(3);
+    }
+
+    @Test
+    void expirationDatesShouldNotBeInThePast() throws RuleException {
+        scanClasses(Suppress.class);
+        Result<Constraint> result = validateConstraint("suppress:ExpirationDatesShouldNotBeNullOrInThePast");
+        assertThat(result.getStatus()).isEqualTo(FAILURE);
+        assertThat(result.getRows()
+            .size()).isEqualTo(2);
+    }
+
+    @Test
+    void suppressExpiresInLessThanOneMonth() throws RuleException {
+        scanClasses(Suppress.class);
+        LocalDate dateInTwoWeeks = LocalDate.now()
+            .plusWeeks(2);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        query("MATCH (n:Java:jQASuppress {name: 'expiredValue'}) SET n.suppressUntil = '" + dateInTwoWeeks.format(formatter) + "' RETURN n");
+        Result<Constraint> result = validateConstraint("suppress:suppressExpiresInLessThanOneMonth");
+        assertThat(result.getStatus()).isEqualTo(FAILURE);
+        assertThat(result.getRows()
+            .size()).isEqualTo(1);
     }
 
     @Test
--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/set/scanner/suppress/Suppress.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/set/scanner/suppress/Suppress.java
@@ -2,14 +2,20 @@ package com.buschmais.jqassistant.plugin.java.test.set.scanner.suppress;
 
 import com.buschmais.jqassistant.plugin.java.api.annotation.jQASuppress;
 
-@jQASuppress(value = "suppress:Class",  reason = "For testing this annotation", until = "25/08/2025")
+@jQASuppress(value = "suppress:Class",  reason = "For testing this annotation", until = "25/08/2075")
 public class Suppress {
 
-    @jQASuppress({ "suppress:Field" })
-    private String value;
+    @jQASuppress(value = "suppress:Field", until = "25/08/2025") // date expired, should not be suppressed
+    private String expiredValue;
 
-    @jQASuppress(value = { "suppress:Method", "suppress:MethodInPrimaryColumn", "suppress:MethodInNonPrimaryColumn" }, column = "method", until = "31/12/2049")
-    public void doSomething() {
+    @jQASuppress({ "suppress:Field" }) // no expiration Date, should not be suppressed
+    private String noExpirationDateValue;
+
+    @jQASuppress(value = "suppress:Field", until = "02/03/2075") // should be suppressed
+    private String suppressedValue;
+
+    @jQASuppress(value = { "suppress:Method", "suppress:MethodInPrimaryColumn", "suppress:MethodInNonPrimaryColumn" }, column = "method", until = "31/12/2075")
+    public void suppressedMethod() {
     }
 
 }
