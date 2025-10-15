8a6536a1f96210d12c3533d334970694db965479
Dirk Mahler <dirk.mahler@buschmais.com>
2025-10-15 19:17:37 +0200

    #893 fixed Neo4jCommunityServerFactoryTest

--- core/neo4j/src/test/java/com/buschmais/jqassistant/neo4j/embedded/impl/Neo4jCommunityServerFactoryTest.java
+++ core/neo4j/src/test/java/com/buschmais/jqassistant/neo4j/embedded/impl/Neo4jCommunityServerFactoryTest.java
@@ -16,9 +16,19 @@ class Neo4jCommunityServerFactoryTest {
     void properties() {
         Properties properties = serverFactory.getProperties(true, "localhost", 7687,
             Map.of("dbms.memory.transaction.total.max", "8G", "db.tx_log.rotation.size", "1M"), emptyList());
-
-        assertThat(properties).containsEntry("neo4j.server.bolt.enabled", "true");
-        assertThat(properties).containsEntry("neo4j.server.bolt.listen_address", "localhost:7687");
+        switch (Runtime.version()
+            .feature()) {
+        case 11:
+            // Neo4j 4x
+            assertThat(properties).containsEntry("neo4j.dbms.connector.bolt.enabled", "true");
+            assertThat(properties).containsEntry("neo4j.dbms.connector.bolt.listen_address", "localhost:7687");
+            break;
+        default:
+            // Neo4j 5x
+            assertThat(properties).containsEntry("neo4j.server.bolt.enabled", "true");
+            assertThat(properties).containsEntry("neo4j.server.bolt.listen_address", "localhost:7687");
+            break;
+        }
         assertThat(properties).containsEntry("neo4j.dbms.memory.transaction.total.max", "8G");
         // overrides default value
         assertThat(properties).containsEntry("neo4j.db.tx_log.rotation.size", "1M");
