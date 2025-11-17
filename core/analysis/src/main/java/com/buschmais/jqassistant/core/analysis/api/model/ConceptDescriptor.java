18717bc8e27441509730f7b684688ebd57b8961f
Dirk Mahler <dirk.mahler@buschmais.com>
2025-11-17 08:05:47 +0100

    #889 improved AnalyzeAuditIT

--- core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/api/model/ConceptDescriptor.java
+++ core/analysis/src/main/java/com/buschmais/jqassistant/core/analysis/api/model/ConceptDescriptor.java
@@ -1,9 +1,14 @@
 package com.buschmais.jqassistant.core.analysis.api.model;
 
+import java.lang.annotation.Retention;
 import java.util.List;
 
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
+import com.buschmais.xo.neo4j.api.annotation.Relation.Incoming;
+import com.buschmais.xo.neo4j.api.annotation.Relation.Outgoing;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Describes an executed constraint.
@@ -11,6 +16,16 @@ import com.buschmais.xo.neo4j.api.annotation.Relation;
 @Label("Concept")
 public interface ConceptDescriptor extends RuleDescriptor, ExecutableRuleTemplate {
 
-    @Relation("PROVIDES_CONCEPT")
+    @Outgoing
+    @ProvidesConcept
     List<ConceptDescriptor> getProvidesConcepts();
+
+    @Incoming
+    @ProvidesConcept
+    List<ConceptDescriptor> getProvidingConcepts();
+
+    @Relation("PROVIDES_CONCEPT")
+    @Retention(RUNTIME)
+    @interface ProvidesConcept {
+    }
 }
--- core/test/src/test/java/com/buschmais/jqassistant/core/test/AnalyzeAuditIT.java
+++ core/test/src/test/java/com/buschmais/jqassistant/core/test/AnalyzeAuditIT.java
@@ -14,13 +14,14 @@ import org.junit.jupiter.api.Test;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.FAILURE;
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
+import static com.buschmais.jqassistant.core.rule.api.model.Severity.MINOR;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnalyzeAuditIT extends AbstractPluginIT {
 
     @Test
-    void test() throws RuleException {
-        executeGroup("core-test:TestGroup");
+    void auditGraph() throws RuleException {
+        executeGroup("core-test-audit:Group");
         store.beginTransaction();
         TestResult query = query(
             "MATCH (task:jQAssistant:Task)-[:INCLUDES_GROUP]->(group)-[:INCLUDES_CONSTRAINT]->(constraint:jQAssistant:Constraint)-[:REQUIRES_CONCEPT]->(concept:jQAssistant:Concept) RETURN task, group, constraint, concept");
@@ -33,20 +34,36 @@ public class AnalyzeAuditIT extends AbstractPluginIT {
 
         GroupDescriptor group = (GroupDescriptor) row.get("group");
         assertThat(group).isNotNull();
-        assertThat(group.getId()).isEqualTo("core-test:TestGroup");
+        assertThat(group.getId()).isEqualTo("core-test-audit:Group");
+        assertThat(group.getSeverity()).isNull();
+        assertThat(group.getEffectiveSeverity()).isNull();
         assertThat(analyzeTask.getIncludesGroups()).contains(group);
         assertThat(analyzeTask.getIncludesConcepts()).isEmpty();
         assertThat(analyzeTask.getIncludesConstraints()).isEmpty();
 
         ConceptDescriptor concept = (ConceptDescriptor) row.get("concept");
         assertThat(concept).isNotNull();
-        assertThat(concept.getId()).isEqualTo("core-test:TestConcept");
+        assertThat(concept.getId()).isEqualTo("core-test-audit:Concept");
         assertThat(concept.getStatus()).isEqualTo(SUCCESS);
+        assertThat(group.getSeverity()).isNull();
+        assertThat(group.getEffectiveSeverity()).isNull();
+        assertThat(concept.getRequiresConcepts()).isEmpty();
+        assertThat(concept.getProvidesConcepts()).isEmpty();
+
+        List<ConceptDescriptor> providingConcepts = concept.getProvidingConcepts();
+        assertThat(providingConcepts).hasSize(1);
+        ConceptDescriptor providingConcept = providingConcepts.get(0);
+        assertThat(providingConcept.getId()).isEqualTo("core-test-audit:ProvidingConcept");
+        assertThat(providingConcept.getStatus()).isEqualTo(SUCCESS);
+        assertThat(providingConcept.getSeverity()).isEqualTo(MINOR);
+        assertThat(providingConcept.getEffectiveSeverity()).isEqualTo(MINOR);
 
         ConstraintDescriptor constraint = (ConstraintDescriptor) row.get("constraint");
         assertThat(constraint).isNotNull();
-        assertThat(constraint.getId()).isEqualTo("core-test:TestConstraint");
+        assertThat(constraint.getId()).isEqualTo("core-test-audit:Constraint");
         assertThat(constraint.getStatus()).isEqualTo(FAILURE);
+        assertThat(group.getSeverity()).isNull();
+        assertThat(group.getEffectiveSeverity()).isNull();
         assertThat(group.getIncludesConstraints()).contains(constraint);
         assertThat(constraint.getRequiresConcepts()).contains(concept);
 
