dd2f10b1305b18155ada73b7725c1160da62da68
Maria Brauer <maria.brauer@aksa-gmbh.de>
2025-11-27 10:20:47 +0100

    #1022 simplified ruleParameter test

--- maven/src/it/singlemodule/ruleparameter/verify.groovy
+++ maven/src/it/singlemodule/ruleparameter/verify.groovy
@@ -2,5 +2,12 @@ def reportFile = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
 assert reportFile.exists()
 def jqassistantReport = new groovy.xml.XmlSlurper().parse(reportFile)
 def defaultGroup = jqassistantReport.group.find{ it.@id = 'default' }
-assert defaultGroup.concept.find { it.@id == 'test:RuleParameter' }.result.rows.@count == 1
-assert defaultGroup.concept.find { it.@id == 'test:MapParameter' }.result.rows.@count == 1
+def simpleResult =  defaultGroup.concept.find { it.@id == 'test:SimpleParameter' }.result
+def mapResult = defaultGroup.concept.find { it.@id == 'test:MapParameter' }.result
+
+assert simpleResult.rows.@count == 1
+assert simpleResult.rows.row[0].column[0].value == "value0"
+assert mapResult.rows.@count == 1
+assert mapResult.rows.row[0].column[0].value == "value1"
+assert mapResult.rows.row[0].column[1].value == "value2"
+
