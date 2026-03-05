630bdec6c193281eec338b313b380ca4b7f62998
Dirk Mahler <dirk.mahler@buschmais.com>
2026-03-05 09:13:40 +0100

    #1127 added scanner for Maven Projects with packaging WAR

---
+++ maven/src/it/singlemodule/packaging-war/verify.groovy
@@ -0,0 +1,6 @@
+def reportFile1 = new File(basedir, 'target/jqassistant/jqassistant-report.xml')
+assert reportFile1.exists()
+def jqassistantReport = new groovy.xml.XmlSlurper().parse(reportFile1)
+def defaultGroup = jqassistantReport.group.find { it.@id = 'it' }
+def concept = defaultGroup.concept.find { it.@id == 'it:ProjectContainsWebXML' }
+assert concept.status == 'success'
