fad1d1a5133395a650a6561c08a37b26deb5b2fb
Dirk Mahler <dirk.mahler@buschmais.com>
2026-03-21 09:56:05 +0100

    #1144 fixed Java JPMS IT

--- plugin/java/src/it/jpms/module1/src/main/java/module-info.java
+++ plugin/java/src/it/jpms/module1/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module java.it.jpms.module1 {
-    requires tools.jackson.core;
+    requires com.fasterxml.jackson.core;
 
     exports com.buschmais.jqassistant.plugin.java.it.jpms.module1.api;
     exports com.buschmais.jqassistant.plugin.java.it.jpms.module1.impl to java.it.jpms.module2;
