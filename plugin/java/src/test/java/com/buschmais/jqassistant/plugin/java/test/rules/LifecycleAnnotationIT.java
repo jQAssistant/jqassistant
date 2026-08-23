91f1600143fb62b113418747d78c0b1bcc3fca96
Dirk Mahler <dirk.mahler@buschmais.com>
2026-08-23 14:24:45 +0200

    #1138 extend concepts "java:PostConstruct" and "java:PreDestroy" to support Jakarta EE annotations

--- plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/LifecycleAnnotationIT.java
+++ plugin/java/src/test/java/com/buschmais/jqassistant/plugin/java/test/rules/LifecycleAnnotationIT.java
@@ -5,28 +5,35 @@ import com.buschmais.jqassistant.plugin.java.api.model.MethodDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
 import com.buschmais.jqassistant.plugin.java.test.set.rules.lifecycleannotation.ManagedResource;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.buschmais.jqassistant.plugin.java.test.assertj.MethodDescriptorCondition.methodDescriptor;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LifecycleAnnotationIT extends AbstractJavaPluginIT {
 
-    @Test
-    void postConstruct() throws NoSuchMethodException, RuleException {
-        scanClasses(ManagedResource.class);
+    @ParameterizedTest
+    @ValueSource(classes = { ManagedResource.JavaEE.class, ManagedResource.JakartaEE.class })
+    void postConstruct(Class<?> type) throws NoSuchMethodException, RuleException {
+        scanClasses(type);
         applyConcept("java:PostConstruct");
         store.beginTransaction();
-        MethodDescriptor m = store.executeQuery("MATCH (m:PostConstruct:Method) RETURN m").getSingleResult().get("m", MethodDescriptor.class);
-        assertThat(m, methodDescriptor(ManagedResource.class, "postConstruct"));
+        MethodDescriptor m = store.executeQuery("MATCH (m:PostConstruct:Method) RETURN m")
+            .getSingleResult()
+            .get("m", MethodDescriptor.class);
+        assertThat(m).is(methodDescriptor(type, "postConstruct"));
     }
 
-    @Test
-    void preDestroy() throws NoSuchMethodException, RuleException {
-        scanClasses(ManagedResource.class);
+    @ParameterizedTest
+    @ValueSource(classes = { ManagedResource.JavaEE.class, ManagedResource.JakartaEE.class })
+    void preDestroy(Class<?> type) throws NoSuchMethodException, RuleException {
+        scanClasses(type);
         applyConcept("java:PreDestroy");
         store.beginTransaction();
-        MethodDescriptor m = store.executeQuery("MATCH (m:PreDestroy:Method) RETURN m").getSingleResult().get("m", MethodDescriptor.class);
-        assertThat(m, methodDescriptor(ManagedResource.class, "preDestroy"));
+        MethodDescriptor m = store.executeQuery("MATCH (m:PreDestroy:Method) RETURN m")
+            .getSingleResult()
+            .get("m", MethodDescriptor.class);
+        assertThat(m).is(methodDescriptor(type, "preDestroy"));
     }
 }
