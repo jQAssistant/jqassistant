169a9534c57d7d32f8157debd3c30a70cecec55e
Dirk Mahler <dirk.mahler@buschmais.com>
2026-09-04 07:36:20 +0200

    #1077 made LocalFileDescriptor non-abstract

--- plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/LocalFileDescriptor.java
+++ plugin/common/src/main/java/com/buschmais/jqassistant/plugin/common/api/model/LocalFileDescriptor.java
@@ -1,12 +1,10 @@
 package com.buschmais.jqassistant.plugin.common.api.model;
 
-import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 /**
  * Describes a local {@link FileDescriptor}.
  */
-@Abstract
 @Label("Local")
 public interface LocalFileDescriptor extends FileDescriptor {
 }
